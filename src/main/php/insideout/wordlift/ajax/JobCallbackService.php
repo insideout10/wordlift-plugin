b58c7469745f9e8ea88e9b79c4a3ece03a4b41d8
David Riccitelli <ziodave@gmail.com>
2012-08-25 12:11:25 +0300

    add code to save an entity subject and bind a post to an entity

--- src/main/php/insideout/wordlift/ajax/JobCallbackService.php
+++ src/main/php/insideout/wordlift/ajax/JobCallbackService.php
@@ -9,8 +9,24 @@ class WordLift_JobCallbackService {
 
     public $logger;
 
+    /** @var WordLift_EntityService $entityService */
+    public $entityService;
+    /** @var WordLift_JobService $jobService */
+    public $jobService;
+
     public function callback( $jobID, $requestBody ) {
         $this->logger->trace( "A message has been received [ jobID :: $jobID ][ requestBody :: $requestBody ]." );
+
+        // exit if the job ID does NOT exist.
+        $posts = $this->jobService->getPostByJobID( $jobID );
+
+        if ( 0 === count( $posts ) ) {
+            $this->logger->error( "No job found for id [ jobID :: $jobID ]." );
+            return;
+        }
+
+        $postID = $posts[0]->ID;
+        $this->logger->trace( "A post was found [ postID :: $postID ][ jobID :: $jobID ]." );
 
         $parser = ARC2::getRDFParser();
         $parser->parseData( $requestBody );
@@ -20,10 +36,46 @@ class WordLift_JobCallbackService {
 
         $index = $parser->getSimpleIndex(0);
 
+        // list all the subjects.
         foreach ( $index as $subject => $predicates ) {
             $predicatesCount = count( $predicates );
 
-            $this->logger->trace( "[ subject :: $subject ][ predicatesCount :: $predicatesCount ]" );
+            // check if the subject is blank node.
+            $isBlankNode = ( 1 === preg_match( '/^\_:/', $subject ) );
+
+            $this->logger->trace( "[ subject :: $subject ][ predicatesCount :: $predicatesCount ][ isBlankNode :: " . ( $isBlankNode ? "yes" : "no" ) . " ]." );
+
+            if ( $isBlankNode ) continue;
+
+            $posts = $this->entityService->getBySubject( $subject );
+            $postsCount = count( $posts );
+
+            $this->logger->trace( "$postsCount post(s) found with subject [ subject :: $subject ]." );
+
+        // ##### E N T I T I E S #####
+        // load the existing entity if the subject is not anonymous.
+
+        // update the existing entity if found.
+
+        // create a new entity if not found.
+
+            if ( 0 === $postsCount )
+                $this->entityService->create( $subject );
+
+            $this->entityService->bindPostToSubjects( $postID, $subject );
+
+        // ##### A N O N Y M O U S  E N T I T I E S #####
+        // create a checksum for the anonymous entity.
+
+        // load an existing entity by checksum.
+
+        // create an entity by checksum if not found.
+
+        // save the properties.
+
+        // create a referenced entity if the property is a bnode reference.
+
+
 
             foreach ( $predicates as $predicate => $objects ) {
                 $objectsCount = count( $objects );
