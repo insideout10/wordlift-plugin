e9912edaa87f7b9ad86794758f3801ae9c0415e8
David Riccitelli <ziodave@gmail.com>
2012-08-28 15:50:50 +0300

    remove unused methods

--- src/main/php/insideout/wordlift/services/PlainTextJobRequestService.php
+++ src/main/php/insideout/wordlift/services/PlainTextJobRequestService.php
@@ -4,14 +4,6 @@ class WordLift_PlainTextJobRequestService implements WordLift_JobRequestService 
 
     public $logger;
 
-    public $url;
-    public $completeAction;
-    public $progressAction;
-
-    public $onCompleteURL;
-    public $onProgressURL;
-    public $chainName;
-
     public $jobService;
 
     public $consumerKey;
@@ -80,58 +72,6 @@ class WordLift_PlainTextJobRequestService implements WordLift_JobRequestService 
         // decode the response to a job response.
         return json_decode( $response );
 
-//        update_post_meta($postID, $this->metaKeyJobID, $jobResponse->jobID );
-//
-//        $this->logger->trace( "[ jobID :: $jobResponse->jobID ]" );
-
-    }
-
-    public function create( $text, $onCompleteURL = NULL, $onProgressURL = NULL, $chainName = NULL ) {
-
-        if ( NULL === $onCompleteURL )
-            $onCompleteURL = admin_url("admin-ajax.php?action=$this->completeAction" );
-
-        if ( NULL === $onProgressURL )
-            $onProgressURL = admin_url("admin-ajax.php?action=$this->progressAction" );
-
-        if ( NULL === $chainName )
-            $chainName = $this->chainName;
-
-        return new WordLift_JobRequest( $text, $onCompleteURL, $onProgressURL, $chainName );
-    }
-
-    public function postText( $text ) {
-        return $this->post( $this->create( $text ) );
-    }
-
-    public function post ( $jobRequest ) {
-
-        $this->logger->trace( "Sending a job-request to $this->url [complete :: $jobRequest->onCompleteUrl][progress :: $jobRequest->onProgressUrl]." );
-
-        $parameters = array(
-            "method" => "POST",
-            "timeout" => 45,
-            "redirection" => 5,
-            "httpversion" => "1.0",
-            "blocking" => true,
-            "headers" => array(
-                "Content-Type" => "application/json",
-                "Accept" => "application/json"
-            ),
-            "body" => json_encode( $jobRequest ),
-            "cookies" => array()
-        );
-
-        $return = wp_remote_post( $this->url, $parameters );
-
-        if ( is_wp_error($return) ) {
-            $this->logger->error('An error occurred: '.$return->get_error_message());
-            return NULL;
-        }
-
-        $jsonBody = json_decode( $return["body"] );
-
-        return $this->jobService->createJob( $jsonBody->id, WordLift_JobService::IN_PROGRESS );
     }
 
 }
