678ab8165cc6b6980828d2ff111123a83ac92376
David Riccitelli <david@insideout.io>
2012-12-30 16:13:31 +0200

    implement changesets when storing the analysis results

--- src/main/php/insideout/wordlift/ajax/PropertyAjaxService.php
+++ src/main/php/insideout/wordlift/ajax/PropertyAjaxService.php
@@ -4,20 +4,44 @@ class WordLift_PropertyAjaxService {
 	
 	public $queryService;
 	public $recordSetService;
+	public $changeSetService;
+	public $triplesUtils;
 
 	// const NAMESPACE = "dbpedia";
 
 	public function delete( $subject, $requestBody ) {
-		$statement = $this->getStatement( $subject, $requestBody );
+// 		$statement = $this->getStatement( $subject, $requestBody );
 
-		$query = <<<EOF
-			DELETE {
-				$statement
-}
+// 		$query = <<<EOF
+// 			DELETE {
+// 				$statement
+// }
 
-EOF;
+// EOF;
 
-		$this->queryService->query( $query );
+		// $this->queryService->query( $query );
+		$predicate = $property->key;
+		$value = $property->value;
+		$language = $property->lang;
+		$type = $property->type;
+
+		$subject = "http://dbpedia.org/resource/$subject";
+
+		$property = json_decode( $requestBody );
+		$index = array( $subject => array(
+				$predicate => array( array(
+						"value" => $value,
+						"type" => $type
+				) )
+		) );
+
+		if ( ! empty( $language ) )
+			$index[ $subject ][ $predicate ][ 0 ][ "lang" ] = $language
+
+        // $index = $this->triplesUtils->getIndexFromData( $requestBody );
+        $newIndex = $this->triplesUtils->bNodesToMD5( $index );
+        $this->changeSetService->applyChanges( $newIndex, "user", false, "User request" );
+
 	}
 
 	public function save( $subject, $requestBody ) {
