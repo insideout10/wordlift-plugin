772d8d908bca067fb4b53f130a6d239f069fc203
David Riccitelli <david@insideout.io>
2014-01-04 10:20:56 +0200

    add wordlift_get_url method to define the scripts/styles URLs based on development flag

--- src/wordlift.php
+++ src/wordlift.php
@@ -9,78 +9,114 @@ Author URI: http://www.insideout.io
 License: APL
 */
 
-function wordlift_load_admin_css() {
-		// TODO Parametrizzare la directory corrente del plugin
-        wp_register_style( 'wordlift_wp_admin_css', '/wp-content/plugins/wordlift/css/wordlift-admin.css', false, '1.0.0' );
-        wp_enqueue_style( 'wordlift_wp_admin_css' );
+/**
+ * Get the URL of the specified physical file.
+ * @param string $file The path to the file from the plugin root folder.
+ * @return string The URL to the file.
+ */
+function wordlift_get_url($file)
+{
+
+    // if WordLift is set into development mode, then provide a static URL, as development is done with symbolic link.
+    if (defined('WORDLIFT_DEVELOPMENT')) {
+        return '/wp-content/plugins/wordlift' . $file;
+    }
+
+    // use standard WP methods in production mode.
+    return plugins_url($file, __FILE__);
 }
 
-add_action( 'admin_enqueue_scripts', 'wordlift_load_admin_css' );
+/**
+ * Load stylesheets for the administrative interface.
+ */
+function wordlift_load_admin_css()
+{
+    wp_register_style('wordlift_wp_admin_css', wordlift_get_url('/css/wordlift-admin.min.css'), false, '1.0.0');
+    wp_enqueue_style('wordlift_wp_admin_css');
+}
 
-function wordlift_buttonhooks() {
-   // Only add hooks when the current user has permissions AND is in Rich Text editor mode
-   if ( ( current_user_can('edit_posts') || current_user_can('edit_pages') ) && get_user_option('rich_editing') ) {
-     add_filter("mce_external_plugins", "wordlift_register_tinymce_javascript");
-     add_filter('mce_buttons', 'wordlift_register_buttons');
-   }
+add_action('admin_enqueue_scripts', 'wordlift_load_admin_css');
+
+/**
+ * Add buttons hook for the TinyMCE editor. This method is called by the WP init hook.
+ */
+function wordlift_buttonhooks()
+{
+
+    // Only add hooks when the current user has permissions AND is in Rich Text editor mode
+    if ((current_user_can('edit_posts') || current_user_can('edit_pages')) && get_user_option('rich_editing')) {
+        add_filter('mce_external_plugins', 'wordlift_register_tinymce_javascript');
+        add_filter('mce_buttons',          'wordlift_register_buttons');
+    }
 }
- 
-function wordlift_register_buttons($buttons) {
-	array_push($buttons, 'wordlift');
-   return $buttons;
+
+/**
+ * Register the TinyMCE buttons. This method is called by the WP mce_buttons hook.
+ * @param array $buttons The existing buttons array.
+ * @return array The modified buttons array.
+ */
+function wordlift_register_buttons($buttons)
+{
+    // push the wordlift button the array.
+    array_push($buttons, 'wordlift');
+    return $buttons;
 }
- 
-// Load the TinyMCE plugin : editor_plugin.js (wp2.5)
-function wordlift_register_tinymce_javascript($plugin_array) {
-   // $plugin_array['wordlift'] = plugins_url('/js/wordlift-tinymce-plugin.js',__file__);
-   $plugin_array['wordlift'] = '/wp-content/plugins/wordlift/js/wordlift-tinymce-plugin.js';
-   
-   return $plugin_array;
+
+/**
+ * Load the TinyMCE plugin. This method is called by the WP mce_external_plugins hook.
+ * @param array $plugin_array The existing plugins array.
+ * @return array The modified plugins array.
+ */
+function wordlift_register_tinymce_javascript($plugin_array)
+{
+    // add the wordlift plugin.
+    $plugin_array['wordlift'] = wordlift_get_url('/js/wordlift-tinymce-plugin.min.js');
+    return $plugin_array;
 }
- 
+
 // init process for button control
 add_action('init', 'wordlift_buttonhooks');
 
 
-
 // Ajax Admin Section
 
-add_action( 'wp_ajax_wordlift_analyze', 'wordlift_ajax_analyze_action' );
+add_action('wp_ajax_wordlift_analyze', 'wordlift_ajax_analyze_action');
 
 // Analyze a text
-function wordlift_ajax_analyze_action() {
-	if ( ( current_user_can('edit_posts') || current_user_can('edit_pages') ) && get_user_option('rich_editing') ) {
-   
-	global $wpdb; // this is how you get access to the database
-/*
-    $api_key = 'XXXXX';
-    $api_analysis_chain = 'YYYY';
-    $api_url = "https://api.redlink.io/1.0-ALPHA/analysis/$api_analysis_chain/enhance?key=$api_key"
+function wordlift_ajax_analyze_action()
+{
+    if ((current_user_can('edit_posts') || current_user_can('edit_pages')) && get_user_option('rich_editing')) {
 
-	$response = wp_remote_post( $url, array(
-			'method' => 'POST',
-			'timeout' => 45,
-			'redirection' => 5,
-			'httpversion' => '1.0',
-			'blocking' => true,
-			'headers' => array(),
-			'body' => array( 'username' => 'bob', 'password' => '1234xyz' ),
-			'cookies' => array()
-    	)
-	);
+        global $wpdb; // this is how you get access to the database
+        /*
+            $api_key = 'XXXXX';
+            $api_analysis_chain = 'YYYY';
+            $api_url = "https://api.redlink.io/1.0-ALPHA/analysis/$api_analysis_chain/enhance?key=$api_key"
 
-if ( is_wp_error( $response ) ) {
-   $error_message = $response->get_error_message();
-   echo "Something went wrong: $error_message";
-} else {
-   echo 'Response:<pre>';
-   print_r( $response );
-   echo '</pre>';
-}
-*/
-    echo 'foo';
-	die(); // this is required to return a proper result
-	
-	
-}
+            $response = wp_remote_post( $url, array(
+                    'method' => 'POST',
+                    'timeout' => 45,
+                    'redirection' => 5,
+                    'httpversion' => '1.0',
+                    'blocking' => true,
+                    'headers' => array(),
+                    'body' => array( 'username' => 'bob', 'password' => '1234xyz' ),
+                    'cookies' => array()
+                )
+            );
+
+        if ( is_wp_error( $response ) ) {
+           $error_message = $response->get_error_message();
+           echo "Something went wrong: $error_message";
+        } else {
+           echo 'Response:<pre>';
+           print_r( $response );
+           echo '</pre>';
+        }
+        */
+        echo 'foo';
+        die(); // this is required to return a proper result
+
+
+    }
 }
