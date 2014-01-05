028d6ad6c695b2ff7ae7b87b83158839df601aa8
David Riccitelli <david@insideout.io>
2014-01-05 15:47:22 +0200

    move WordLift admin bar icon to the right and add submenu 'about WordLift' linking to WordLift Web Site

--- src/wordlift_admin_bar.php
+++ src/wordlift_admin_bar.php
@@ -13,11 +13,18 @@ function wordlift_admin_bar( $wp_admin_bar ){
         'href'   => false,
         'parent' => false, // false for a root menu, pass the ID value for a submenu of that menu.
         'id'     => 'wordlift', // defaults to a sanitized title value.
-        'title'  => ''
+        'title'  => '' // the title is replaced by an icon using stylesheets.
         // 'meta' => false // array of any of the following options: array( 'html' => '', 'class' => '', 'onclick' => '', 'target' => '', 'title' => '' );
     );
 
     $wp_admin_bar->add_menu($defaults);
+
+    $wp_admin_bar->add_menu( array(
+        'parent' => 'wordlift',
+        'id' => 'wordlift-about',
+        'title' => 'About WordLift',
+        'href' => 'http://wordlift.it'
+    ) );
 }
 
-add_action('admin_bar_menu', 'wordlift_admin_bar');
+add_action('admin_bar_menu', 'wordlift_admin_bar', 100);
