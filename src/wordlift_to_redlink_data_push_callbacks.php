b8701526f8b0796348390bb7b8dcd486cc6f5b6d
David Riccitelli <david@insideout.io>
2014-01-07 16:16:55 +0200

    clean up some code; solve issues with save_post looping; there is an issue with the sparql queries towards marmotta

--- src/wordlift_to_redlink_data_push_callbacks.php
+++ src/wordlift_to_redlink_data_push_callbacks.php
@@ -4,62 +4,91 @@ add_action('save_post', 'wordlift_on_post_save_callback');
 add_action('save_post', 'wordlift_update_entity_on_post_save_callback');
 
 /**
-
- **/
+ * Create triples about the entity being saved.
+ * @param int $post_id The post ID (posts that are not entities are ignored).
+ * @return null
+ */
 function wordlift_update_entity_on_post_save_callback($post_id) {
 
-	$post = get_post($post_id); 
+    // don't do anything if it's an autosave.
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return;
 
-	// If current post does not stay for an entity, nothing to do!
+    // get the post.
+    $post = get_post($post_id);
+
+	// don't do anything if this is an entity.
     if ('entity' != $post->post_type) {
         return true;
     } 
 
-    $title = addslashes($post->post_title);
-    $entity_url = get_post_meta( $post->ID, 'entity_url', true );
-    $entity_sameas = get_post_meta( $post->ID, 'entity_sameas', true );
-    $textual_description = addslashes(strip_tags($post->post_content));
-    $terms = wp_get_post_terms( $post->ID, 'entity_type' );
+    // get the entity label.
+    $title  = addslashes($post->post_title);
+    // get the entity URL.
+    $url    = get_post_meta( $post->ID, 'entity_url', true );
+    // get the entity 'same as' URLs.
+    $sameas = get_post_meta( $post->ID, 'entity_sameas', true );
+    // get the description.
+    $descr  = addslashes(strip_tags($post->post_content));
+    // get the types.
+    $terms  = wp_get_post_terms( $post->ID, 'entity_type' );
+    // get the post link.
+    $link   = get_permalink($post->ID);
 
-    $sparql  = "\n<{$entity_url}> rdfs:label '{$title}'."; 
+    // build the sparql query.
+    $sparql  = "<{$url}> rdfs:label '{$title}' ; ";
     foreach ($terms as $term) {
-    	$sparql .= "\n<{$entity_url}> a <http://schema.org/{$term->name}>."; 
-    } 
-    $sparql .= "\n<{$entity_url}> schema:url <".get_permalink($post->ID).">."; 
- 	$sparql .= "\n<{$entity_url}> schema:description '{$textual_description}'."; 
-    
-    $sparql_query = <<<EOT
-PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX schema: <http://schema.org/>
+    	$sparql .= " rdf:type <http://schema.org/{$term->name}> ; ";
+    }
+    $sparql .= "<http://schema.org/url> <$link> ; ";
+ 	$sparql .= "<http://schema.org/description> '{$descr}' . ";
 
-INSERT DATA
+    // get the ns prefixes.
+    $ns     = wordlift_get_ns_prefixes();
 
-{
-	$sparql
+    // because of a bug in Marmotta we cannot currently send one query DELETE/INSERT/WHERE.
+    // TODO: as soon as the bug is solved, use the DELETE/INSERT/WHERE query.
+    // create the insert/delete queries.
+    $insert = $ns . "INSERT DATA { $sparql }";
+    $delete = $ns . <<<EOF
+        DELETE WHERE {
+            <{$url}> rdfs:label ?label ;
+                schema:url ?url ;
+                schema:description ?description ;
+                a ?type
+        }
+EOF;
+
+	wordlift_push_data_triple_store($delete);
+	wordlift_push_data_triple_store($insert);
 }
-EOT;
 
-    $sparql_delete_query = <<<EOT
-PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX schema: <http://schema.org/>
-
-DELETE WHERE {
-    <{$entity_url}> ?a ?b
-}
-EOT;
-	wordlift_push_data_triple_store($sparql_delete_query);
-	wordlift_push_data_triple_store($sparql_query);
-}
 /**
+ * Get a string representing the NS prefixes for a SPARQL query.
+ * @return string The PREFIX lines.
+ */
+function wordlift_get_ns_prefixes() {
 
- **/
+    return <<<EOF
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX schema: <http://schema.org/>
+
+EOF;
+
+}
+
+/**
+ * @param $post_id
+ */
 function wordlift_on_post_save_callback($post_id) {
-   write_log("Going to update post with ID ".$post_id);
- 
+
+    // ignore autosaves
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return;
+
+    write_log("Going to update post with ID ".$post_id);
    
     $client_id = 353;
     $dataset_id = 'wordlift';
@@ -76,17 +105,26 @@ function wordlift_on_post_save_callback($post_id) {
     $doc->loadHTML($source);
     // Find all span tags: a span tag could be a textAnnotation
     $tags = $doc->getElementsByTagName('span');
+
     // Loops on founded span tags
     foreach ($tags as $tag) {
         // If itemid attribute is set, then the node is a textAnnotation
     	if ($tag->attributes->getNamedItem('itemid')) {
-    		
+
+            // the item id is the URL to the entity in DBpedia or Freebase.
+            $item_id   = $tag->attributes->getNamedItem('itemid')->value;
+            // the name is the final fragment of the URL.
+            $item_name = end(explode('/', $item_id));
+
             $entity_attributes = array(
                 'label' => addslashes($tag->nodeValue),
                 'sameas' => $tag->attributes->getNamedItem('itemid')->value,
-                'redlink_entity_url' => "http://data.redlink.io/$client_id/$dataset_id/resource/".end(explode('/', $tag->attributes->getNamedItem('itemid')->value)),                
-                );
-    		if($tag->attributes->getNamedItem('itemtype')) {
+                'redlink_entity_url' => "http://data.redlink.io/$client_id/$dataset_id/resource/$item_name",
+            );
+
+            // set the type only if available.
+    		if ($tag->attributes->getNamedItem('itemtype')) {
+
                 $entity_attributes['type'] = $tag->attributes->getNamedItem('itemtype')->value;
             }
              
@@ -104,33 +142,17 @@ function wordlift_on_post_save_callback($post_id) {
 
     }
 
-    $sparql_query = <<<EOT
-PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX schema: <http://schema.org/>
+    $ns     = wordlift_get_ns_prefixes();
+    $insert = $ns . "INSERT DATA { $sparql }";
+    $delete = $ns . "DELETE WHERE { <{$redlink_post_url}> dcterms:references ?ref }";
 
-INSERT DATA
-
-{
-	$sparql
-}
-EOT;
-
-    $sparql_delete_query = <<<EOT
-PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX schema: <http://schema.org/>
-
-DELETE WHERE {
-    <{$redlink_post_url}> dcterms:references ?ref
-}
-EOT;
-    wordlift_push_data_triple_store($sparql_delete_query);
-    wordlift_push_data_triple_store($sparql_query);
+    write_log('wordlift_on_post_save_callback(' . $post_id . ')/start: committing changes to Redlink');
+    wordlift_push_data_triple_store($delete);
+    wordlift_push_data_triple_store($insert);
+    write_log('wordlift_on_post_save_callback(' . $post_id . ')/end  : committing changes to Redlink');
 
 }
+
 function add_or_update_related_entity_post($attributes) {
     write_log($attributes);
     $params = array(
@@ -166,19 +188,28 @@ function add_or_update_related_entity_post($attributes) {
             'entity_type' => array( $toxonomized_type )
             );
     }
-    // Create or update the post
+
+    // remove the save post hook to avoid processing for entities being saved (see http://codex.wordpress.org/Plugin_API/Action_Reference/save_post).
+    remove_action('save_post', 'wordlift_on_post_save_callback');
+    remove_action('save_post', 'wordlift_update_entity_on_post_save_callback');
+
+    // create or update the post.
     $post_id = wp_insert_post($params, false);
-    // On success, add custom fields values
-    if ($post_id > 0) { 
-        update_post_meta( $post_id, 'entity_url', $attributes['redlink_entity_url'] );
+
+    // on success, add custom fields values
+    if ($post_id > 0) {
+        update_post_meta( $post_id, 'entity_url'   , $attributes['redlink_entity_url'] );
         update_post_meta( $post_id, 'entity_sameas', $attributes['sameas'] );
     }
 
-    return true; 
-         
+    // add back the save post hook.
+    add_action('save_post', 'wordlift_on_post_save_callback');
+    add_action('save_post', 'wordlift_update_entity_on_post_save_callback');
+
+    return true;
 }
 
-function wordlift_push_data_triple_store($sparql_query) {
+function wordlift_push_data_triple_store($query) {
 
     $api_key = '5VnRvvkRyWCN5IWUPhrH7ahXfGCBV8N0197dbccf';
     $api_analysis_chain = 'wordlift';
@@ -193,16 +224,22 @@ function wordlift_push_data_triple_store($sparql_query) {
             'headers' => array(
                 'Content-type' => 'application/sparql-update',
             ),
-            'body' => $sparql_query,
+            'body' => $query,
             'cookies' => array()
         )
     );
 
-    if ( is_wp_error( $response ) ) {
-        write_log("Something went wrong with sparql query\n\n$sparql_query\n\n$error_message");
-        return false;
-    } else {
-        write_log("Sparql query done!!\n\n{$sparql_query}");
-    }
+    write_log('== SPARQL QUERY ===============');
+    write_log($query);
+    write_log('===============================');
+    write_log(var_export($response, true));
+
+    // TODO: handle errors.
+//    if ( is_wp_error( $response ) ) {
+//        write_log("Something went wrong with sparql query\n\n$sparql_query\n\n$error_message");
+//        return false;
+//    } else {
+//        write_log("Sparql query done!!\n\n{$sparql_query}");
+//    }
     return true;
 }
