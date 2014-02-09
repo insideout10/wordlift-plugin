9b03266939b5de37befeea153d5919a5fe2dd964
David Riccitelli <david@insideout.io>
2014-02-09 12:28:08 +0200

    add tests

---
+++ src/tests/english.sh
@@ -0,0 +1,1 @@
+curl -X POST -d @english.txt -H "Content-type: text/plain" -H "Accept: application/rdf+xml" "https://api.redlink.io/1.0-ALPHA/analysis/wordlift/enhance?key=5VnRvvkRyWCN5IWUPhrH7ahXfGCBV8N0197dbccf" -o english.xml
