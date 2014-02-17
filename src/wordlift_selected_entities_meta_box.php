c63296d0d0de82755a4700501eb7d0d29f1e633d
David Riccitelli <david@insideout.io>
2014-02-17 11:34:27 +0200

    remove plugin header which was causing the plugin to be listed twice in the plugins list

--- src/wordlift_selected_entities_meta_box.php
+++ src/wordlift_selected_entities_meta_box.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-Plugin Name: WordLift
-Plugin URI: http://wordlift.it
-Description: Supercharge your WordPress Site with Smart Tagging and #Schemaorg support - a brand new way to write, organise and publish your contents to the Linked Data Cloud.
-Version: 3.0.0-SNAPSHOT
-Author: InSideOut10
-Author URI: http://www.insideout.io
-License: APL
-*/
-
 function wordlift_admin_add_selected_entities_meta_box($post_type) {
     add_meta_box(
         'wordlift_selected_entitities_box',
