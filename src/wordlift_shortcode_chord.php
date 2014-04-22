852bc8131ff23cfef58d2746f5630e6b5477eacb
pieroit <savastanos@libero.it>
2014-04-22 15:48:22 +0200

    first implementation of the recursive search for related entities

--- src/wordlift_shortcode_chord.php
+++ src/wordlift_shortcode_chord.php
@@ -1,5 +1,30 @@
 <?php
 
+function wl_ajax_related_entities($id, $related, $depth) {
+	
+	//get related entities for this entity
+	$rel = wl_get_related_entities($id);
+	array_merge( $rel, wl_get_related_post_ids($id) );
+	
+	foreach ($rel as $e) {
+		if( ! in_array($e, $related) ) {
+			//found new related entity
+			$related[] = $e;
+			
+			//end condition 1: obtained enough related entities
+			if ( sizeof($related) >= $depth ){
+				return $related;
+			} else {
+				//recursive call
+				$related += wl_ajax_related_entities($e, $related, $depth);
+			}
+		}
+	}
+	
+	//end condition 2: no more entities to search for
+	return $related;
+}
+
 //set up widget
 function wordlift_chord_widget_func($atts){
 	
@@ -14,9 +39,16 @@ function wordlift_chord_widget_func($atts){
 								'main_color' =>	'blue'
 							), $atts));
 	
-	
 	//returning html tamplate
 	ob_start();
+	$post_id = get_the_ID();
+	$result = wl_ajax_related_entities($post_id, array(), 100);
+	print_r($result);
+	foreach($result as $r){
+		$p = get_post($r);
+		echo("<br>" . $p->post_title);
+	}
+	
     include('wordlift_shortcode_chord_template.php');
     return ob_get_clean();	
 }
