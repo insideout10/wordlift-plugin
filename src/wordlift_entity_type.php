7620541af4a4fd6b2048c2f028e8c8c13e8a3f9e
savastanos@libero.it <5dz5dz5dz>
2014-07-07 13:38:36 +0200

    change 'Entities' in 'Vocabulary'. Deleted 'category' menu from wp-admin

--- src/wordlift_entity_type.php
+++ src/wordlift_entity_type.php
@@ -7,29 +7,29 @@ function wl_entity_type_register()
 {
 
     $labels = array(
-        'name' => _x('Entities', 'post type general name', 'wordlift'),
+        'name' => _x('Vocabulary', 'post type general name', 'wordlift'),
         'singular_name' => _x('Entity', 'post type singular name', 'wordlift'),
-        'add_new' => _x('Add New', 'entity', 'wordlift'),
+        'add_new' => _x('Add New Entity', 'entity', 'wordlift'),
         'add_new_item' => __('Add New Entity', 'wordlift'),
         'edit_item' => __('Edit Entity', 'wordlift'),
         'new_item' => __('New Entity', 'wordlift'),
         'all_items' => __('All Entities', 'wordlift'),
         'view_item' => __('View Entity', 'wordlift'),
-        'search_items' => __('Search Entities', 'wordlift'),
+        'search_items' => __('Search in Vocabulary', 'wordlift'),
         'not_found' => __('No entities found', 'wordlift'),
         'not_found_in_trash' => __('No entities found in the Trash', 'wordlift'),
         'parent_item_colon' => '',
-        'menu_name' => 'Entities'
+        'menu_name' => __('Vocabulary', 'wordlift')
     );
 
     $args = array(
         'labels' => $labels,
-        'description' => 'Holds our entities and entity specific data',
+        'description' => 'Holds our vocabulary (set of entities) and entity specific data',
         'public' => true,
         'menu_position' => 20, // after the pages menu.
         'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'comments'),
-        'has_archive' => true,
-        'taxonomies' => array('category')
+        'has_archive' => true/*,
+        'taxonomies' => array('category')*/
     );
 
     register_post_type( WL_ENTITY_TYPE_NAME, $args);
--- src/wordlift_entity_type_taxonomy.php
+++ src/wordlift_entity_type_taxonomy.php
@@ -27,7 +27,8 @@ function wl_entity_type_taxonomy_register()
 
     $args = array(
         'labels' => $labels,
-        'hierarchical' => false
+        'hierarchical' => false,
+        'show_admin_column' => true
     );
 
     register_taxonomy(WL_ENTITY_TYPE_TAXONOMY_NAME, 'entity', $args);
