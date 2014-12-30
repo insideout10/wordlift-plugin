94bb2a28177ad3ef1edce20c8007fe8259ae8461
David Riccitelli <david@insideout.io>
2014-12-30 10:04:21 +0200

    reformat file

--- src/admin/wordlift_admin.php
+++ src/admin/wordlift_admin.php
@@ -8,45 +8,53 @@ require_once( 'wordlift_admin_menu.php' );
 
 /**
  * Serialize an entity post.
+ *
  * @param array $entity The entity post.
+ *
  * @return array mixed The entity data array.
  */
-function wl_serialize_entity($entity){
-        
-        $type = wl_entity_get_type( $entity->ID );
-        $images = wl_get_image_urls( $entity->ID );
-        
-        return array(
-            'id'            =>      wl_get_entity_uri( $entity->ID ),
-            'label'         =>      $entity->post_title,
-            'sameAs'        =>      wl_get_same_as( $entity->ID ),
-            'type'          =>      $type['uri'],
-            'css'           =>      $type['css_class'],
-            'types'         =>      wl_get_entity_types( $entity->ID ),
-            'thumbnail'     =>      ( isset( $images[0] ) ? $images[0] : null ),
-            'thumbnails'    =>      $images,
-            'source'        =>      'wordlift',
-            'sources'       =>      array('wordlift')            
-        );
+function wl_serialize_entity( $entity ) {
+
+	$type   = wl_entity_get_type( $entity->ID );
+	$images = wl_get_image_urls( $entity->ID );
+
+	return array(
+		'id'         => wl_get_entity_uri( $entity->ID ),
+		'label'      => $entity->post_title,
+		'sameAs'     => wl_get_same_as( $entity->ID ),
+		'type'       => $type['uri'],
+		'css'        => $type['css_class'],
+		'types'      => wl_get_entity_types( $entity->ID ),
+		'thumbnail'  => ( isset( $images[0] ) ? $images[0] : null ),
+		'thumbnails' => $images,
+		'source'     => 'wordlift',
+		'sources'    => array( 'wordlift' )
+	);
 }
+
 /**
  * Removes empty text annotations from the post content.
+ *
+ * @since 1.0.0
+ *
  * @param array $data The post data.
+ *
  * @return array mixed The post data array.
  */
-function wl_remove_text_annotations($data) {
+function wl_remove_text_annotations( $data ) {
 
-    wl_write_log("wl_remove_text_annotations [ data content :: ${data['post_content']} ]");
+	//    <span class="textannotation" id="urn:enhancement-777cbed4-b131-00fb-54a4-ed9b26ae57ea">
+	//    $pattern = '/<span class=\\\"textannotation\\\" id=\\\"[^\"]+\\\">([^<]+)<\/span>/i';
+	$pattern = '/<(\w+)[^>]*\sclass=\\\"textannotation\\\"[^>]*>([^<]+)<\/\1>/im';
 
-    //    <span class="textannotation" id="urn:enhancement-777cbed4-b131-00fb-54a4-ed9b26ae57ea">
-    //    $pattern = '/<span class=\\\"textannotation\\\" id=\\\"[^\"]+\\\">([^<]+)<\/span>/i';
-    $pattern = '/<(\w+)[^>]*\sclass=\\\"textannotation\\\"[^>]*>([^<]+)<\/\1>/im';
+	wl_write_log( "Removing text annotations [ pattern :: $pattern ][ data content :: ${data['post_content']} ]" );
 
-    // Remove the pattern while it is found (match nested annotations).
-    while (1 === preg_match($pattern, $data['post_content'])) {
-        $data['post_content'] = preg_replace($pattern, '$2', $data['post_content'], -1, $count);
-    }
+	// Remove the pattern while it is found (match nested annotations).
+	while ( 1 === preg_match( $pattern, $data['post_content'] ) ) {
+		$data['post_content'] = preg_replace( $pattern, '$2', $data['post_content'], - 1, $count );
+	}
 
-    return $data;
+	return $data;
 }
-add_filter('wp_insert_post_data', 'wl_remove_text_annotations', '98', 1);
+
+add_filter( 'wp_insert_post_data', 'wl_remove_text_annotations', '98', 1 );
