841670cfc95329ece334462ebe0eb1f19f2f9ab7
Marcello Colacino <marcello.colacino@gmail.com>
2015-02-07 00:17:42 +0100

    Force ssl v3 on travis

--- tests/functions.php
+++ tests/functions.php
@@ -14,6 +14,13 @@ define( 'WL_JSON_LD_CONTEXT', serialize( array(
 // Disable buffering.
 define( 'WL_BUFFER_SPARQL_UPDATE_QUERIES', false );
 
+if ( 'true' === getenv( 'WL_SSL_V3_FORCED' )) { 
+	add_action( 'http_api_curl', 'force_curl_ssl_v3' );
+	function force_curl_ssl_v3( $handle ) {
+    	curl_setopt($handle, CURLOPT_SSLVERSION, 3);
+	}
+}
+
 require_once( 'jsonld.php' );
 
 /**
