14b4342afb4be2ccc777532a322e6ea92912c0e5
pieroit <savastanos@libero.it>
2015-07-28 12:12:35 +0200

    fix an undefined notice in wl_save_entities

--- src/modules/linked_data/wordlift_linked_data.php
+++ src/modules/linked_data/wordlift_linked_data.php
@@ -70,14 +70,14 @@ function wl_linked_data_save_post_and_related_entities( $post_id ) {
             // Save each entity and store the post id.
             foreach ( $entities_via_post as $index => $entity ) {
 
-                    //if ( preg_match( '/^local-entity-.+/', $entity['uri'] ) > 0 ) {
+                    if ( preg_match( '/^local-entity-.+/', $entity['uri'] ) > 0 ) {
                             // Build the proper uri 
                             $uri = sprintf( '%s/%s/%s', wl_configuration_get_redlink_dataset_uri(), 'entity', wl_sanitize_uri_path( $entity['label'] ) );
                             // Populate the mapping
                             $entities_uri_mapping[ $entity['uri'] ] = $uri;
                             // Override the entity obj
                             $entities_via_post[ $index ]['uri'] = $uri;
-                    //}
+                    }
             }
 
             // Populate the $entities_predicates_mapping
@@ -187,7 +187,9 @@ function wl_save_entities( $entities, $related_post_id = null ) {
 		$main_type_uri = $entity['main_type'];
 
 		// the preferred type.
-		$type_uris = $entity['type'];
+		$type_uris = ( isset( $entity['type'] ) ?
+                                $entity['type'] :
+                                array() );
 
 		$description = $entity['description'];
 		$images      = ( isset( $entity['image'] ) ?
