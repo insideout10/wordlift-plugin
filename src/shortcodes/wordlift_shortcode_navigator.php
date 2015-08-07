0e46aa050e64fdb938f2ff087b351a77c97ef1ee
pieroit <savastanos@libero.it>
2015-08-07 12:43:04 +0200

    fix chord and navigator to only show published content

--- src/shortcodes/wordlift_shortcode_navigator.php
+++ src/shortcodes/wordlift_shortcode_navigator.php
@@ -10,57 +10,56 @@ function wordlift_register_shortcode_navigator() {
  * 
  * @param int $post_id Id of the post.
  * 
- * @return Array List of posts organized in this way:
- *      Array[0] = Array( id_of_recommended_post_from_the_same_category, null )
- *      Array[n] = Array( id_of_other_post_recommended_from_entity, id_of_that_entity )
+ * @return Array List of tuples organized in this way:
+ *      Array(
+ *          [0] => Array(
+ *              [0] => related post Object
+ *              [1] => related entity Object (main post and the post above are related via this entity)
+ *          )
+ *          [1] => another tuple
+            [2] => ...
+ *      )
  */
 function wordlift_shortcode_navigator_populate( $post_id ) {
-    
-    // add as first the most recent article in the same iptc category
-    // $most_recent_related_post = wl_iptc_get_most_recent_post_in_same_category( $post_id );
 
     // prepare structures to memorize other related posts
     $related_posts_ids = array();
     $related_posts = array();
-    
-    /*
-    if( isset( $most_recent_related_post->ID ) ) {
-        $related_posts_ids[] = $most_recent_related_post->ID;
-        $related_posts[] = array( $most_recent_related_post->ID, null );
-    }
-    */
 
     // get the related entities, ordered by WHO-WHAT-WHERE-WHEN (as established the 29/7/2015 12:45 in the grottino)
-    // TODO: shoud be a single query
-    $related_entities = wl_core_get_related_entity_ids( $post_id, array(
-                'predicate' => WL_WHO_RELATION
+    // TODO: should be a single query
+    $related_entities = wl_core_get_related_entities( $post_id, array(
+                'predicate' => WL_WHO_RELATION,
+                'status'    => 'publish'
     ));
-    $related_entities = array_merge( $related_entities, wl_core_get_related_entity_ids( $post_id, array(
-                'predicate' => WL_WHAT_RELATION
+    $related_entities = array_merge( $related_entities, wl_core_get_related_entities( $post_id, array(
+                'predicate' => WL_WHAT_RELATION,
+                'status'    => 'publish'
     )));
-    $related_entities = array_merge( $related_entities, wl_core_get_related_entity_ids( $post_id, array(
-                'predicate' => WL_WHERE_RELATION
+    $related_entities = array_merge( $related_entities, wl_core_get_related_entities( $post_id, array(
+                'predicate' => WL_WHERE_RELATION,
+                'status'    => 'publish'
     )));
-    $related_entities = array_merge( $related_entities, wl_core_get_related_entity_ids( $post_id, array(
-                'predicate' => WL_WHEN_RELATION
+    $related_entities = array_merge( $related_entities, wl_core_get_related_entities( $post_id, array(
+                'predicate' => WL_WHEN_RELATION,
+                'status'    => 'publish'
     )));
-    
-    wl_write_log("Entities related to post $post_id");
-    wl_write_log( $related_entities );
     
     foreach ( $related_entities as $rel_entity ) {
         
-        wl_write_log("Looking for posts related to entity $rel_entity");
+        wl_write_log("Looking for posts related to entity $rel_entity->ID");
        
         // take the id of posts referencing the entity
-        $referencing_posts_ids = wl_core_get_related_post_ids( $rel_entity );
-        wl_write_log($referencing_posts_ids);
+        $referencing_posts = wl_core_get_related_posts( $rel_entity->ID, array(
+            'status' => 'publish'
+        ) );
         
         // loop over them and take the first one which is not already in the $related_posts
-        foreach ( $referencing_posts_ids as $referencing_post_id ) {
-            if( !in_array( $referencing_post_id, $related_posts_ids ) && $referencing_post_id != $post_id ) {
-                $related_posts_ids[] = $referencing_post_id;
-                $related_posts[] = array( $referencing_post_id, $rel_entity );
+        foreach ( $referencing_posts as $referencing_post ) {
+            
+            if( !in_array( $referencing_post->ID, $related_posts_ids ) && $referencing_post->ID != $post_id ) {
+                $related_posts_ids[] = $referencing_post->ID;
+                $related_posts[] = array( $referencing_post, $rel_entity );
             }
         }
     }
@@ -99,6 +98,7 @@ function wordlift_shortcode_navigator() {
     
     
     // get posts that will populate the navigator (criteria may vary, see function *wordlift_shortcode_navigator_populate*)
+    // The result array will contains tuples (post_object, entity_object)
     $related_posts_and_entities = wordlift_shortcode_navigator_populate( get_the_ID() );
     
     // build the HTML
@@ -107,16 +107,16 @@ function wordlift_shortcode_navigator() {
     
     foreach ( $related_posts_and_entities as $related_post_entity ) {
         
-        $related_post_id = $related_post_entity[0];
-        $related_post = get_post( $related_post_id );
+        $related_post = $related_post_entity[0];
+        $related_entity = $related_post_entity[1];
         
-        $thumb = wl_get_the_post_thumbnail_src( get_the_post_thumbnail( $related_post_id, 'medium' ) );
+        $thumb = wl_get_the_post_thumbnail_src( get_the_post_thumbnail( $related_post->ID, 'medium' ) );
         if( empty( $thumb ) ) {
             $thumb = WL_DEFAULT_THUMBNAIL_PATH;
         }
 
-        $context_link = get_permalink( $related_post_entity[1] );
-        $context_name = get_post( $related_post_entity[1] )->post_title;
+        $context_link = get_permalink( $related_entity->ID );
+        $context_name = $related_entity->post_title;
 
         $counter+=1;
         
@@ -124,7 +124,7 @@ function wordlift_shortcode_navigator() {
         $content .= '<div class="wl-navigator-card">
             <div class="wl-navigator-lens" style="background-image:url(' . $thumb . ')">
                 <span class="wl-navigator-trigger">
-                    <a href="' . get_permalink( $related_post_id ) . '">' . $related_post->post_title . '</a>
+                    <a href="' . get_permalink( $related_post->ID ) . '">' . $related_post->post_title . '</a>
                 </span>
             </div>
             <div class="wl-navigator-context">
