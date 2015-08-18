3bf2acd4c7f3d320478fdadd567aa10db1542ded
David Riccitelli <david@insideout.io>
2015-08-18 17:50:12 +0200

    see #46: add a method to hide the WordLift Key from a text

--- src/modules/core/wordlift_core_functions.php
+++ src/modules/core/wordlift_core_functions.php
@@ -5,8 +5,23 @@
  * Ensure output buffer is cleaned
  */
 function wl_core_send_json( $response ) {
-	if ( ob_get_contents() )  {
+	if ( ob_get_contents() ) {
 		ob_clean();
 	}
+
 	return wp_send_json( $response );
+}
+
+/**
+ * Hide the WordLift Key from the provided text.
+ *
+ * @since 3.0.0
+ *
+ * @param $text string A text that may potentially contain a WL key.
+ *
+ * @return string A text with the key hidden.
+ */
+function wl_core_hide_key( $text ) {
+
+	return str_ireplace( wl_configuration_get_key(), '<hidden>', $text );
 }
