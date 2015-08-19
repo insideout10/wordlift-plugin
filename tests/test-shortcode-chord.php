1e9593ec5037558e18de7451eca9ed55725ddd81
pieroit <savastanos@libero.it>
2015-08-19 10:49:17 +0200

    renew chord unit tests

--- tests/test-shortcode-chord.php
+++ tests/test-shortcode-chord.php
@@ -21,79 +21,25 @@ class ChordShortcodeTest extends WP_UnitTestCase
 
         // Empty the blog.
         wl_empty_blog();
-
-        // Check that entities and posts have been deleted.
-        $this->assertEquals( 0, count( get_posts( array(
-            'posts_per_page' => -1,
-            'post_type'      => 'post',
-            'post_status'    => 'any'
-        ) ) ) );
-        $this->assertEquals( 0, count( get_posts( array(
-            'posts_per_page' => -1,
-            'post_type'      => 'entity',
-            'post_status'    => 'any'
-        ) ) ) );
-		
 		
         // Creating 2 fake entities 
-        $entities = array();
-        $ent = wl_save_entity( array(
-            'uri'               => 'http://example.org/entity1',
-            'label'             => 'Entity1',
-            'main_type_uri'     => 'http://schema.org/Thing',
-            'description'       => 'An example entity.',
-            'type_uris'         => array(),
-            'related_post_id'   => null,
-            'images'            => array(),
-            'same_as'           => array()
-        ));
-	$entities[] = $ent->ID;
-        
-        $ent = wl_save_entity( array(
-            'uri'               => 'http://example.org/entity2',
-            'label'             => 'Entity2',
-            'main_type_uri'     => 'http://schema.org/Thing',
-            'description'       => 'An example entity.',
-            'type_uris'         => array(),
-            'related_post_id'   => null,
-            'images'            => array(),
-            'same_as'           => array()
-        ));
-	$entities[] = $ent->ID;
-                
-        
+        $entities = array(
+            wl_create_post( 'content', 'entity1', 'title1', 'publish', 'entity' ),
+            wl_create_post( 'content', 'entity2', 'title2', 'publish', 'entity' )
+        );
+                     
         // Creating a fake post
-        $content = 'This is a fake post. Ohhh yeah';
-        $slug = 'yeah';
-        $title = 'Yeah';
-		$status = 'publish';
-		$type = 'post';
-		self::$FIRST_POST_ID = wl_create_post( $content, $slug, $title, $status, $type);
+        self::$FIRST_POST_ID = wl_create_post( 'content', 'post1', 'title1', 'publish', 'post' );
         
         wl_core_add_relation_instances( self::$FIRST_POST_ID, WL_WHAT_RELATION, $entities );
 		
 		// Creating another fake post and entity (the most connected one)
 		
 	// Creating a fake post
-        $content = 'This is another fake post. Ohhh yeah';
-        $slug = 'yeah';
-        $title = 'Yeah';
-		$status = 'publish';
-		$type = 'post';
-		$new_post = wl_create_post( $content, $slug, $title, $status, $type);
-		
-        $ent = wl_save_entity( array(
-            'uri'               => 'http://example.org/entity3',
-            'label'             => 'Entity3',
-            'main_type_uri'     => 'http://schema.org/Thing',
-            'description'       => 'Another example entity only related to an entity.',
-            'type_uris'         => array(),
-            'related_post_id'   => null,
-            'images'            => array(),
-            'same_as'           => array()
-        ));    
-           
-        self::$MOST_CONNECTED_ENTITY_ID = $ent->ID;
+        $new_post = wl_create_post( 'content', 'post2', 'title2', 'publish', 'post' );   
+        
+        // Create the most connected entity
+        self::$MOST_CONNECTED_ENTITY_ID = wl_create_post( 'content', 'entity2', 'title2', 'publish', 'entity' );
 
         wl_core_add_relation_instance( $new_post, WL_WHAT_RELATION, self::$MOST_CONNECTED_ENTITY_ID );
         wl_core_add_relation_instance( self::$FIRST_POST_ID, WL_WHAT_RELATION, self::$MOST_CONNECTED_ENTITY_ID );
