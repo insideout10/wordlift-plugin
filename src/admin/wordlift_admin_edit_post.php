f49fad16b326d55b6326b2682513ba789bf29c48
pieroit <savastanos@libero.it>
2015-08-20 09:58:55 +0200

    'view on redlink' and 'test google rich snippets' buttons open in a new tab

--- src/admin/wordlift_admin_edit_post.php
+++ src/admin/wordlift_admin_edit_post.php
@@ -19,12 +19,12 @@ function wl_admin_permalink_html( $html, $post_id, $new_title, $new_slug ) {
     // If the post is published, add the button view on Redlink.
     if ( 'publish' == get_post_status( $post_id ) ) {
         $uri_esc  =  esc_attr( wl_get_entity_uri( $post_id ) );
-        $html     .= "<span id='view-post-btn'><a href='$uri_esc' class='button button-small'>" .
+        $html     .= "<span id='view-post-btn'><a href='$uri_esc' class='button button-small' target='_blank'>" .
             __('View on Redlink', 'wordlift') .
             "</a></span>\n";
         $html     .= "<span id='view-post-btn'><a href='" . WL_CONFIG_TEST_GOOGLE_RICH_SNIPPETS_URL .
             urlencode( get_permalink( $post_id ) ) .
-            "' class='button button-small'>" .
+            "' class='button button-small' target='_blank'>" .
             __('Test Google Rich Snippets', 'wordlift') .
             "</a></span>\n";
     }
