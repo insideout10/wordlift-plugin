f4e040fb90ea60b75350fd2f35a86716a23e0c65
pieroit <savastanos@libero.it>
2015-10-05 18:45:53 +0200

    3.0.8

--- src/wordlift.php
+++ src/wordlift.php
@@ -3,7 +3,7 @@
 Plugin Name: WordLift
 Plugin URI: http://wordlift.it
 Description: Supercharge your WordPress Site with Smart Tagging and #Schemaorg support - a brand new way to write, organise and publish your contents to the Linked Data Cloud.
-Version: 3.0.7
+Version: 3.0.8
 Author: InsideOut10
 Author URI: http://www.insideout.io
 License: APL
