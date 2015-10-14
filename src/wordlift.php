2371dfec34c599c267656093b981a755db0f7c28
David Riccitelli <david@insideout.io>
2015-10-14 20:00:59 +0300

    fix merge tags

--- src/wordlift.php
+++ src/wordlift.php
@@ -3,11 +3,7 @@
 Plugin Name: WordLift
 Plugin URI: http://wordlift.it
 Description: Supercharge your WordPress Site with Smart Tagging and #Schemaorg support - a brand new way to write, organise and publish your contents to the Linked Data Cloud.
-<<<<<<< HEAD
 Version: 3.1.1-dev
-=======
-Version: 3.0.10
->>>>>>> master
 Author: InsideOut10
 Author URI: http://www.insideout.io
 License: APL
