ead268a36d63364b6e3fb0f1adfc47fc3eac3156
pieroit <savastanos@libero.it>
2015-10-14 16:29:12 +0200

    test setup for the metaboxes

--- tests/test-metaboxes.php
+++ tests/test-metaboxes.php
@@ -28,33 +28,40 @@ class MetaboxTest extends WP_UnitTestCase
      * Test the WL_Metabox obj is built properly
      */
     function testWL_Metabox_constructor() {
-        
-        $args = array(
-            // TODO
-        );
-        
+       
         $metabox = new WL_Metabox();
+        
+        // Verify the object has been built (verify hooks, mainly)
     }
     
     /*
-     * Test the WL_Metabox obj print correctly html
+     * Test the WL_Metabox fields are built properly
      */
-    function testWL_Metabox_html() {
+    function testWL_Metabox_field_instantiation() {
+       
+        $metabox = new WL_Metabox();
         
+        $$entity_id = 23; // TODO: create entity
+        
+        $metabox->instantiate_fields( $entity_id );
+        // Verify the correct fields have been built.
     }
     
+    
     /*
-     * Test the WL_Metabox loads and saves data correcly
+     * Test Fileds. The following are about the base class WL_Metabox_Filed.
      */
-    function testWL_Metabox_data() {
-        
-    }
+    
     
     /*
      * Test the WL_Metabox_Field obj is built properly
      */
     function testWL_Metabox_Field_constructor() {
-        
+        $args = array(
+            // TODO
+        );
+        $field = new WL_Metabox_Field( $args );
+        // Verify Field has been built correctly
     }
     
     /*
@@ -62,12 +69,44 @@ class MetaboxTest extends WP_UnitTestCase
      */
     function testWL_Metabox_Field_html() {
         
+        // TODO: insert data into DB (also invalid data)
+        
+        $args = array(
+            // TODO
+        );
+        $field = new WL_Metabox_Field( $args );
+        
+        // TODO: load data from DB
+        
+        // verify html methods
+        $field->html_wrapper_open();
+        $field->html();
+        $field->html_input( 'aaaah' );
+        $field->html_wrapper_close();
     }
     
     /*
-     * Test the WL_Metabox_Field loads and saves data correcly
+     * Test the WL_Metabox_Field loads data correcly
      */
     function testWL_Metabox_Field_data() {
         
+        // TODO: insert data into DB (also invalid data)
+        
+        $args = array(
+            // TODO
+        );
+        $field = new WL_Metabox_Field( $args );
+        
+        $field->get_data();
+        // Verify data is loaded correctly from DB
+        
+        $field->sanitize_data( array('', 12, 'aaaaioio') );
+        // Verify only valid data pass
+        
+        $field->sanitize_data_filter( '' );
+        // Verify data filter
+        
+        $field->save_data();
+        // Verify new DB values
     }
 }
