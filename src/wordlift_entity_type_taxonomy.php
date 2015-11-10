f25334314dbd3f2a204c7ad0fdd00b59bf1b6262
David Riccitelli <david@insideout.io>
2015-11-10 14:28:23 +0200

    see #155: remove excessive logging

--- src/wordlift_entity_type_taxonomy.php
+++ src/wordlift_entity_type_taxonomy.php
@@ -108,9 +108,7 @@ function wl_entity_type_taxonomy_get_type( $post_id ) {
 function wl_entity_type_taxonomy_get_term_options( $term_id ) {
 
 	$term = get_option( WL_ENTITY_TYPE_TAXONOMY_NAME . "_$term_id" );
-
-	// wl_write_log( "wl_entity_type_taxonomy_get_term_options [ term :: " . var_export( $term , true ) . " ]" );
-
+	
 	return $term;
 }
 
