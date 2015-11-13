e777e379d7be9412cb117dc8cd487defff757c8f
David Riccitelli <david@insideout.io>
2015-11-13 18:36:29 +0200

    remove src folder; update push script

--- push-to-wordpress-org.sh
+++ push-to-wordpress-org.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-FILE='wordlift/wordlift.php'
+FILE='src/wordlift/wordlift.php'
 README='trunk/readme.txt'
 
 echo "Checking out and updating the svn branch..."
 git checkout -b svn >/dev/null 2>&1
 git pull origin svn >/dev/null 2>&1
-echo "Removing make-zip.sh..."
-rm -fr make-zip.sh
+# echo "Removing make-zip.sh..."
+# rm -fr make-zip.sh
 echo "Updating the svn branch..."
 svn up
-echo "Checking make-zip.sh from master branch..."
-git checkout master -- make-zip.sh
-echo "Remove dist and wordlift folders..."
-rm -fr dist wordlift
-echo "Making wordlift.zip..."
-./make-zip.sh master > /dev/null
-echo "Unzipping to wordlift/..."
-unzip dist/wordlift-*.zip > /dev/null
-echo "Remove dist folder..."
-rm -fr dist
+
+# echo "Checking make-zip.sh from master branch..."
+# git checkout master -- make-zip.sh
+# echo "Remove dist and wordlift folders..."
+# rm -fr dist wordlift
+# echo "Making wordlift.zip..."
+# ./make-zip.sh master > /dev/null
+# echo "Unzipping to wordlift/..."
+# unzip dist/wordlift-*.zip > /dev/null
+# echo "Remove dist folder..."
+# rm -fr dist
 
 VERSION=`egrep -o "Version:\s+\d+\.\d+\.\d+" $FILE | egrep -o "\d+\.\d+\.\d+"`
 
@@ -28,6 +29,9 @@ echo $VERSION
 if [[ -z "$VERSION" ]]; then
 	echo "Version not set, halting."
 else
+    echo "Checking out updated src..."
+    git checkout master -- src
+
 	echo "Removing tag $VERSION..."
 	svn rm --force tags/$VERSION > /dev/null
 	echo "Removing trunk..."
@@ -35,7 +39,7 @@ else
 
 	echo "If you see 'forbidden by the server', you need to authenticate to the server first."
 	svn ci -m "$VERSION: updating trunk (1 of 2)"
-	mv wordlift trunk
+	mv src trunk
 	echo "Setting the stable tag in $README..."
 	sed -i '' "s/Stable tag: .*/Stable tag: $VERSION/g" $README
 	svn add trunk > /dev/null
@@ -43,6 +47,9 @@ else
 
 	echo "If you see 'forbidden by the server', you need to authenticate to the server first."
 	svn ci -m "$VERSION: updating trunk (2 of 2)"
+
+    echo "Removing src..."
+    rm -fr src
 
 	git add -A
 	git commit -m "bump to $VERSION" -a
