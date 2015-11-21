e9e212ad0826b7a3ff6e6ec8a06f0b08d9d2d235
David Riccitelli <david@insideout.io>
2015-11-21 09:56:10 +0200

    bump to 3.1.6

---
+++ tags/3.1.6/includes/class-wordlift-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://wordlift.it
+ * @since      1.0.0
+ *
+ * @package    Wordlift
+ * @subpackage Wordlift/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wordlift
+ * @subpackage Wordlift/includes
+ * @author     WordLift <hello@wordlift.it>
+ */
+class Wordlift_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
