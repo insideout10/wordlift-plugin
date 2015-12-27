907f0d14d130aaefd930d086f1405c1fd05b1098
Marcello Colacino <marcello.colacino@gmail.com>
2015-12-27 01:34:23 +0100

    fix metaboxes unit tests

--- tests/test-metaboxes.php
+++ tests/test-metaboxes.php
@@ -204,8 +204,8 @@ class MetaboxTest extends WP_UnitTestCase {
 		$this->assertContains( "data-cardinality='INF'", $html );                                   // Cardinality
 		$this->assertContains( "data-expected-types='Person,Organization'", $html );                // Expected types
 		$this->assertContains( 'name="wl_metaboxes[wl_author][]"', $html );                         // $_POST array
-		$this->assertContains( 'button class="button wl-remove-input"', $html );                           // Remove button
-		$this->assertContains( 'button class="button wl-add-input"', $html );                              // Add button
+		$this->assertContains( 'button class="button wl-remove-input wl-button"', $html );                           // Remove button
+		$this->assertContains( 'button class="button wl-add-input wl-button"', $html );                              // Add button
 	}
 
 	/*
