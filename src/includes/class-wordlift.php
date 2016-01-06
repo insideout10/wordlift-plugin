c34deb807711a485dce08579210c9d89d67bb946
Marcello Colacino <marcello.colacino@gmail.com>
2016-01-06 12:49:23 +0100

    Merge 3.3.o on 3.4.0-dev

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -177,7 +177,7 @@ class Wordlift {
 	public function __construct() {
 
 		$this->plugin_name = 'wordlift';
-		$this->version = '3.3.0';
+		$this->version = '3.4.0-dev';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
