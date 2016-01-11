21ced8062e1319f200f5c4a538e1bdc61e2f07a9
Marcello Colacino <marcello.colacino@gmail.com>
2016-01-11 17:06:20 +0100

    Add method to count all annotated published posts

--- src/admin/class-wordlift-admin-dashboard.php
+++ src/admin/class-wordlift-admin-dashboard.php
@@ -43,11 +43,32 @@ class Wordlift_Dashboard_Service {
 	 * @uses https://codex.wordpress.org/it:Riferimento_funzioni/wp_count_posts
 	 * @since 3.4.0
 	 *
-	 * @return int Total number of posts.
+	 * @return int Total number of published posts.
 	 */
 	public function count_posts( ) {
 		
 		return wp_count_posts()->publish;		
+	}
+
+	/**
+	 * Calculate total number of annotated published posts
+	 * @since 3.4.0
+	 *
+	 * @return int Total number of annotated published posts.
+	 */
+	public function count_annotated_posts( ) {
+		
+		// Prepare interaction with db
+    	global $wpdb;
+    	// Retrieve Wordlift relation instances table name
+    	$table_name = wl_core_get_relation_instances_table_name();
+    	// Calculate sql statement
+		$sql_statement = <<<EOF
+    		SELECT COUNT(p.*) FROM $wpdb->posts as p JOIN $table_name as r ON p.id = r.subject_id AND p.post_type = 'posts' AND p.post_status = 'publish';
+EOF;
+		// Perform the query
+		return $wpdb->get_var( $sql_statement ); 
+		
 	}
 
 	/**
