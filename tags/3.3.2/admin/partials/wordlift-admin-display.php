899a6dd1eb90bac4116eef49fdc0f51bf396eb1b
Marcello Colacino <marcello.colacino@gmail.com>
2016-01-14 14:38:39 +0100

    bump to 3.3.2

---
+++ tags/3.3.2/admin/partials/wordlift-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://wordlift.it
+ * @since      1.0.0
+ *
+ * @package    Wordlift
+ * @subpackage Wordlift/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
