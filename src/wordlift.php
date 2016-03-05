7a4acc94d2537a7054910f60400b4c7cccd13fd6
Marcello Colacino <marcello.colacino@gmail.com>
2016-03-05 15:52:40 +0100

    Add angularjs-geolocation dependency for disambiguation widget

--- src/wordlift.php
+++ src/wordlift.php
@@ -217,6 +217,7 @@ function wordlift_admin_enqueue_scripts() {
 
 	wp_enqueue_script( 'jquery-ui-autocomplete' );
 	wp_enqueue_script( 'angularjs', 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.11/angular.min.js' );
+	wp_enqueue_script( 'angularjs-geolocation', dirname( plugin_dir_url( __FILE__ ) ) . '/bower_components/angularjs-geolocation/dist/angularjs-geolocation.min.js' );
 
 	// Disable auto-save for custom entity posts only
 	if( Wordlift_Entity_Service::TYPE_NAME === get_post_type() ){
