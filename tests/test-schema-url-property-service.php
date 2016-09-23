78ac85330c1e80ab740c26b1f13781b87a7ba80c
David Riccitelli <david@insideout.io>
2016-09-23 22:39:42 +0300

    see #346: add some unit tests

--- tests/test-schema-url-property-service.php
+++ tests/test-schema-url-property-service.php
@@ -17,9 +17,80 @@ class Wordlift_Schema_Url_Property_Service_Test extends WP_UnitTestCase {
 	}
 
 
+	/**
+	 * Test getting/setting the post meta.
+	 *
+	 * @since 3.6.0
+	 */
 	public function test() {
 
-//		$this->schema_url_property_service->get_post_metadata();
+		// Create a fake post.
+		$id = $this->factory->post->create();
+
+		// 1. Check that the post meta returned when there's no meta set is <permalink>
+		$meta = get_post_meta( $id, Wordlift_Schema_Url_Property_Service::META_KEY );
+
+		$this->assertTrue( is_array( $meta ) );
+		$this->assertCount( 1, $meta );
+		$this->assertEquals( '<permalink>', $meta[0] );
+
+		// Check that Wordlift_Schema_Url_Property_Service returns the same <permalink>.
+		$values = $this->schema_url_property_service->get( $id );
+
+		$this->assertTrue( is_array( $values ) );
+		$this->assertCount( 1, $values );
+		$this->assertEquals( '<permalink>', $values[0] );
+
+		// Check that the query is empty.
+		$query = $this->schema_url_property_service->get_insert_query( 'http://example.org', $id );
+
+		$this->assertNotEmpty( $query );
+
+		// 2. Now add an empty string post meta.
+		add_post_meta( $id, Wordlift_Schema_Url_Property_Service::META_KEY, '' );
+
+		// Check that get_post_meta returns the empty string.
+		$meta = get_post_meta( $id, Wordlift_Schema_Url_Property_Service::META_KEY );
+
+		$this->assertTrue( is_array( $meta ) );
+		$this->assertCount( 1, $meta );
+		$this->assertEquals( '', $meta[0] );
+
+		// Check that Wordlift_Schema_Url_Property_Service returns NULL instead.
+		$values = $this->schema_url_property_service->get( $id );
+
+		$this->assertNull( $values );
+
+		// Check that the query is empty.
+		$query = $this->schema_url_property_service->get_insert_query( 'http://example.org', $id );
+
+		$this->assertEmpty( $query );
+
+		// 3. Empty the post meta and add another value.
+		delete_post_meta( $id, Wordlift_Schema_Url_Property_Service::META_KEY );
+
+		// Now add a string post meta.
+		add_post_meta( $id, Wordlift_Schema_Url_Property_Service::META_KEY, 'http://example.org' );
+
+		// Check that get_post_meta returns the empty string.
+		$meta = get_post_meta( $id, Wordlift_Schema_Url_Property_Service::META_KEY );
+
+		$this->assertTrue( is_array( $meta ) );
+		$this->assertCount( 1, $meta );
+		$this->assertEquals( 'http://example.org', $meta[0] );
+
+		// Check that Wordlift_Schema_Url_Property_Service returns NULL instead.
+		$values = $this->schema_url_property_service->get( $id );
+
+		$this->assertTrue( is_array( $values ) );
+		$this->assertCount( 1, $values );
+		$this->assertEquals( 'http://example.org', $values[0] );
+
+		// Check that the query is empty.
+		$query = $this->schema_url_property_service->get_insert_query( 'http://example.org', $id );
+
+		$this->assertNotEmpty( $query );
 
 	}
+
 }
