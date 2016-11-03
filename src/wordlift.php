b47af5fa7d19edb52ab82158e6af1c4e173dee4c
David Riccitelli <david@insideout.io>
2016-11-03 19:58:09 +0200

    bump to 3.9.0-dev

--- src/wordlift.php
+++ src/wordlift.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WordLift
  * Plugin URI:        http://wordlift.it
  * Description:       WordLift brings the power of AI to organize content, attract new readers and get their attention. To activate the plugin ​<a href="https://wordlift.io/">visit our website</a>.
- * Version:           3.8.0
+ * Version:           3.9.0-dev
  * Author:            WordLift, Insideout10
  * Author URI:        http://wordlift.it
  * License:           GPL-2.0+
