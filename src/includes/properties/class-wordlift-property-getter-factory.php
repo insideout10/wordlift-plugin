4b81705c760baa19a941472fe9f57ed0a5ccb594
David Riccitelli <david@insideout.io>
2016-11-07 17:46:17 +0200

    see #393: provide a structured response for location entities that have only a textual name set

--- src/includes/properties/class-wordlift-property-getter-factory.php
+++ src/includes/properties/class-wordlift-property-getter-factory.php
@@ -3,6 +3,7 @@
 require_once( 'class-wordlift-property-getter.php' );
 require_once( 'class-wordlift-simple-property-service.php' );
 require_once( 'class-wordlift-entity-property-service.php' );
+require_once( 'class-wordlift-location-property-service.php' );
 require_once( 'class-wordlift-url-property-service.php' );
 require_once( 'class-wordlift-double-property-service.php' );
 
@@ -26,17 +27,19 @@ class Wordlift_Property_Getter_Factory {
 
 		$property_getter = new Wordlift_Property_Getter( new Wordlift_Simple_Property_Service() );
 		$property_getter->register( new Wordlift_Entity_Property_Service( $entity_service ), array(
-			Wordlift_Schema_Service::FIELD_LOCATION,
 			Wordlift_Schema_Service::FIELD_FOUNDER,
 			Wordlift_Schema_Service::FIELD_AUTHOR,
 			Wordlift_Schema_Service::FIELD_KNOWS,
 			Wordlift_Schema_Service::FIELD_BIRTH_PLACE,
 			Wordlift_Schema_Service::FIELD_AFFILIATION,
 		) );
+		$property_getter->register( new Wordlift_Location_Property_Service( $entity_service ), array(
+			Wordlift_Schema_Service::FIELD_LOCATION,
+		) );
 		$property_getter->register( new Wordlift_Url_Property_Service(), array( Wordlift_Url_Property_Service::META_KEY ) );
 		$property_getter->register( new Wordlift_Double_Property_Service(), array(
 			Wordlift_Schema_Service::FIELD_GEO_LATITUDE,
-			Wordlift_Schema_Service::FIELD_GEO_LONGITUDE
+			Wordlift_Schema_Service::FIELD_GEO_LONGITUDE,
 		) );
 
 		return $property_getter;
