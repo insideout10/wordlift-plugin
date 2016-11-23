47feea8a33044e0c767b87ae954174db0b5eeeec
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2016-11-23 16:28:50 +0100

    desktop styles 😎

--- src/admin/partials/wordlift-admin-install-wizard-step-5.php
+++ src/admin/partials/wordlift-admin-install-wizard-step-5.php
@@ -6,42 +6,49 @@
  *
  * @link       https://wordlift.io
  * @since      3.9.0
- *
- * @package    Wordlift
- * @subpackage Wordlift/admin/partials
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 ?>
-<div id="title"><?php esc_html_e( 'Publisher', 'wordlift' ) ?></div>
-<div id="message"><?php esc_html_e( 'Are you going to publish as an individual or as a company?', 'wordlift' ) ?></div>
+<div id="title"><?php esc_html_e('Publisher', 'wordlift') ?></div>
+<div id="message"><?php esc_html_e('Are you going to publish as an individual or as a company?', 'wordlift') ?></div>
 <div id="radio">
-	<label for="personal"><input id="personal" type="radio" name="user_type"
-	                             value="personal" <?php checked( $type, 'personal' ) ?>><?php esc_html_e( 'Personal', 'wordlift' ) ?>
+	<label for="personal">
+		<input id="personal" type="radio" name="user_type"
+	                             value="personal" <?php checked($type, 'personal') ?>>
+		 <span class="radio"><span class="check"></span></span>
+		 <span class="label">
+			 <?php esc_html_e('Personal', 'wordlift') ?>
+		 </span>
 	</label>
-	<label for="company"><input id="company" type="radio" name="user_type"
-	                            value="company" <?php checked( $type, 'company' ) ?>><?php esc_html_e( 'Company', 'wordlift' ) ?>
+	<label for="company">
+		<input id="company" type="radio" name="user_type"
+	                            value="company" <?php checked($type, 'company') ?>>
+			<span class="radio"><span class="check"></span></span>
+			<span class="label">
+				<?php esc_html_e('Company', 'wordlift') ?>
+			</span>
 	</label>
 </div>
-<div id="input"><input class="input" id="key" type="text" name="key" value="<?php echo esc_attr( $name ) ?>"
-                       placeholder="<?php esc_html_e( 'Name', 'wordlift' ) ?>"></div>
-<div id="addlogo" <?php if ( 0 != $image_id )
-	echo 'style="display:none"' ?>><a class="wl-add-logo"
-                                      href="javascript:void(0);"><?php esc_html_e( 'Add your logo', 'wordlift' ) ?></a>
+<div id="input"><input class="input" id="key" type="text" name="key" value="<?php echo esc_attr($name) ?>"
+                       placeholder="<?php esc_html_e('Name', 'wordlift') ?>"></div>
+<div id="addlogo" <?php if (0 != $image_id) {
+    echo 'style="display:none"';
+} ?>><a class="wl-add-logo"
+                                      href="javascript:void(0);"><?php esc_html_e('Add your logo', 'wordlift') ?></a>
 </div>
-<div id="logo" style="<?php echo( 0 == $image_id ? 'display:none' : '' ); ?>">
-	<img src="<?php echo esc_attr( 0 == $image_id ? '' : $image_url ); ?>"
-	     data-id="<?php echo esc_attr( 0 == $image_id ? '' : $image_id ); ?>"
+<div id="logo" style="<?php echo  0 == $image_id ? 'display:none' : ''; ?>">
+	<img src="<?php echo esc_attr(0 == $image_id ? '' : $image_url); ?>"
+	     data-id="<?php echo esc_attr(0 == $image_id ? '' : $image_id); ?>"
 	     width="100" height="100">
-	<a href="javascript:void(0);" title="<?php esc_html_e( 'Remove the logo', 'wordlift' ) ?>"><span
+	<a href="javascript:void(0);" title="<?php esc_html_e('Remove the logo', 'wordlift') ?>"><span
 			class="fa fa-times"></a>
 </div>
 <div id="buttons">
 	<a id="nextstep" onclick="savevalue()"
-	   href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'step', '5' ), 'wordlift_finish_nonce', '_wl_finish_nonce' ) ); ?>"><?php esc_html_e( 'Finish', 'wordlift' ); ?></a>
+	   href="<?php echo esc_url(wp_nonce_url(add_query_arg('step', '5'), 'wordlift_finish_nonce', '_wl_finish_nonce')); ?>"><?php esc_html_e('Finish', 'wordlift'); ?></a>
 </div>
 <script type="text/javascript">
 
