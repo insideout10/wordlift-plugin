8cd0855ac0a06774ed564af235a5d46fa5448729
David Riccitelli <david@insideout.io>
2016-12-09 19:34:16 +0200

    see #417: update encrypted envs

--- wdio.conf.js
+++ wdio.conf.js
@@ -58,8 +58,8 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 10,
-        name: 'WordLift Cross-Browsing Tests (#' + process.env.TRAVIS_BUILD_NUMBER + ')',
+        maxInstances: 5,
+        // name: 'WordLift Cross-Browsing Tests (#' + process.env.TRAVIS_BUILD_NUMBER + ')',
 
         // If using Open Sauce (https://saucelabs.com/opensauce/),
         // capabilities must be tagged as "public" for the jobs's status
