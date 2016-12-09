159d3b655aa921b910dd0c553a1f8aad7d4410eb
David Riccitelli <david@insideout.io>
2016-12-09 12:49:57 +0200

    bump to 3.8.1

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -286,7 +286,7 @@ class Wordlift {
 	public function __construct() {
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.8.0';
+		$this->version     = '3.8.1';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
