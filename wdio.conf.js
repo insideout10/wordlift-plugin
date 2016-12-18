45f56792afdc21b81c3592bfcd2d5de333005c64
David Riccitelli <david@insideout.io>
2016-12-18 16:52:34 +0200

    see #417: take 12

--- wdio.conf.js
+++ wdio.conf.js
@@ -288,7 +288,7 @@ if (process.env.CI) {
     for (var i = 0; i < config.capabilities.length; i++) {
         config.capabilities[i].desiredCapabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
         config.capabilities[i].desiredCapabilities['build'] = process.env.TRAVIS_BUILD_NUMBER;
-        config.capabilities[i].desiredCapabilities['baseUrl'] = BASE_URL + '/' + (i + 1);
+        config.capabilities[i].baseUrl = 'http://' + (i + 1) + '.localhost';
     }
 
 }
