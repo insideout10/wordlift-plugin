f613d55f8b52a29a9388a59f2438eb013f7c8846
David Riccitelli <david@insideout.io>
2016-12-22 12:11:15 +0200

    merge release/3.9.0; bump to 3.10.0-dev

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -304,7 +304,7 @@ class Wordlift {
 	public function __construct() {
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.9.0';
+		$this->version     = '3.10.0-dev';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
