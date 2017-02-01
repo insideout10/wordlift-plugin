ce7062335816053e878c734ef0a6ee8276934c88
David Riccitelli <david@insideout.io>
2017-02-01 16:12:51 +0200

    see #335: getting back on track with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,41 @@
-module.exports = {
-    entry: './src/admin/js/wordlift-admin.js',
-    output: {
-        filename: 'wordlift-admin.bundle.js',
-        path: './src/admin/js'
-    }
+const webpack = require( 'webpack' ); //to access built-in plugins
+const path = require( 'path' );
+
+// @todo: consider splitting vendor, see:
+// * https://robertknight.github.io/posts/webpack-dll-plugins/
+// * https://webpack.js.org/concepts/entry-points/
+
+// @todo: add the other files:
+// * wordlift, for the WordPress front-end,
+// * wordlift-admin, for the WordPress admin,
+// * wordlift-editor, for the WordPress TinyMCE editor.
+// see https://webpack.js.org/concepts/output/#output-filename.
+
+// @todo: migration, we're migrating all the JavaScript files to WebPack and
+// organizing them in three main files (front-end, admin, editor). For some time
+// the old files and the new files will cohexist.
+
+const config = {
+	entry: {
+		'wordlift-admin': './src/scripts/admin/index.js'
+	},
+	output: {
+		path: path.resolve( __dirname, 'src/admin/js' ),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [
+			{ test: /\.(js|jsx)$/, use: 'babel-loader' }
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin( {
+			'process.env': {
+				NODE_ENV: JSON.stringify( 'production' )
+			}
+		} ),
+		new webpack.optimize.UglifyJsPlugin()
+	]
 };
+
+module.exports = config;
