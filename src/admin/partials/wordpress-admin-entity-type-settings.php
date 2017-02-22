4079bb2ba7b6f142f935d6f914edbaa10739600a
mark kaplun <mark@marksw.com>
2017-02-22 18:24:25 +0200

    Make the settings form into a partial

---
+++ src/admin/partials/wordpress-admin-entity-type-settings.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Pages: entity type term settings page.
+ *
+ * @since   3.11.0
+ * @package Wordlift/admin
+ */
+
+$term_id = absint( $_REQUEST['tag_ID'] );
+$term    = get_term( $term_id, 'wl_entity_type' );
+
+$settings = get_option( 'wl_entity_type_settings', array() );
+$term_settings = isset( $settings[ $term_id ] ) ? $settings[ $term_id ] : array(
+	'title' => '',
+	'description' => '',
+);
+
+?>
+<div class="wrap">
+	<h1><?php _e( 'Edit Entity Type', 'wordlift' ) ?></h1>
+
+	<form name="edittag" id="edittag" method="post" action="<?php echo admin_url( 'admin.php?page=wl_entity_type_settings' )?>" class="validate">
+	<input type="hidden" name="tag_ID" value="<?php echo esc_attr( $term_id ) ?>"/>
+	<input type="hidden" name="action" value="wl_edit_entity_type_term"/>
+	<?php
+	wp_nonce_field( 'update-entity_type_term_' . $term_id );
+
+	?>
+		<table class="form-table">
+			<tr class="form-field form-required term-name-wrap">
+				<th scope="row"><label><?php _e( 'Name', 'wordlift' ); ?></label></th>
+				<td><?php echo esc_html( $term->name )?></td>
+			</tr>
+			<tr class="form-field form-required term-name-wrap">
+				<th scope="row"><label for="title"><?php _e( 'SEO Title', 'wordlift' ); ?></label></th>
+				<td><input name="title" id="title" type="text" value="<?php echo esc_attr( $term_settings['title'] )?>" size="40" />
+				<p class="description"><?php _e( 'The HTML title to be used at the entity type achieve page.' ); ?></p></td>
+			</tr>
+			<tr class="form-field term-description-wrap">
+				<th scope="row"><label for="description"><?php _e( 'SEP Description' ); ?></label></th>
+				<td><textarea name="description" id="description" rows="5" cols="50" class="large-text"><?php echo esc_html( $term_settings['description'] )  ?></textarea>
+				<p class="description"><?php _e( 'The description meta used in the entity type achieve page.', 'wordlift' ); ?></p></td>
+			</tr>
+		</table>
+	<?php
+	submit_button( __( 'Update' ) );
+	?>
+	</form>
+</div>
