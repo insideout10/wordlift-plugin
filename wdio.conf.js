345cf8e21c1a446f3fe38efb0662a30f4c4b56f2
David Riccitelli <david@insideout.io>
2017-02-24 00:13:52 +0200

    see #335: compile e2e tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -156,7 +156,7 @@ var config = {
     jasmineNodeOpts: {
         //
         // Jasmine default timeout
-        defaultTimeoutInterval: 20000,
+        defaultTimeoutInterval: 60000,
         //
         // The Jasmine framework allows interception of each assertion in order to log the state of the application
         // or website depending on the result. For example, it is pretty handy to take a screenshot every time
