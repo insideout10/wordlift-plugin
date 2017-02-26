6abe018f6f97e74d7873797e539feba5e3c39100
mark kaplun <mark@marksw.com>
2017-02-26 19:18:46 +0200

    instead of being meta title specific, apply entity type term name and description changes for the whole front end #398

--- src/public/class-wordlift-seo-service.php
+++ src/public/class-wordlift-seo-service.php
@@ -26,95 +26,39 @@ class Wordlift_Seo_Service {
 	 */
 	function __construct() {
 
-		/*
-		 * Set filters to manipulate the HTML title element.
-		 *
-		 * There are two filters here, the first is the post WordPress 4.4
-		 * proffered way, and the second is for backward compatibility with
-		 * themes that do not play well with the first ones.
-		 *
-		 * Seems like Yoast SEO gladly ignores the title we supply and uses the
-		 * term name, so for now use a higher priority to override its generated
-		 * title (Yoast uses 15).
-		 */
-		add_filter( 'pre_get_document_title', array( $this, 'title' ), 20 );
-		add_filter( 'wp_title', array( $this, 'title' ), 20, 3 );
-
-		// Set the action to inject the description meta.
-		add_action( 'wp_head', array( $this, 'description_meta' ) );
-
+		// If we are not on the admin, run the get_term filter for entity type terms.
+		if ( ! is_admin() ) {
+			add_filter( 'get_wl_entity_type', array( $this, 'get_wl_entity_type' ), 10, 2 );
+		}
 	}
 
 	/**
-	 * Override the default wordpress title for the entity type term archieve
-	 * page.
+	 * Filter the entity term object, replace title and description
+	 * with whatever was set in the entity settings page
 	 *
-	 * @since 3.11.0
+	 * @since 3.11
 	 *
-	 * @param string $title              The title to be changed.
-	 * @param string $separator          The separator between title elements.
-	 * @param string $separator_location The location of the separator in the title.
+	 * @param WP_Term	$Term	The term to filters.
+	 * @param string 	$taxonomy	The taxonomy name.
 	 *
-	 * @return string If it is a entity type term page, the title set for the term
-	 *                in the term settings screen. Otherwise return the $title argument.
+	 * @return WP_Term which is $term with fields changed
 	 */
-	function title( $title, $separator = '', $separator_location = '' ) {
+	function get_wl_entity_type( $term, $taxonomy ) {
 
-		// First check if we are serving an entity type term archive page.
-		if ( is_tax( 'wl_entity_type' ) ) {
+		$entity_settings = get_option( 'wl_entity_type_settings', array() );
 
-			// Get the term for the archive of which is displayed.
-			$term = get_queried_object();
+		if ( isset( $entity_settings[ $term->term_id ] ) ) {
 
-			/*
-			 * Check if there is a none empty title setting for the term,
-			 * and if there is, return it
-			 */
-			$entity_settings = get_option( 'wl_entity_type_settings', array() );
-
-			if ( isset( $entity_settings[ $term->term_id ] ) ) {
-
-				$settings = $entity_settings[ $term->term_id ];
-
-				if ( ! empty( $settings['title'] ) ) {
-
-					return $settings['title'];
-				}
-
+			$settings = $entity_settings[ $term->term_id ];
+			if ( ! empty( $settings['title'] ) ) {
+				$term->name = $settings['title'];
+			}
+			if ( ! empty( $settings['description'] ) ) {
+				$term->description = $settings['description'];
 			}
 		}
 
-		// There was no modification done before, so just return the original.
-		return $title;
-	}
-
-	/**
-	 * Output a description meta into an entity type term archive page.
-	 *
-	 * @since 3.11.0
-	 */
-	function description_meta() {
-
-		if ( is_tax( 'wl_entity_type' ) ) {
-
-			$term = get_queried_object();
-
-			$entity_settings = get_option( 'wl_entity_type_settings', array() );
-
-			if ( isset( $entity_settings[ $term->term_id ] ) ) {
-
-				$settings = $entity_settings[ $term->term_id ];
-
-				if ( ! empty( $settings['description'] ) ) {
-
-					echo '<meta name="description" content="', esc_attr( strip_tags( $settings['description'] ) ), '"/>', "\n";
-
-				}
-
-			}
-
-		}
-
+		return $term;
 	}
 
 }
