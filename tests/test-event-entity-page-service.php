800e07b87c3e81896b1a6c2899b2d9482177df3b
mark kaplun <mark@marksw.com>
2017-03-21 16:42:14 +0200

    order event entitis in desc order based on their start time on the eveent entities archieve page

---
+++ tests/test-event-entity-page-service.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Tests: event entity archive page Service.
+ *
+ * @since   3.12.0
+ * @package Wordlift
+ */
+
+/**
+ * Define the {@link Wordlift_Event_entity_Page_Service_Test} class.
+ *
+ * @since   3.12.0
+ * @package Wordlift
+ */
+class Wordlift_Event_entity_Page_Service_Test extends Wordlift_Unit_Test_Case {
+
+	/**
+	 * Test non main query is ignored.
+	 *
+	 * @since 3.12.0
+	 */
+	public function test_non_main_query_ignored() {
+
+		$entity_1_id   = $this->factory->post->create( array(
+			'post_type'    => 'entity',
+			'post_content' => '',
+			'post_title'   => 'test post',
+			'post_status'  => 'publish',
+		) );
+
+		wl_set_entity_main_type( $entity_1_id, Wordlift_Schema_Service::SCHEMA_EVENT_TYPE );
+		add_post_meta( $entity_1_id, Wordlift_Schema_Service::FIELD_DATE_START, '2014-01-01', true );
+		add_post_meta( $entity_1_id, Wordlift_Schema_Service::FIELD_DATE_END, '2014-01-07', true );
+
+		$entity_2_id   = $this->factory->post->create( array(
+			'post_type'    => 'entity',
+			'post_content' => '',
+			'post_title'   => 'test post',
+			'post_status'  => 'publish',
+		) );
+
+		wl_set_entity_main_type( $entity_2_id, Wordlift_Schema_Service::SCHEMA_EVENT_TYPE );
+		add_post_meta( $entity_2_id, Wordlift_Schema_Service::FIELD_DATE_START, '2014-02-01', true );
+
+		$entity_3_id   = $this->factory->post->create( array(
+			'post_type'    => 'entity',
+			'post_content' => '',
+			'post_title'   => 'test post',
+			'post_status'  => 'publish',
+		) );
+
+		wl_set_entity_main_type( $entity_3_id, Wordlift_Schema_Service::SCHEMA_EVENT_TYPE );
+		add_post_meta( $entity_3_id, Wordlift_Schema_Service::FIELD_DATE_START, '2014-01-21', true );
+
+		$query = new WP_Query( array(
+			'cat' => $category_id,
+		) );
+		$posts = $query->get_posts();
+
+		// test that three entities are returned.
+		$this->assertEquals( 2, count( $posts ) );
+
+		// ... and that it is the reverse publishing order.
+		$this->assertEquals( $entity_1_id, $posts[0]->ID );
+		$this->assertEquals( $entity_2_id, $posts[1]->ID );
+		$this->assertEquals( $entity_3_id, $posts[2]->ID );
+	}
+
+	/**
+	 * Test event entity archive page shows entities in desc order
+	 * of start time.
+	 *
+	 * Test done by simulating a main query.
+	 *
+	 * @since 3.12
+	 */
+	public function test_event_entity_archive_page() {
+
+		$entity_1_id   = $this->factory->post->create( array(
+			'post_type'    => 'entity',
+			'post_content' => '',
+			'post_title'   => 'test post',
+			'post_status'  => 'publish',
+		) );
+
+		wl_set_entity_main_type( $entity_1_id, Wordlift_Schema_Service::SCHEMA_EVENT_TYPE );
+		add_post_meta( $entity_1_id, Wordlift_Schema_Service::FIELD_DATE_START, '2014-01-01', true );
+		add_post_meta( $entity_1_id, Wordlift_Schema_Service::FIELD_DATE_END, '2014-01-07', true );
+
+		$entity_2_id   = $this->factory->post->create( array(
+			'post_type'    => 'entity',
+			'post_content' => '',
+			'post_title'   => 'test post',
+			'post_status'  => 'publish',
+		) );
+
+		wl_set_entity_main_type( $entity_2_id, Wordlift_Schema_Service::SCHEMA_EVENT_TYPE );
+		add_post_meta( $entity_2_id, Wordlift_Schema_Service::FIELD_DATE_START, '2014-02-01', true );
+
+		$entity_3_id   = $this->factory->post->create( array(
+			'post_type'    => 'entity',
+			'post_content' => '',
+			'post_title'   => 'test post',
+			'post_status'  => 'publish',
+		) );
+
+		wl_set_entity_main_type( $entity_3_id, Wordlift_Schema_Service::SCHEMA_EVENT_TYPE );
+		add_post_meta( $entity_3_id, Wordlift_Schema_Service::FIELD_DATE_START, '2014-01-21', true );
+
+		$this->go_to( '?wl_entity_type=event' );
+
+		global $wp_the_query;
+
+		$posts = $wp_the_query->get_posts();
+
+		// test that three entities are returned.
+		$this->assertEquals( 3, count( $posts ) );
+
+		// ... and that it is the reverse order by start time.
+		$this->assertEquals( $entity_2_id, $posts[0]->ID );
+		$this->assertEquals( $entity_3_id, $posts[1]->ID );
+		$this->assertEquals( $entity_1_id, $posts[2]->ID );
+	}
+}
