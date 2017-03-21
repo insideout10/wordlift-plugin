bcc544bb378a5c3c444d0cffbb78e1539b50b8bd
mark kaplun <mark@marksw.com>
2017-03-21 13:12:23 +0200

    Add a wl_cloud shortode to insert related entities cloud into content. #490

--- src/public/class-wordlift-related-entities-cloud-widget.php
+++ src/public/class-wordlift-related-entities-cloud-widget.php
@@ -63,69 +63,17 @@ class Wordlift_Related_Entities_Cloud_Widget extends Wordlift_Widget {
 	}
 
 	/**
-	 * Find the related entities to the currently displayed post and
-	 * calculate the "tags" for them as wp_generate_tag_cloud expects to get.
-	 *
-	 * @since 3.11.0
-	 *
-	 * @return array    Array of tags. Empty array in case we re not in a context
-	 *                  of a post, or it has no related entities.
-	 */
-	public function get_related_entities_tags() {
-		// Define the supported types list.
-		$supported_types = array( 'post', Wordlift_Entity_Service::TYPE_NAME );
-
-		// Show nothing if not on a post or entity page.
-		if ( ! is_singular( $supported_types ) ) {
-			return array();
-		}
-
-		// Get the IDs of entities related to current post.
-		$related_entities = wl_core_get_related_entity_ids( get_the_ID(), array( 'status' => 'publish' ) );
-
-		// Bail out if there are no associated entities.
-		if ( empty( $related_entities ) ) {
-			return array();
-		}
-
-		/*
-		 * Create an array of "tags" to feed to wp_generate_tag_cloud.
-		 * Use the number of posts and entities connected to the entity as a weight.
-		 */
-		$tags = array();
-
-		foreach ( $related_entities as $entity_id ) {
-
-			$connected_entities = count( wl_core_get_related_entity_ids( $entity_id, array( 'status' => 'publish' ) ) );
-			$connected_posts    = count( wl_core_get_related_posts( $entity_id, array( 'status' => 'publish' ) ) );
-
-			$tags[] = (object) array(
-				'id'    => $entity_id,
-				// Used to give a unique class on the tag.
-				'name'  => get_the_title( $entity_id ),
-				// The text of the tag.
-				'slug'  => get_the_title( $entity_id ),
-				// Required but not seem to be relevant
-				'link'  => get_permalink( $entity_id ),
-				// the url the tag links to.
-				'count' => $connected_entities + $connected_posts,
-				// The weight.
-			);
-		}
-
-		return $tags;
-	}
-
-	/**
 	 * @inheritdoc
 	 */
 	public function widget( $args, $instance ) {
 
-		$tags = $this->get_related_entities_tags();
-
-		// Bail out if there are no associated entities.
-		if ( empty( $tags ) ) {
-			return;
+		/*
+		 * Use the shortcode to calculate the HTML required to show the cloud
+		 * if there is no such html do not render the widget at all.
+		 */
+		$cloud_html = do_shortcode( '[wl_cloud]' );
+		if ( empty( $cloud_html ) ) {
+			return false;
 		}
 
 		// The widget title.
@@ -134,23 +82,14 @@ class Wordlift_Related_Entities_Cloud_Widget extends Wordlift_Widget {
 		// Standard filter all widgets should apply
 		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
 
-		/*
-		 * Need to have the same class as the core tagcloud widget, to easily
-		 * inherit its styling.
-		 */
-
 		echo $args['before_widget'];
 
 		if ( $title ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
-		?>
-		<div class="tagcloud wl-related-entities-cloud"><?php
-			echo wp_generate_tag_cloud( $tags ); ?></div>
-		<?php
 
+		echo $cloud_html;
 		echo $args['after_widget'];
-
 	}
 
 }
