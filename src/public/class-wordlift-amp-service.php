e7da7d6af1b369bf4eb6230cb4d9a57a964bd000
David Riccitelli <david@insideout.io>
2017-04-23 17:59:26 +0300

    see #212: add comments

--- src/public/class-wordlift-amp-service.php
+++ src/public/class-wordlift-amp-service.php
@@ -28,11 +28,25 @@ class Wordlift_AMP_Service {
 
 		// Integrate with automattic's AMP plugin if it is available
 		if ( defined( 'AMP__VERSION' ) ) {
-			add_action( 'amp_init', array( $this, 'register_entity_cpt_with_amp_plugin' ) );
+			add_action( 'amp_init', array(
+				$this,
+				'register_entity_cpt_with_amp_plugin',
+			) );
 		}
+
 	}
 
+	/**
+	 * Register the `entity` post type with the AMP plugin.
+	 *
+	 * @since 3.12.0
+	 */
 	function register_entity_cpt_with_amp_plugin() {
-		add_post_type_support( 'entity', AMP_QUERY_VAR );
+
+		if ( defined( 'AMP_QUERY_VAR' ) ) {
+			add_post_type_support( 'entity', AMP_QUERY_VAR );
+		}
+
 	}
+
 }
