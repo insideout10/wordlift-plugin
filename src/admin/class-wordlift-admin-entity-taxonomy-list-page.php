8dc7c7eea62560840524fcbef95d5854628a77c1
David Riccitelli <david@insideout.io>
2017-04-23 16:33:33 +0300

    see #512: add comments

--- src/admin/class-wordlift-admin-entity-taxonomy-list-page.php
+++ src/admin/class-wordlift-admin-entity-taxonomy-list-page.php
@@ -46,10 +46,12 @@ class Wordlift_Admin_Entity_Taxonomy_List_Page {
 	function wl_entity_type_row_actions( $actions, $term ) {
 
 		$url               = admin_url( "admin.php?page=wl_entity_type_settings&tag_ID=$term->term_id" );
-		$actions['wl_seo'] = '<a href="' . esc_url( $url ) . '">' . __( 'SEO Settings', 'wordlift' ) . '</a>';
+		$actions['wl-seo'] = '<a href="' . esc_url( $url ) . '">' . __( 'SEO Settings', 'wordlift' ) . '</a>';
 
-		// Hacks for pre 4.7 compatibility.
+		// Hacks for pre 4.7 compatibility:
+		// * remove the quick edit:
 		unset( $actions['inline hide-if-no-js'] );
+		// * remove the edit link:
 		unset( $actions['edit'] );
 
 		return $actions;
@@ -88,6 +90,13 @@ class Wordlift_Admin_Entity_Taxonomy_List_Page {
 	 * Override the capabilities related to managing the entity type terms
 	 * required for WordPress < 4.7 to get access to the admin page.
 	 *
+	 * Before 4.7, WordPress checks whether the user has permission to access
+	 * the `edit-tags.php` page. So we need to provide temporary the permission
+	 * otherwise the user will get an `access not allowed` when trying to access
+	 * the page.
+	 *
+	 * @see   https://github.com/insideout10/wordlift-plugin/issues/512
+	 *
 	 * @since 3.12.0
 	 *
 	 * @param array  $caps    The user's current capabilities.
@@ -98,31 +107,34 @@ class Wordlift_Admin_Entity_Taxonomy_List_Page {
 	 * @return array    Array containing the manage_options capability
 	 */
 	function enable_admin_access_pre_47( $caps, $cap, $user_id, $args ) {
-		if ( 'wl_entity_type_edit_term' != $cap ) {
+
+		// Bail out if we're not dealing with the `wl_entity_type_edit_term`
+		// capability.
+		if ( 'wl_entity_type_edit_term' !== $cap ) {
 			return $caps;
 		}
 
-		$parts = parse_url( $_SERVER['REQUEST_URI'] );
-		if ( ! isset( $parts['path'] ) ) {
+		// Bail out if the `path` cannot be determined.
+		if ( null === $path = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) ) {
 			return $caps;
 		}
 
-		$components = explode( '/', $parts['path'] );
-		if ( empty( $components ) ) {
-			return $caps;
-		}
-
-		if ( 'edit-tags.php' != $components[ count( $components ) - 1 ] ) {
-			return $caps;
-		}
-
-		// When an action is set, it is an indication that a specific term is being handled.
+		// Bail out if a specific term is being handled.
 		if ( isset( $_REQUEST['action'] ) ) {
 			return $caps;
 		}
 
+		// Split the path and check if the page is the `edit-tags.php`. We can't
+		// use WP's own function because this is called very early.
+		$components = explode( '/', $path );
+		if ( 'edit-tags.php' !== $components[ count( $components ) - 1 ] ) {
+			return $caps;
+		}
+
+		// Give permission to the user.
 		$mapped[] = 'manage_options';
 
 		return $mapped;
 	}
+
 }
