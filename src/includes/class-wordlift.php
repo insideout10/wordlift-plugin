f68490009fe29fb4a2821a732e8f13816639f47d
David Riccitelli <david@insideout.io>
2017-06-08 14:06:12 +0300

    bump to 3.13.0 and update changelog

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -494,7 +494,7 @@ class Wordlift {
 	public function __construct() {
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.13.0-dev';
+		$this->version     = '3.13.0';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
