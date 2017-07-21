34d08553f3bb06345e26cb01f1a6bf9471cd5870
David Riccitelli <david@insideout.io>
2017-07-21 19:05:17 +0300

    restore sanitize function

--- src/admin/WL_Metabox/class-wordlift-metabox-field-duration.php
+++ src/admin/WL_Metabox/class-wordlift-metabox-field-duration.php
@@ -30,4 +30,29 @@ class Wordlift_Metabox_Field_Duration extends WL_Metabox_Field_date {
 
 	}
 
+	/**
+	 * Sanitize a single value. Called from $this->sanitize_data. Default sanitization excludes empty values.
+	 * make sure the value is either empty, an integer representing valid number of minutes
+	 * or an HH:MM time format.
+	 *
+	 * @param mixed $value The value being sanitized.
+	 *
+	 * @return mixed Returns sanitized value, or null.
+	 */
+	public function sanitize_data_filter( $value ) {
+
+		if ( ! is_null( $value ) && '' !== $value ) {         // do not use 'empty()' -> https://www.virendrachandak.com/techtalk/php-isset-vs-empty-vs-is_null/ .
+			preg_match( '#((([01]?[0-9]{1}|2[0-3]{1}):)?[0-5]{1})?[0-9]{1}#',
+				trim( $value ),
+				$matches
+			);
+
+			if ( count( $matches ) > 0 ) {
+				return $matches[0];
+			}
+		}
+
+		return null;
+	}
+
 }
