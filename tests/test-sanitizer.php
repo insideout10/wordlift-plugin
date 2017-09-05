6972ee973039f9c462e1e2426359e6df6a7c149a
mark kaplun <mark@marksw.com>
2017-09-05 13:42:03 +0300

    UT for #547

--- tests/test-sanitizer.php
+++ tests/test-sanitizer.php
@@ -15,12 +15,21 @@ class Wordlift_Sanitizer_Test extends Wordlift_Unit_Test_Case {
 
 	public function test() {
 
-
 		$this->assertNotNull( Wordlift_Sanitizer::sanitize_url( 'http://dbpedia.org/resource/Huttau' ) );
 
 		$this->assertNotNull( Wordlift_Sanitizer::sanitize_url( 'http://dbpedia.org/resource/Hüttau' ) );
 
+		// Check https url.
+		$this->assertNotNull( Wordlift_Sanitizer::sanitize_url( 'https://dbpedia.org/resource/Hüttau' ) );
 
+		// Check space are trader_cdlrisefall3methods
+		$this->assertEquals( 'https://test', Wordlift_Sanitizer::sanitize_url( '   https://test' ) );
+
+		// Check empty strings are accepted.
+		$this->assertEquals( '', Wordlift_Sanitizer::sanitize_url( '   ' ) );
+
+		// Check Non URLs return null.
+		$this->assertNull( Wordlift_Sanitizer::sanitize_url( 'wordlift' ) );
 	}
 
 }
