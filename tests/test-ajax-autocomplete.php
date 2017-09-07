a7be8c65eea7204cc86cb731b6bf4b041013e504
Stanimir <stanimir.k.stoyanov@gmail.com>
2017-09-07 11:01:03 +0300

    see #607: add more tests for autocomplete feature

--- tests/test-ajax-autocomplete.php
+++ tests/test-ajax-autocomplete.php
@@ -5,37 +5,32 @@
  * @since   3.15.0
  * @package Wordlift
  */
-require_once 'functions.php';
 
 /**
- * Class AjaxAutocompleteTest
+ * Class Wordlift_Autocomplete_Test
  * Extend Wordlift_Ajax_Unit_Test_Case
  *
  * @since   3.15.0
  * @package Wordlift
  */
-class AjaxAutocompleteTest extends Wordlift_Ajax_Unit_Test_Case {
+class Wordlift_Autocomplete_Test extends Wordlift_Ajax_Unit_Test_Case {
 	/**
-	 * A {@link Wordlift_Configuration_Service} instance.
+	 * A {@link Wordlift_Autocomplete_Service} instance.
 	 *
 	 * @since  3.15.0
 	 * @access private
-	 * @var \Wordlift_Configuration_Service $configuration_service A {@link Wordlift_Configuration_Service} instance.
+	 * @var \Wordlift_Autocomplete_Service $configuration_service A {@link Wordlift_Autocomplete_Service} instance.
 	 */
-	private $configuration_service;
+	private $autocomplete_service;
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function setUp() {
 		parent::setUp();
-
-		$wordlift = new Wordlift_Test();
-		$this->configuration_service = $wordlift->get_configuration_service();
 	}
 
 	public function test_autocomplete_without_nonce() {
-		// Spoof the nonce in the POST superglobal
 		$_POST['query'] = 'test';
 
 		try {
@@ -44,6 +39,23 @@ class AjaxAutocompleteTest extends Wordlift_Ajax_Unit_Test_Case {
 	 
 		$response = json_decode( $this->_last_response );
 
+		$this->assertInternalType( 'object', $response );
+		$this->assertObjectHasAttribute( 'success', $response );
+		$this->assertFalse( $response->success );
+		$this->assertObjectHasAttribute( 'data', $response );
+		$this->assertObjectHasAttribute( 'message', $response->data );
+		$this->assertEquals( 'Nonce field doens\'t match', $response->data->message );
+	}
+
+	public function test_autocomplete_with_wrong_nonce() {
+		$_POST['_wpnonce'] = wp_create_nonce( 'wrong_nonce' );
+		$_POST['query']    = 'test';
+
+		try {
+			$this->_handleAjax( 'wl_autocomplete' );
+		} catch ( WPAjaxDieContinueException $e ) {}
+
+		$response = json_decode( $this->_last_response );
 
 		$this->assertInternalType( 'object', $response );
 		$this->assertObjectHasAttribute( 'success', $response );
@@ -60,7 +72,7 @@ class AjaxAutocompleteTest extends Wordlift_Ajax_Unit_Test_Case {
 		try {
 			$this->_handleAjax( 'wl_autocomplete' );
 		} catch ( WPAjaxDieContinueException $e ) {}
-	 
+
 		$response = json_decode( $this->_last_response );
 
 		$this->assertInternalType( 'object', $response );
@@ -69,25 +81,80 @@ class AjaxAutocompleteTest extends Wordlift_Ajax_Unit_Test_Case {
 		$this->assertObjectHasAttribute( 'data', $response );
 		$this->assertObjectHasAttribute( 'message', $response->data );
 		$this->assertEquals( 'The query param is empty!', $response->data->message );
-
 	}
 
-	public function test_autocomplete() {
+	public function test_autocomplete_with_nonce_with_empty_query_param() {
 		// Spoof the nonce in the POST superglobal
 		$_POST['_wpnonce'] = wp_create_nonce( 'wordlift_autocomplete' );
-		$_POST['query']    = 'test';
-
+		$_POST['query']    = '';
 
 		try {
 			$this->_handleAjax( 'wl_autocomplete' );
 		} catch ( WPAjaxDieContinueException $e ) {}
-	 
+
 		$response = json_decode( $this->_last_response );
 
 		$this->assertInternalType( 'object', $response );
 		$this->assertObjectHasAttribute( 'success', $response );
-		$this->assertTrue( $response->success );
+		$this->assertFalse( $response->success );
 		$this->assertObjectHasAttribute( 'data', $response );
-		$this->assertObjectHasAttribute( 'suggestions', $response->data );
+		$this->assertObjectHasAttribute( 'message', $response->data );
+		$this->assertEquals( 'The query param is empty!', $response->data->message );
 	}
+
+	public function test_autocomplete_error_status_code() {
+		// Spoof the nonce in the POST superglobal
+		$_POST['_wpnonce'] = wp_create_nonce( 'wordlift_autocomplete' );
+		$_POST['query']    = 'test';
+
+		add_filter( 'pre_http_request', array( $this, 'return_error_code' ), 100 );
+
+		try {
+			$this->_handleAjax( 'wl_autocomplete' );
+		} catch ( WPAjaxDieContinueException $e ) {}
+
+		$response = json_decode( $this->_last_response );
+
+		$this->assertInternalType( 'object', $response );
+		$this->assertObjectHasAttribute( 'success', $response );
+		$this->assertFalse( $response->success );
+		$this->assertObjectHasAttribute( 'data', $response );
+		$this->assertObjectHasAttribute( 'message', $response->data );
+		$this->assertEquals( 'Something went wrong.', $response->data->message );
+	}
+
+	public function test_autocomplete_wp_error() {
+		// Spoof the nonce in the POST superglobal
+		$_POST['_wpnonce'] = wp_create_nonce( 'wordlift_autocomplete' );
+		$_POST['query']    = 'test';
+
+		add_filter( 'pre_http_request', array( $this, 'return_wp_error' ), 100 );
+
+		try {
+			$this->_handleAjax( 'wl_autocomplete' );
+		} catch ( WPAjaxDieContinueException $e ) {}
+
+		$response = json_decode( $this->_last_response );
+
+		$this->assertInternalType( 'object', $response );
+		$this->assertObjectHasAttribute( 'success', $response );
+		$this->assertFalse( $response->success );
+		$this->assertObjectHasAttribute( 'data', $response );
+		$this->assertObjectHasAttribute( 'message', $response->data );
+		$this->assertEquals( 'WP Error!', $response->data->message );
+	}
+
+
+	public function return_wp_error() {
+		return new WP_Error( 'broke', 'WP Error!' );
+	}
+
+	public function return_error_code() {
+		return array(
+			'response' => array(
+				'code' => 404,
+			),
+		);
+	}
+
 }
