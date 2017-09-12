1f047e1b57df72a58f81488a33205426f7d0c5c8
Stanimir <stanimir.k.stoyanov@gmail.com>
2017-09-12 14:47:01 +0300

    see #552: Use constant to show `JSON-LD` button on 'Download Your Data' page

--- src/admin/partials/wordlift-admin-download-your-data.php
+++ src/admin/partials/wordlift-admin-download-your-data.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The HTML fragment for the 'Download Your Data' page.
  *
@@ -9,20 +8,27 @@
  * @package    Wordlift
  * @subpackage Wordlift/admin/partials
  */
+
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 
 <div class="wrap">
-	<h2><?php _ex( 'Download Your Data', 'Page title', 'wordlift' ); ?></h2>
+	<h2><?php echo esc_html_x( 'Download Your Data', 'Page title', 'wordlift' ); ?></h2>
 	<p><?php esc_html_e( 'Choose the format to download your data:', 'wordlift' ); ?></p>
 
-	<a href="<?php echo esc_url( admin_url( 'admin-ajax.php?action=wl_download_your_data&out=json' ) ); ?>"
-	   class="button wl-add-input wl-button"><?php esc_html_e( 'JSON-LD', 'wordlift' ); ?></a>
+	<?php
+	// Show the 'JSON-LD' button only if the constant is defined and set to true.
+	if ( defined( 'WL_CONFIG_DOWNLOAD_JSON_LD' ) && WL_CONFIG_DOWNLOAD_JSON_LD ) :
+	?>
+		<a href="<?php echo esc_url( admin_url( 'admin-ajax.php?action=wl_download_your_data&out=json' ) ); ?>"
+		   class="button wl-add-input wl-button"><?php esc_html_e( 'JSON-LD', 'wordlift' ); ?></a>
+	<?php endif ?>
+
 	<a href="<?php echo esc_url( admin_url( 'admin-ajax.php?action=wl_download_your_data&out=rdf' ) ); ?>"
 	   class="button wl-add-input wl-button"><?php esc_html_e( 'RDF/XML', 'wordlift' ); ?></a>
 	<a href="<?php echo esc_url( admin_url( 'admin-ajax.php?action=wl_download_your_data&out=ttl' ) ); ?>"
-	   class="button wl-add-input wl-button"><?php _ex( 'Turtle', 'File format, not the animal', 'wordlift' ); ?></a>
+	   class="button wl-add-input wl-button"><?php echo esc_html_x( 'Turtle', 'File format, not the animal', 'wordlift' ); ?></a>
 	<a href="<?php echo esc_url( admin_url( 'admin-ajax.php?action=wl_download_your_data&out=n3' ) ); ?>"
 	   class="button wl-add-input wl-button"><?php esc_html_e( 'N3', 'wordlift' ); ?></a>
 </div>
