9c7968f4dc23ad241e64c91721bea5773ec97f04
David Riccitelli <david@insideout.io>
2017-09-25 11:35:14 +0300

    fix git ssh config

--- .ci/travis/install_ssh_key.sh
+++ .ci/travis/install_ssh_key.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Protect the key file.
+chmd 600 $(pwd)/.ci/travis/id_rsa
+
+# Add the key file to the ssh config.
 (
 echo "Host gitlab.com"
 echo "    RSAAuthentication yes"
@@ -7,6 +11,7 @@ echo "    User git"
 echo "    IdentityFile $(pwd)/.ci/travis/id_rsa"
 )> ~/.ssh/config
 
+# Add gitlab to known hosts.
 echo "gitlab.com,52.167.219.168 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFSMqzJeV9rUzU4kWitGjeR4PWSa29SPqJ1fVkhtj3Hw9xjLVXVYrU9QlYWrOLXBpQ6KWjbjTDTdDkoohFzgbEY=" >> ~/.ssh/known_hosts
 
 cat ~/.ssh/config
