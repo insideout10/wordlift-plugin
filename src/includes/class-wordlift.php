e90524417805216b65624bdc302ff7358f92423c
David Riccitelli <david@insideout.io>
2018-01-15 11:08:23 +0100

    bump to 3.17.0

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -673,7 +673,7 @@ class Wordlift {
 	public function __construct() {
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.17.0-rc1';
+		$this->version     = '3.17.0';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
