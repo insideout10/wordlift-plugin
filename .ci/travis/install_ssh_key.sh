dc45858d02ad604f0c5f418e4c10b4428298e5d2
David Riccitelli <david@insideout.io>
2018-03-05 11:54:08 +0100

    check that wordlift-ui is successfully cloned

--- .ci/travis/install_ssh_key.sh
+++ .ci/travis/install_ssh_key.sh
@@ -6,7 +6,6 @@ chmod 600 $(pwd)/.ci/travis/id_rsa
 # Add the key file to the ssh config.
 (
 echo "Host gitlab.com"
-echo "    RSAAuthentication yes"
 echo "    User git"
 echo "    IdentityFile $(pwd)/.ci/travis/id_rsa"
 )> ~/.ssh/config
@@ -15,3 +14,6 @@ echo "    IdentityFile $(pwd)/.ci/travis/id_rsa"
 echo "gitlab.com,52.167.219.168 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFSMqzJeV9rUzU4kWitGjeR4PWSa29SPqJ1fVkhtj3Hw9xjLVXVYrU9QlYWrOLXBpQ6KWjbjTDTdDkoohFzgbEY=" >> ~/.ssh/known_hosts
 
 cat ~/.ssh/config
+
+echo "Testing repository clone..."
+git clone git@gitlab.com:wordlift/wordlift-ui.git /tmp/wordlift-ui
