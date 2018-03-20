4b33d61c2cf463e1a2936acbb048e9c3a252fe7a
David Riccitelli <david@insideout.io>
2018-03-20 14:51:18 +0100

    remove older tags

--- tags/3.14.4/admin/js/modules/wordlift.js
+++
@@ -1,8 +0,0 @@
-window.wp = window.wp || {};
-window.wp.wordlift = window.wp.wordlift || {};
-
-if ( typeof window.wp.wordlift.trigger === 'undefined' ) {
-	_.extend( window.wp.wordlift, Backbone.Events );
-}
-
-export default window.wp.wordlift;
