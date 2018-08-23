9257623beaec99ce5d879d5ad3dc9694fe2ecdea
David Riccitelli <david@insideout.io>
2018-08-23 16:01:46 +0200

    see #835: use HOCs for open and selected.

--- src-js/webpack/src/Edit/index.schema-class-tree.js
+++ src-js/webpack/src/Edit/index.schema-class-tree.js
@@ -11,97 +11,18 @@
  */
 import React from "react";
 import ReactDOM from "react-dom";
-import SchemaClassTree from "wordlift-for-schemaorg/src/SchemaClassTree";
+import { SchemaClassTree } from "wordlift-for-schemaorg";
 
-/**
- * Sort the provided data structure by name case insensitive.
- *
- * @since 3.20.0
- * @param {{name}[]} data An array of objects with the name field.
- * @returns {Object[]} The sorted array.
+import withLoader from "./SchemaClassTree/withLoader";
+import withProps from "./SchemaClassTree/withProps";
+
+/*
+ * `withLoader` provides us with the `open` property therefore goes after
+ * `withProps`.
  */
-const sortByNameCaseInsensitive = data =>
-  data.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-
-    if (nameA < nameB) return -1;
-
-    if (nameA > nameB) return 1;
-
-    return 0;
-  });
-
-/**
- * A HOC component which loads the tree data from WordPress.
- *
- * @since 3.20.0
- * @param {Object} Component The target component.
- * @returns {Object} The HOC component.
- */
-const withLoader = Component =>
-  class extends React.Component {
-    constructor(props) {
-      super(props);
-
-      // Bind the onData.
-      this.onData = this.onData.bind(this);
-
-      // Set the initial state.
-      this.state = {
-        // All the items in the tree.
-        items: [],
-        // The root items.
-        roots: []
-      };
-    }
-
-    /**
-     * As soon as the component is mounted, load the data from WordPress.
-     *
-     * @since 3.20.0
-     */
-    componentDidMount() {
-      wp.ajax
-        .post("wl_schemaorg_class", {})
-        .then(json => json["schemaClasses"])
-        .then(sortByNameCaseInsensitive)
-        .then(this.onData);
-    }
-
-    /**
-     * Receive the list of tree items.
-     *
-     * @since 3.20.0
-     * @param {{id, name, dashname, description, children}[]} items An array of tree item.
-     */
-    onData(items) {
-      // Select `thing` as root.
-      const roots = items.filter(item => "thing" === item.dashname);
-
-      this.setState({
-        // Set the items to the data.
-        items,
-        // Select `thing` as the root item.
-        roots,
-        // Map the roots to their id.
-        open: roots.map(item => item.id)
-      });
-    }
-
-    /**
-     * @inheritDoc
-     */
-    render() {
-      const { items, roots, open } = this.state;
-
-      return (
-        <Component {...this.props} items={items} roots={roots} open={open} />
-      );
-    }
-  };
-
-const DecoratedTree = withLoader(SchemaClassTree);
+const DecoratedTree = withLoader(
+  withProps("open", "selected")(SchemaClassTree)
+);
 
 /**
  * Add the SchemaClassTree.
@@ -174,10 +95,6 @@ window.addEventListener("load", () => {
   ReactDOM.render(
     <DecoratedTree
       selected={settings["entity_types"]}
-      onOpen={item => {}}
-      onClose={item => {}}
-      onSelect={item => {}}
-      onDeselect={item => {}}
       leafDecorator={leafDecorator}
     />,
     element
