ea90c7235aa7b4fd8cb62a48e707698a2e59a86c
David Riccitelli <david@insideout.io>
2018-09-27 10:53:42 +0200

    see #852: bump to 3.20.0-dev5.

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -712,7 +712,7 @@ class Wordlift {
 	public function __construct() {
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.20.0-dev4';
+		$this->version     = '3.20.0-dev5';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
