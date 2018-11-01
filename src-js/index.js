be8c60f8f1a8277daba37c95ec894743b27ed055
David Riccitelli <david@insideout.io>
2018-11-01 16:32:08 +0100

    see #761: remove old code from index.js to allow development work on SearchRankings

--- src-js/index.js
+++ src-js/index.js
@@ -3,38 +3,38 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
-import AddEntity from "./screens/Admin/screens/Edit/components/AddEntity";
+// import AddEntity from "./screens/Admin/screens/Edit/components/AddEntity";
 import SearchRankings from "./screens/Admin/screens/SearchRankings/App";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { editor: { selection: "Word" } };
-    this.store = createStore((state, action) => {
-      switch (action.type) {
-        case "TEXT_CHANGE":
-          return Object.assign({}, state, {
-            editor: { selection: action.payload }
-          });
-        default:
-          return state;
-      }
-    }, this.state);
-  }
-
-  render() {
-    return (
-      <Provider store={this.store}>
-        <div style={{ width: "250px" }}>
-          <AddEntity />
-        </div>
-      </Provider>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("button"));
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = { editor: { selection: "Word" } };
+//     this.store = createStore((state, action) => {
+//       switch (action.type) {
+//         case "TEXT_CHANGE":
+//           return Object.assign({}, state, {
+//             editor: { selection: action.payload }
+//           });
+//         default:
+//           return state;
+//       }
+//     }, this.state);
+//   }
+//
+//   render() {
+//     return (
+//       <Provider store={this.store}>
+//         <div style={{ width: "250px" }}>
+//           <AddEntity />
+//         </div>
+//       </Provider>
+//     );
+//   }
+// }
+//
+// ReactDOM.render(<App />, document.getElementById("button"));
 
 // Treemap.
 ReactDOM.render(<SearchRankings />, document.getElementById("treemap"));
