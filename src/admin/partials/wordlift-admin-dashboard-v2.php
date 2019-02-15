47356631ba299cd5600ab7f8e5d8d02460622d80
David Riccitelli <david@insideout.io>
2019-02-15 12:53:09 +0100

    see #879: add more styling

--- src/admin/partials/wordlift-admin-dashboard-v2.php
+++ src/admin/partials/wordlift-admin-dashboard-v2.php
@@ -59,14 +59,84 @@
         flex-wrap: wrap;
     }
 
-    #wl-dashboard-v2 .wl-dashboard__block__body > * {
-        flex: 0 0 50%;
+    #wl-dashboard-v2 .wl-dashboard__block__body > div
+        , #wl-dashboard__show-more ~ label > span {
+        padding: 4px 8px 4px 0;
+        box-sizing: border-box;
+    }
+
+    #wl-dashboard-v2 .wl-dashboard__block__body > div:nth-child(odd)
+        , #wl-dashboard__show-more ~ label > span {
+        text-align: right;
+        width: 120px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    #wl-dashboard-v2 .wl-dashboard__block__body > div:nth-child(even) {
+        flex: calc(100% - 120px);
+        background: #ebf6ff;
+    }
+
+    #wl-dashboard-v2 .wl-dashboard__block__body > label {
+        flex: 100%;
+        background: linear-gradient(to right, white 120px, #ebf6ff 120px 100%);
     }
 
     #wl-dashboard-v2 .wl-dashboard__block__body--locked {
         margin: 50px;
         text-align: center;
         font-size: 1.4em;
+    }
+
+    #wl-dashboard-v2 .wl-dashboard__legend::after {
+        content: ' ';
+        width: 10px;
+        height: 10px;
+        display: inline-block;
+        border-radius: 10px;
+        margin: 0 6px;
+    }
+
+    #wl-dashboard-v2 .wl-dashboard__bar {
+        height: 10px;
+        border-radius: 0 10px 10px 0;
+    }
+
+    #wl-dashboard-v2 .wl-dashboard__legend--entities::after, #wl-dashboard-v2 .wl-dashboard__bar--entities {
+        background: rgb(0, 154, 255);
+        background: linear-gradient(90deg, rgb(0, 154, 255) 0%, rgb(0, 179, 255) 50%, rgb(0, 206, 255) 100%);
+    }
+
+    #wl-dashboard-v2 .wl-dashboard__legend--posts::after, #wl-dashboard-v2 .wl-dashboard__bar--posts {
+        background: rgb(0, 66, 212);
+        background: linear-gradient(90deg, rgba(0, 66, 212, 1) 0%, rgba(0, 110, 238, 1) 50%, rgba(68, 147, 255, 1) 100%);
+    }
+
+    /* Hide the checkbox. */
+    #wl-dashboard__show-more {
+        display: none;
+    }
+
+    #wl-dashboard__show-more ~ label {
+        color: #0073aa;
+        transition-property: border, background, color;
+        transition-duration: .05s;
+        transition-timing-function: ease-in-out;
+        text-decoration: none;
+    }
+
+    #wl-dashboard__show-more ~ div
+        , #wl-dashboard__show-more ~ label > span:nth-child(2)
+        , #wl-dashboard__show-more:checked ~ label > span:nth-child(1) {
+        display: none;
+    }
+
+    #wl-dashboard__show-more:checked ~ div
+        , #wl-dashboard__show-more:checked ~ label > span:nth-child(2)
+        , #wl-dashboard__show-more ~ label > span:nth-child(1) {
+        display: block;
     }
 </style>
 <?php
@@ -116,16 +186,16 @@ $country_code = $configuration_service->get_country_code();
     <header>
         <span class="dashicons dashicons-editor-help"></span>
         <h3><?php echo __( 'Top entities', 'wordlift' ); ?></h3>
-        <span id="dashboard__link_with_entities"><?php echo esc_html( _x( 'Links with entities', 'Dashboard', 'wordlift' ) ); ?></span>
-        <span id="dashboard__post_with_entity"><?php echo esc_html( _x( 'Post with entities', 'Dashboard', 'wordlift' ) ); ?></span>
+        <span class="wl-dashboard__legend wl-dashboard__legend--entities"><?php echo esc_html( _x( 'Links with entities', 'Dashboard', 'wordlift' ) ); ?></span>
+        <span class="wl-dashboard__legend wl-dashboard__legend--posts"><?php echo esc_html( _x( 'Post with entities', 'Dashboard', 'wordlift' ) ); ?></span>
     </header>
     <div class="wl-dashboard__block__body">
 		<?php
 		$top_entities = $this->get_top_entities();
 		if ( ! empty( $top_entities ) ) {
 			$max = $top_entities[0]->total;
+			$i   = 0;
 			foreach ( $this->get_top_entities() as $post ) {
-
 				$permalink    = get_permalink( $post->ID );
 				$title        = $post->post_title;
 				$entities_100 = 100 * $post->entities / $max;
@@ -133,13 +203,26 @@ $country_code = $configuration_service->get_country_code();
 				?>
                 <div><a href="<?php echo esc_attr( $permalink ); ?>"><?php echo esc_html( $title ); ?></a></div>
                 <div>
-                    <div style="height: 10px; width: <?php echo $posts_100 ?>%; background: violet;">
+                    <div class="wl-dashboard__bar wl-dashboard__bar--posts" style="width: <?php echo $posts_100 ?>%;">
                     </div>
-                    <div style="height: 10px; width: <?php echo $entities_100 ?>%; background: salmon;">
+                    <div class="wl-dashboard__bar wl-dashboard__bar--entities"
+                         style="width: <?php echo $entities_100 ?>%;">
                     </div>
                 </div>
 				<?php
-
+				if ( 4 === $i ++ ) {
+					?>
+                    <input id="wl-dashboard__show-more" type="checkbox">
+                    <label for="wl-dashboard__show-more">
+                        <span>
+						    <?php echo esc_html( __( 'Show more', 'Dashboard', 'wordlift' ) ); ?>
+                        </span>
+                        <span>
+                            <?php echo esc_html( __( 'Hide', 'Dashboard', 'wordlift' ) ); ?>
+                        </span>
+                    </label>
+					<?php
+				}
 			}
 		}
 		?>
