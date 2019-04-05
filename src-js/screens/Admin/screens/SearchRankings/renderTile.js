4a3b3d8ebb185a428c684ddfe9fcac3848a675b8
David Riccitelli <david@insideout.io>
2019-04-05 11:53:22 +0200

    see #907: encode the URI component.

--- src-js/screens/Admin/screens/SearchRankings/renderTile.js
+++ src-js/screens/Admin/screens/SearchRankings/renderTile.js
@@ -8,26 +8,28 @@ function renderTile({ click, select }) {
   return function(node, elem) {
     if (node.data && node.data.entity && node.data.score) {
       node.data.entity.type = toType(node.data.entity.type);
+      node.data.entity.encodedURI = encodeURIComponent(node.data.entity.itemId);
       // Each star is 20px. The relative score is value between 0.0 and 1.0.
       node.data.entity.width = `${node.data.score.relative * 100}px`;
 
       // Hook the click event.
       elem.addEventListener("click", () => {
         const state = select();
-        console.debug({start:state});
-        const elClassListPrev = state && state.node ? state.node.elemClassList : null;
+        console.debug({ start: state });
+        const elClassListPrev =
+          state && state.node ? state.node.elemClassList : null;
         const elClassListNext = arguments[1].classList;
-        console.debug({prev:elClassListPrev, next:elClassListNext});
+        console.debug({ prev: elClassListPrev, next: elClassListNext });
         if (elClassListPrev !== elClassListNext) {
-          console.debug('mutation');
+          console.debug("mutation");
           if (elClassListPrev) {
-            elClassListPrev.remove('treemap__hierarchy__tile--selected');
+            elClassListPrev.remove("treemap__hierarchy__tile--selected");
             state.node.elemClassList = null;
           }
-          elClassListNext.add('treemap__hierarchy__tile--selected');
+          elClassListNext.add("treemap__hierarchy__tile--selected");
           node.data.elemClassList = elClassListNext;
         }
-        console.debug({node});
+        console.debug({ node });
         click(node, arguments);
       });
     }
@@ -36,7 +38,9 @@ function renderTile({ click, select }) {
       `
         {{#entity}}
         <div class="tile">
-          <a href="${window["wp"].ajax.settings.url}?action=wordlift_redirect&uri={{itemId}}&to=edit" class="tile__label">{{label}}</a>
+          <a href="${
+            window["wp"].ajax.settings.url
+          }?action=wordlift_redirect&uri={{encodedURI}}&to=edit" class="tile__label">{{label}}</a>
           <div class="tile__type">{{type}}</div>
           <div class="tile__score" style="width: {{width}};"></div>
         </div>
