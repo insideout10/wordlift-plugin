365864c4328cae8c374bc8a38457d96f47203bc2
David Riccitelli <david@insideout.io>
2019-06-20 20:13:48 +0200

    see #913: fix test

--- tests/test-schema-service.php
+++ tests/test-schema-service.php
@@ -169,7 +169,7 @@ class Wordlift_Schema_Service_Test extends Wordlift_Unit_Test_Case {
 		$this->assertArrayHasKey( 'entity_same_as', $event_schema['custom_fields'] );
 
 		// This value may change if we add/remove properties from `linked_data`.
-		$this->assertCount( 17, $event_schema['linked_data'] );
+		$this->assertCount( 18, $event_schema['linked_data'] );
 
 	}
 
