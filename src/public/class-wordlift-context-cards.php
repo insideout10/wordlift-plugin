5410751beb852ad00218a4c32507788e05460217
Akshay Raje <akshay.raje@gmail.com>
2019-08-25 02:50:41 +0530

    #934 Minor enhancement

--- src/public/class-wordlift-context-cards.php
+++ src/public/class-wordlift-context-cards.php
@@ -9,14 +9,13 @@
 
 class Wordlift_Context_Cards_Service {
 
-	protected $entity_uri_service;
-
 	function __construct($entity_uri_service) {
 
 		$this->entity_uri_service = $entity_uri_service;
+		$this->endpoint = '/jsonld';
 
 		add_action( 'rest_api_init', function () {
-			register_rest_route( WL_REST_ROUTE_DEFAULT_NAMESPACE, '/context', array(
+			register_rest_route( WL_REST_ROUTE_DEFAULT_NAMESPACE, $this->endpoint, array(
 				'methods' => 'GET',
 				'callback' => array($this, 'context_data')
 			) );
@@ -29,7 +28,7 @@ class Wordlift_Context_Cards_Service {
 	function context_data( $request ) {
 
 		$entity_uri = $request->get_param( 'entity_url' );
-		//$entity_id = $this->entity_uri_service->get_entity( $entity_uri );
+		//$entity_id = Wordlift_Entity_Service::get_instance()->get_entity_post_by_uri( $entity_uri );
 		$entity_id = url_to_postid( $entity_uri );
 		$jsonld = Wordlift_Jsonld_Service::get_instance()->get_jsonld( false, $entity_id );
 		return $jsonld;
@@ -38,7 +37,7 @@ class Wordlift_Context_Cards_Service {
 
 	function enqueue_script() {
 		wp_enqueue_script( 'wordlift-cloud', plugin_dir_url( dirname( __FILE__ ) ) . 'js/dist/wordlift-cloud.js', array(), false, true );
-		wp_add_inline_script( 'wordlift-cloud', "wordliftCloud.contextCards('a.wl-entity-page-link', '".get_rest_url() . WL_REST_ROUTE_DEFAULT_NAMESPACE."/context')" );
+		wp_add_inline_script( 'wordlift-cloud', "wordliftCloud.contextCards('a.wl-entity-page-link', '".get_rest_url() . WL_REST_ROUTE_DEFAULT_NAMESPACE . $this->endpoint."')" );
 	}
 
 }
