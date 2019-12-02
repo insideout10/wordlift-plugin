3b8ad440a59ebb47172f8b11c15447745c631f0b
naveen <kmnaveen101@gmail.com>
2019-12-02 11:30:28 +0530

    see #953: html added for the mockup

--- src/admin/partials/wordlift-admin-mappings-page-mockup.php
+++ src/admin/partials/wordlift-admin-mappings-page-mockup.php
@@ -1,2 +1,76 @@
+<!DOCTYPE html>
 <html>
+    <head>
+        <h3>
+            Mappings
+            <button>Add New</button>
+        </h3>
+    </head>
+    <table>
+        <tr>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                Title
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                My custom post type
+                <br/>
+                <b>
+                    <a>
+                        Edit
+                    </a>|
+                    <a>
+                        Duplicate
+                    </a>|
+                    <a>
+                        Trash
+                    </a>|
+                </b>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                Another custom post type
+                <br />
+                <b>
+                    <a>
+                        Edit
+                    </a>|
+                    <a>
+                        Duplicate
+                    </a>|
+                    <a>
+                        Trash
+                    </a>|
+                </b>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                Title
+            </td>
+        </tr>
+    </table>
+
+    <select>
+        <option value="-1">Bulk Actions</option>
+        <option value="duplicate">Duplicate</option>
+        <option value="trash">Move to Trash</option>
+    </select>
 </html>
