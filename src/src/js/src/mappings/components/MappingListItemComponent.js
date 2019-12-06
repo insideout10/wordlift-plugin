bf6e3fb7f775269b71cfa5c1107dcd38c17201da
naveen <kmnaveen101@gmail.com>
2019-12-06 11:56:07 +0530

    [GREEN] test given title rendering properly

--- src/src/js/src/mappings/components/MappingListItemComponent.js
+++ src/src/js/src/mappings/components/MappingListItemComponent.js
@@ -1,6 +1,9 @@
 import React from "react"
 export default class MappingListItemComponent extends
 React.Component {
+    constructor(props) {
+        super(props)
+    }
     render() {
         return <p></p>
     }
--- src/src/js/src/mappings/tests/MappingListItemComponent.test.js
+++ src/src/js/src/mappings/tests/MappingListItemComponent.test.js
@@ -10,3 +10,8 @@ import MappingListItemComponent from '../components/MappingListItemComponent'
 test("check whether mapping list item can be rendered", ()=> {
     renderer.create(<MappingListItemComponent />)
 })
+
+test("check provided property rendering properly", ()=> {
+   const component = renderer.create(<MappingListItemComponent title="foo" />).root
+   expect(component.props.title).toBe("foo")
+})
