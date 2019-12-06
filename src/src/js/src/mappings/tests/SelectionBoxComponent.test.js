78a8a1a7cc30ed86482889cded66f515e550531a
naveen <kmnaveen101@gmail.com>
2019-12-06 13:12:59 +0530

    [RED] option count test failing

--- src/src/js/src/mappings/tests/SelectionBoxComponent.test.js
+++ src/src/js/src/mappings/tests/SelectionBoxComponent.test.js
@@ -10,3 +10,21 @@ import SelectionBoxComponent from '../components/SelectionBoxComponent'
 test("check whether selection box component can be rendered", ()=> {
     renderer.create(<SelectionBoxComponent />)
 })
+
+test("provided options via property, then renders the options", ()=> {
+    const options = [
+        {
+            text:"foo",
+            value: "bar"
+        },
+    ]
+    const componentRoot = renderer
+    .create(<SelectionBoxComponent options={options} />).root
+
+    // now check if the options got rendered
+    const optionCount = componentRoot.findAll(
+        (el) => el.type == 'option'
+    ).length
+    // the options should be 1
+    expect(optionCount).toBe(1)
+})
