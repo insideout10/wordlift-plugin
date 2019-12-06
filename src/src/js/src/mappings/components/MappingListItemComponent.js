3b408dd259ba4ec15640ec5f2289bead0afe3e7a
naveen <kmnaveen101@gmail.com>
2019-12-06 11:29:20 +0530

    [GREEN] Test passing for ability to render mapping list item component

---
+++ src/src/js/src/mappings/components/MappingListItemComponent.js
@@ -0,0 +1,7 @@
+import React from "react"
+export default class MappingListItemComponent extends
+React.Component {
+    render() {
+        return <p></p>
+    }
+}
--- src/src/js/src/mappings/tests/MappingListItemComponent.test.js
+++ src/src/js/src/mappings/tests/MappingListItemComponent.test.js
@@ -5,7 +5,7 @@
  */
 import React from "react";
 import renderer from 'react-test-renderer';
-
+import MappingListItemComponent from '../components/MappingListItemComponent'
 
 test("check whether mapping list item can be rendered", ()=> {
     renderer.create(<MappingListItemComponent />)
