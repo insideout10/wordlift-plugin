a87d8d651590cc03d73056e1161bb8c8a765796f
naveen <kmnaveen101@gmail.com>
2019-12-09 10:35:11 +0530

    Rule component finished

--- src/src/js/src/mappings/components/RuleComponent.js
+++ src/src/js/src/mappings/components/RuleComponent.js
@@ -4,25 +4,30 @@ import Select from 'react-select'
 export default class RuleComponent extends React.Component {
     constructor(props) {
         super(props)
-        console.log(props + " from rule component")
     }
     render() {
         return (
                 <div className="wl-container">
                     <div className="wl-col">
-                        <Select value={this.props.field_one_options[0]} options={this.props.field_one_options} className="wl-field-one-select wl-form-select">
+                        <Select options={this.props.rule_field_one_options}
+                         className="wl-field-one-select wl-form-select">
                         </Select>
                     </div>
                     <div className="wl-col">
-                        <Select options={this.props.logic_field_options} className="wl-field-logic wl-form-select">
+                        <Select options={this.props.rule_logic_field_options}
+                         className="wl-field-logic wl-form-select">
                         </Select>
                     </div>
                     <div className="wl-col">
-                        <Select options={this.props.field_two_options} className="wl-field-two-select wl-form-select">
+                        <Select options={this.props.rule_field_two_options}
+                         className="wl-field-two-select wl-form-select">
                         </Select>
                     </div>
                     <div className="wl-col">
-                        <button className="button action"> And </button>
+                        <button className="button action wl-and-button"
+                         onClick={this.props.addNewRuleHandler(this.props.rule_index)}>
+                             And 
+                         </button>
                     </div>
                 </div>
 
--- src/src/js/src/mappings/tests/RuleComponent.test.js
+++ src/src/js/src/mappings/tests/RuleComponent.test.js
@@ -12,26 +12,44 @@ const options = [
 ]
 
 test("should be able to render rule component", ()=> {
-    shallow(<RuleComponent />)
+    const addNewRuleMockHandler = jest.fn()
+    shallow(<RuleComponent addNewRuleHandler={addNewRuleMockHandler}/>)
 })
 
 test("given options field one rendering correctly", ()=> {
-    const wrapper = shallow(<RuleComponent field_one_options={options}/>)
+    const addNewRuleMockHandler = jest.fn()
+    const wrapper = shallow(<RuleComponent 
+        rule_field_one_options={options} 
+        addNewRuleHandler={addNewRuleMockHandler}/>)
     // 3 items supplied, so it should be reflected in ui
     expect(wrapper.find('.wl-field-one-select').props().options)
     .toHaveLength(3)
 })
 
 test("given options logic field rendering correctly", ()=> {
-    const wrapper = shallow(<RuleComponent logic_field_options={options}/>)
+    const addNewRuleMockHandler = jest.fn()
+    const wrapper = shallow(<RuleComponent 
+        rule_logic_field_options={options} 
+        addNewRuleHandler={addNewRuleMockHandler}/>)   
     // 3 items supplied, so it should be reflected in ui
     expect(wrapper.find('.wl-field-logic').props().options)
     .toHaveLength(3)
 })
 
 test("given options field two rendering correctly", ()=> {
-    const wrapper = shallow(<RuleComponent field_two_options={options}/>)
+    const addNewRuleMockHandler = jest.fn()
+    const wrapper = shallow(<RuleComponent 
+        rule_field_two_options={options} 
+        addNewRuleHandler={addNewRuleMockHandler}/>)  
     // 3 items supplied, so it should be reflected in ui
     expect(wrapper.find('.wl-field-two-select').props().options)
     .toHaveLength(3)
+})
+
+test("when and button clicked, add additional row below", ()=> {
+    const addNewRuleMockHandler = jest.fn()
+    const wrapper = shallow(<RuleComponent 
+        addNewRuleHandler={addNewRuleMockHandler}/>)
+    wrapper.find('.wl-and-button').simulate('click')
+    expect(addNewRuleMockHandler.mock.calls.length).toEqual(1)
 })
