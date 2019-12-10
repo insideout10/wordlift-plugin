1ce74f4ad8608a829bd0b5d59426e1673d0577c9
David Riccitelli <david@wordlift.io>
2019-12-10 18:16:26 +0200

    bump to 3.23.2

---
+++ tags/3.23.2/admin/WL_Metabox/WL_Metabox_Field_date.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Metaxboxes: Date Field.
+ *
+ * This file defines the WL_Metabox_Field_date class which displays a date field
+ * in WordPress' entity posts pages.
+ *
+ * @since   3.2.0
+ * @package Wordlift
+ */
+
+/**
+ * The WL_Metabox_Field_date class extends {@link WL_Metabox_Field} and provides
+ * support for date fields.
+ *
+ * @since   3.2.0
+ * @package Wordlift
+ */
+class WL_Metabox_Field_date extends WL_Metabox_Field {
+
+	/**
+	 * Attribute to distinguish between date formats, inferred from the schema property export type
+	 *
+	 * @since  3.2.0
+	 * @access protected
+	 * @var string $date_format The date format.
+	 */
+	protected $date_format;
+
+	/**
+	 * Boolean flag to decide if the calendar should include time or not
+	 *
+	 * @since  3.2.0
+	 * @access protected
+	 * @var boolean $timepicker A boolean flag.
+	 */
+	protected $timepicker;
+
+	/**
+	 * Whether the calendar should be displayed or not.
+	 *
+	 * @since  3.14.0
+	 * @access protected
+	 * @var boolean $no_calendar Whether the calendar should be displayed or not.
+	 */
+	protected $no_calendar;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __construct( $args ) {
+		parent::__construct( $args );
+
+		$this->no_calendar = false;
+
+		// Distinguish between date and datetime
+		if ( isset( $this->raw_custom_field['export_type'] ) && 'xsd:dateTime' === $this->raw_custom_field['export_type'] ) {
+			$this->date_format = 'Y/m/d H:i';
+			$this->timepicker  = true;
+		} else {
+			$this->date_format = 'Y/m/d';
+			$this->timepicker  = false;
+		}
+
+	}
+
+	/**
+	 * @param mixed $date
+	 *
+	 * @return string
+	 */
+	public function html_input( $date ) {
+
+		$this->log->debug("Creating date input with date value $date...");
+
+		ob_start();
+		?>
+			<div class="wl-input-wrapper">
+				<input
+					type="text"
+					class="<?php echo esc_attr( $this->meta_name ); ?>"
+					name="wl_metaboxes[<?php echo $this->meta_name ?>][]"
+					value="<?php echo $date ?>"
+					style="width:88%"
+				/>
+
+				<button class="button wl-remove-input wl-button" type="button">
+					<?php esc_html_e( 'Remove', 'wordlift' ); ?>
+				</button>
+			</div>
+		<?php
+		$html = ob_get_clean();
+
+		return $html;
+	}
+
+	public function html_wrapper_close() {
+
+		// Should the widget include time picker?
+		$timepicker  = json_encode( $this->timepicker );
+		$date_format = json_encode( $this->date_format );
+		$no_calendar = json_encode( $this->no_calendar );
+
+		// Set up the datetimepicker.
+		//
+		// See https://github.com/trentrichardson/jQuery-Timepicker-Addon
+		// See in http://trentrichardson.com/examples/timepicker.
+		@ob_start();
+		?>
+			<script type='text/javascript'>
+				( function( $ ) {
+
+					$( function() {
+
+						$( '.<?php echo $this->meta_name; ?>[type=text]' ).flatpickr( {
+							enableTime: <?php echo $timepicker; ?>,
+							noCalendar: <?php echo $no_calendar; ?>,
+							time_24hr: true,
+							dateFormat: <?php echo $date_format; ?>
+						 } );
+					} );
+				} ) ( jQuery );
+			</script>
+		<?php
+		$html = ob_get_clean();
+
+		$html .= parent::html_wrapper_close();
+
+		return $html;
+	}
+
+}
---
+++ tags/3.23.2/public/class-wordlift-context-cards.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Context Cards Service
+ *
+ * @since      3.22.0
+ * @package    Wordlift
+ * @subpackage Wordlift/public
+ */
+
+class Wordlift_Context_Cards_Service {
+
+	/**
+	 * @var string
+	 */
+	private $endpoint;
+
+	function __construct() {
+
+		$this->endpoint = '/jsonld';
+
+		// PHP 5.3 compatibility as `$this` cannot be used in closures.
+		$that = $this;
+
+		add_action( 'rest_api_init', function () use ( $that ) {
+			register_rest_route( WL_REST_ROUTE_DEFAULT_NAMESPACE, $that->endpoint, array(
+				'methods'  => 'GET',
+				'callback' => array( $that, 'context_data' ),
+			) );
+		} );
+
+	}
+
+	public function context_data( $request ) {
+
+		$entity_uri = urldecode( $request->get_param( 'entity_url' ) );
+		$entity_id  = $this->url_to_postid( $entity_uri );
+		$jsonld     = Wordlift_Jsonld_Service::get_instance()->get_jsonld( false, $entity_id );
+
+		return $jsonld;
+
+	}
+
+	public function enqueue_scripts() {
+		$show_context_cards = true;
+		$show_context_cards = apply_filters( 'wl_show_context_cards', $show_context_cards );
+		if ( $show_context_cards ) {
+			wp_enqueue_script( 'wordlift-cloud' );
+			wp_add_inline_script( 'wordlift-cloud', "window.addEventListener( 'load', function() { wordliftCloud.contextCards('a.wl-entity-page-link', '" . get_rest_url() . WL_REST_ROUTE_DEFAULT_NAMESPACE . $this->endpoint . "'); } );" );
+		}
+	}
+
+	private function url_to_postid( $url ) {
+		// Try with url_to_postid
+		$post_id = url_to_postid( $url );
+		if ( $post_id == 0 ) {
+			// Try with get_page_by_path
+			$post = get_page_by_path( basename( untrailingslashit( $url ) ), OBJECT, 'entity' );
+			if ( $post ) {
+				$post_id = $post->ID;
+			}
+		}
+
+		return $post_id;
+	}
+
+}
