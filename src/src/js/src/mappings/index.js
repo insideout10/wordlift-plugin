b5ad0119a940e670ef8be059811edc3fb033bf39
naveen <kmnaveen101@gmail.com>
2019-12-12 12:45:21 +0530

    mapping table screen completed

--- src/src/js/src/mappings/index.js
+++ src/src/js/src/mappings/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import EditComponent from './components/EditComponent'
+import MappingComponent from './components/MappingComponent'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import { createStore } from 'redux'
 import {MOCK_INITIAL_STATE, mock_reducers } from './tests/MockStore'
 const MOCK_STORE = createStore(mock_reducers, MOCK_INITIAL_STATE)
 
+// ReactDOM.render(
+//     <Provider store={MOCK_STORE}>
+//         <EditComponent />
+//     </Provider>,
+//     document.getElementById("container"))
+
 ReactDOM.render(
-    <Provider store={MOCK_STORE}>
-        <EditComponent />
-    </Provider>,
+    <MappingComponent mappingItems={[{title:'foo'}, {title:'sample'}]}/>,
     document.getElementById("container"))
