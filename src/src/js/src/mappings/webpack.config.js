340604988db75d38470254f436b269c881f03252
naveen <kmnaveen101@gmail.com>
2019-12-12 05:47:03 +0530

    sync mappings page styling added from mockup

--- src/src/js/src/mappings/webpack.config.js
+++ src/src/js/src/mappings/webpack.config.js
@@ -8,20 +8,8 @@ module.exports = {
   // Here the application starts executing
   // and webpack starts bundling
   output: {
-    // options related to how webpack emits results
-    path: path.resolve(__dirname, "dist"), // string
-    // the target directory for all output files
-    // must be an absolute path (use the Node.js path module)
-    filename: "bundle.js", // string
-    // the filename template for entry chunks
-    publicPath: "/assets/", // string
-    // the url to the output directory resolved relative to the HTML page
-    library: "MyLibrary", // string,
-    // the name of the exported library
-    libraryTarget: "umd", // universal module definition
-    // the type of the exported library
-    /* Advanced output configuration (click to show) */
-    /* Expert output configuration (on own risk) */
+    filename: "mappings.js",
+    path: path.resolve(__dirname, "../../../../js/dist")
   },
   module: {
     rules: [
