34126767964be6b07d5c3177cb6492ab6a6e19da
David Riccitelli <david@wordlift.io>
2019-12-19 18:33:51 +0200

    add check for php configuration.

--- .ci/travis/install_requirements.sh
+++ .ci/travis/install_requirements.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get install -y php5-gd
+# sudo apt-get update
+# sudo apt-get install -y php5-gd
 
-echo "extension=gd.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+# echo "extension=gd.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 php -i
