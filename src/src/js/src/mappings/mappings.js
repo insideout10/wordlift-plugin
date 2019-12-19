48b371f7a798c27f0ae5bff02410a066b405ab00
naveen <kmnaveen101@gmail.com>
2019-12-19 11:06:07 +0530

    preparing edit screen to connect to rest api

--- src/src/js/src/mappings/mappings.js
+++ src/src/js/src/mappings/mappings.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import EditComponent from './components/EditComponent'
 import MappingComponent from './components/MappingComponent'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
 import { createStore } from 'redux'
 import {MOCK_INITIAL_STATE, mock_reducers } from './tests/MockStore'
 import './mappings.css'
