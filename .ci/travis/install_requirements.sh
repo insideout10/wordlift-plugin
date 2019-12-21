19db9038db84ebb5480eac7bd669708977909e1d
David Riccitelli <david@wordlift.io>
2019-12-21 16:37:17 +0200

    Delete install_requirements.sh

--- .ci/travis/install_requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-# sudo apt-get install -y php5-gd
-sudo apt-get install -y curl php5-curl
-# echo "extension=gd.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
-php -i
