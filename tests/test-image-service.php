ebb763763afd27c4cb74d5df72b4929726f64b1a
David Riccitelli <david@wordlift.io>
2019-12-21 12:00:11 +0200

    revert to WikiCommons images.

--- tests/test-image-service.php
+++ tests/test-image-service.php
@@ -42,7 +42,7 @@ class Wordlift_Image_Service_Test extends Wordlift_Unit_Test_Case {
 	 * @since 3.18.0
 	 */
 	public function test_png() {
-		$response = Wordlift_Remote_Image_Service::save_from_url( 'https://cdn.wordlift.io/blog/en/wp-content/uploads/sites/3/2017/10/search-marketing192x192.png' );
+		$response = Wordlift_Remote_Image_Service::save_from_url( 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Flag_of_Rome.svg/2000px-Flag_of_Rome.svg.png' );
 
 		$this->assertFalse( is_wp_error( $response ), 'Error: ' . ( is_wp_error( $response ) ? $response->get_error_message() : 'Unknown' ) );
 		$this->assertInternalType( 'array', $response );
@@ -56,7 +56,7 @@ class Wordlift_Image_Service_Test extends Wordlift_Unit_Test_Case {
 	 * @since 3.18.0
 	 */
 	public function test_gif() {
-		$response = Wordlift_Remote_Image_Service::save_from_url( 'https://wordlift.io/wp-content/uploads/2017/07/GIF_2_Chord_1.0.gif' );
+		$response = Wordlift_Remote_Image_Service::save_from_url( 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Rotating_earth_%28large%29.gif/200px-Rotating_earth_%28large%29.gif' );
 
 		$this->assertFalse( is_wp_error( $response ), 'Error: ' . ( is_wp_error( $response ) ? $response->get_error_message() : 'Unknown' ) );
 		$this->assertInternalType( 'array', $response, 'Expecting array, instead got: ' . var_export( $response, true ) );
@@ -70,7 +70,7 @@ class Wordlift_Image_Service_Test extends Wordlift_Unit_Test_Case {
 	 * @since 3.18.0
 	 */
 	public function test_jpg() {
-		$response = Wordlift_Remote_Image_Service::save_from_url( 'https://wordlift.io/wp-content/uploads/2017/07/grunauer.jpg' );
+		$response = Wordlift_Remote_Image_Service::save_from_url( 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Wikipedia_logo_593.jpg' );
 
 		$this->assertFalse( is_wp_error( $response ), 'Error: ' . ( is_wp_error( $response ) ? $response->get_error_message() : 'Unknown' ) );
 		$this->assertInternalType( 'array', $response );
