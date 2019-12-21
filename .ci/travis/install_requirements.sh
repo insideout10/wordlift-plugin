6a4eb14d50b2eb6470a8b03ca9e94a8b9b19193a
David Riccitelli <david@wordlift.io>
2019-12-21 12:45:58 +0200

    try updating curl.

--- .ci/travis/install_requirements.sh
+++ .ci/travis/install_requirements.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 # sudo apt-get install -y php5-gd
-apt-get install -y curl php5-curl
+sudo apt-get install -y curl php5-curl
 # echo "extension=gd.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 php -i
