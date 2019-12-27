56cc55562c30a4c1e56c8d7a401af2de13f7f8ed
David Riccitelli <david@wordlift.io>
2019-12-27 13:10:45 +0100

    bump to 3.23.5.

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -742,7 +742,7 @@ class Wordlift {
 		self::$instance = $this;
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.23.4';
+		$this->version     = '3.23.5';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
