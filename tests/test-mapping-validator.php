8871da783dc45de21c3cce63e600e9cfc2e6843f
naveen <kmnaveen101@gmail.com>
2019-12-30 11:26:33 +0530

    Test for validating rulegroup fail

--- tests/test-mapping-validator.php
+++ tests/test-mapping-validator.php
@@ -19,12 +19,49 @@ class Wordlift_Mapping_Validator_Test extends WP_UnitTestCase {
 	 *
 	 * @since  3.25.0
 	 * @access private
-	 * @var \Wordlift_Mapping_Validator $wordlift_mapping_validator_instance The {@link Wordlift_Mapping_Validator} instance to test.
+	 * @var \Wordlift_Mapping_Validator $validator The {@link Wordlift_Mapping_Validator} instance to test.
 	 */
-	private $wordlift_mapping_validator_instance;
+	private $validator;
 
+	/**
+	 * The {@link Wordlift_Mapping_DBO} instance to test.
+	 *
+	 * @since  3.25.0
+	 * @access private
+	 * @var \Wordlift_Mapping_DBO $dbo The {@link Wordlift_Mapping_DBO} instance to test.
+	 */
+	private $dbo;
+
+	/**
+	 * @inheritdoc
+	 */
+	public function setUp() {
+		parent::setUp();	
+		$this->dbo = new Wordlift_Mapping_DBO();
+	}
 	/** Check if validator class can be initalised */
 	public function test_can_initialize_validator() {
 		$this->assertNotNull( new Wordlift_Mapping_Validator() );
+	}
+
+	/** When the rules didnt match the post type it should return false */
+	public function test_given_post_id_and_wrong_rules_should_return_false() {
+		// Create a post with no post type.
+		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+		// Create a mapping item with single rule group and rule.
+		$mapping_id = $this->dbo->insert_mapping_item( 'foo' );
+		// Create a rule group.
+		$rule_group_id = $this->dbo->insert_rule_group( $mapping_id );
+		// Create a rule to match post type foo.
+		$rule_id = $this->dbo->insert_or_update_rule_item(
+			array(
+				'rule_field_one'   => 'post_type',
+				'rule_logic_field' => '===',
+				'rule_field_two'   => 'foo',
+				'rule_group_id'    => $rule_group_id,
+			)
+		);
+		// Since this post didnt have that post type, it should return false.
+		$this->assertFalse( $this->validator->validate( $post_id ) );
 	}
 }
