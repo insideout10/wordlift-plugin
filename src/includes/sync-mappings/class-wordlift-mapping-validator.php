0307a066a3faf2b7fb3db35b9db6e0d316b2effd
naveen <kmnaveen101@gmail.com>
2019-12-30 11:04:47 +0530

    Adding validator class

---
+++ src/includes/sync-mappings/class-wordlift-mapping-validator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Define the {@link Wordlift_Mapping_Validator} class.
+ * Validates the mapping for single post and return the
+ * schema.org properties mapped to ACF, custom field or text
+ * to be used for JSON-LD output.
+ *
+ * @since      3.25.0
+ * @package    Wordlift
+ * @subpackage Wordlift/includes/sync-mappings
+ */
+final class Wordlift_Mapping_Validator {
+
+}
