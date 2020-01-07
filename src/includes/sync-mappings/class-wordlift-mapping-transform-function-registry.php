5183df66b6ccb5e7887809aad448debe92a5241b
naveen <kmnaveen101@gmail.com>
2020-01-07 12:10:09 +0530

    Creating registry class for transform functions

---
+++ src/includes/sync-mappings/class-wordlift-mapping-transform-function-registry.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The class to hold all the transform functions for the JSON-LD Mapping.
+ * Define the {@link Wordlift_Mapping_Transform_Function_Registry} class.
+ *
+ * @since      3.25.0
+ * @package    Wordlift
+ * @subpackage Wordlift/includes/sync-mappings
+ */
+class Wordlift_Mapping_Transform_Function_Registry {
+
+}
