76590b77ad0db0f5f60a10b8104a531fd401c457
naveen <kmnaveen101@gmail.com>
2020-01-17 13:25:20 +0530

    see #953: Removing mapping converter from jsonld service

--- src/includes/class-wordlift-jsonld-service.php
+++ src/includes/class-wordlift-jsonld-service.php
@@ -185,11 +185,7 @@ class Wordlift_Jsonld_Service {
 				// via the `@id` but no other properties are loaded.
 				return $entity_to_jsonld_converter->convert( $item, $references );
 			}, $references ) ) );
-
-		$mapping_converter_instance = new Wordlift_Mapping_Jsonld_Converter( $post_id, $jsonld );
-
-		// Apply the mappings and return json ld.
-		return $mapping_converter_instance->get_jsonld_data();
+		return $jsonld;
 	}
 
 	/**
