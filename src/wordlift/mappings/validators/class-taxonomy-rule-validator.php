b8b1979b4c3217ea1a8facc789d5a648daa206fd
David Riccitelli <david@wordlift.io>
2020-01-22 12:44:15 +0100

    see #953: refactor the taxonomy rule validator.

--- src/wordlift/mappings/validators/class-taxonomy-rule-validator.php
+++ src/wordlift/mappings/validators/class-taxonomy-rule-validator.php
@@ -21,26 +21,17 @@ class Taxonomy_Rule_Validator implements Rule_Validator {
 	 */
 	public function is_valid( $post_id, $operator, $operand_1, $operand_2 ) {
 
-		$taxonomy       = $operand_1;
-		$terms          = get_the_terms( $post_id, $taxonomy );
-		$terms_id_array = array();
-		if ( is_wp_error( $terms ) || 0 === count( $terms ) ) {
-			// If no terms present then the rule is invalid.
+		$taxonomy  = $operand_1;
+		$term_slug = $operand_2;
+
+		$is_object_in_term = is_object_in_term( $post_id, $taxonomy, $term_slug );
+
+		if ( is_wp_error( $is_object_in_term ) ) {
 			return false;
 		}
-		foreach ( $terms as $term ) {
-			array_push( $terms_id_array, (string) $term->term_id );
-		}
-		if ( self::IS_EQUAL_TO === $operator ) {
-			// Rule is made to check if the term is present for post, so
-			// do in_array.
-			return in_array( (string) $operand_2, $terms_id_array, true );
-		} elseif ( self::IS_NOT_EQUAL_TO === $operator ) {
-			// The term  should not be present in the post terms.
-			return ! in_array( (string) $operand_2, $terms_id_array, true );
-		}
 
-		return false;
+		return ( $is_object_in_term && self::IS_EQUAL_TO === $operator )
+		       || ( ! $is_object_in_term && self::IS_NOT_EQUAL_TO === $operator );
 	}
 
 	/**
