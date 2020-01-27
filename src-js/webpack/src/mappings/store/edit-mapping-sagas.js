fbaff92d39ca7b550d8bcd2b7f85f8cd74f6e81d
naveen <kmnaveen101@gmail.com>
2020-01-27 13:36:33 +0530

    see #953: Moving API calls from edit mapping component to saga

---
+++ src-js/webpack/src/mappings/store/edit-mapping-sagas.js
@@ -0,0 +1,12 @@
+/**
+ * This file provides the sagas for the edit mappings screen.
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ * @since 3.25.0
+ */
+
+
+function* editMappingSaga() {
+
+}
+
+export default editMappingSaga;
