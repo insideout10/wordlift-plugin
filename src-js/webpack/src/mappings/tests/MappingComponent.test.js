876b175ed3755d7816bf80940eed239d57659bd6
naveen <kmnaveen101@gmail.com>
2020-01-30 14:48:25 +0530

    see #953: saving the changes to test

--- src-js/webpack/src/mappings/tests/MappingComponent.test.js
+++ src-js/webpack/src/mappings/tests/MappingComponent.test.js
@@ -8,15 +8,45 @@ import React from "react";
 import { Provider } from "react-redux";
 import MappingComponent from "../components/mapping-component";
 import store from "../store/index";
-import { mount, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import MappingComponentHelper from "../components/mapping-component/mapping-component-helper";
 
 configure({ adapter: new Adapter() });
 test("can render edit component", () => {
-    global.MockHttpServer.enqueueResponse([{}])
+  global.MockHttpServer.enqueueResponse([{}]);
   mount(
     <Provider store={store}>
       <MappingComponent />
     </Provider>
   );
+});
+
+test("when given mapping items ui works correctly", () => {
+  const mockResponse = MappingComponentHelper.applyApiFilters([
+    {
+      mapping_id: "11",
+      mapping_title: "item 1",
+      mapping_status: "active"
+    },
+    {
+      mapping_id: "12",
+      mapping_title: "item 2",
+      mapping_status: "active"
+    },
+    {
+      mapping_id: "13",
+      mapping_title: "item 3",
+      mapping_status: "trash"
+    }
+  ]);
+  global.MockHttpServer.enqueueResponse([mockResponse]);
+    // Now when rendering the component our ui will get this data by the mocked fetch method.
+    const component = mount(
+        <Provider store={store}>
+            <MappingComponent />
+        </Provider>
+    );
+    console.log(store.getState())
+
 });
