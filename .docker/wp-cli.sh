c22c6239deab8e382581f4ed7eb472d520795941
David Riccitelli <david@wordlift.io>
2020-01-31 14:15:43 +0100

    cleanup docker files.

--- .docker/wp-cli.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm --volumes-from docker_wordlift-plugin-wordpress_1 --network container:docker_wordlift-plugin-wordpress_1 --user 33:33 wordpress:cli --path=/var/www/html "$@"
