5e2da3a88daece9e8498c7ef5b5faef384a1d0bc
naveen <kmnaveen101@gmail.com>
2020-02-05 10:00:59 +0530

    see #973: [RED] insert faq item test fails

--- src/wordlift/faq/class-faq-rest-controller.php
+++ src/wordlift/faq/class-faq-rest-controller.php
@@ -1,9 +1,39 @@
 <?php
+/**
+ * This file defines the rest controller for FAQ
+ *
+ * @since 3.26.0
+ * @package Wordlift\FAQ
+ */
 
 namespace Wordlift\FAQ;
 
+/**
+ * Class FAQ_Rest_Controller
+ * @package Wordlift\FAQ
+ *
+ */
 class FAQ_Rest_Controller {
+	const FAQ_META_KEY = 'wl_faq';
 	public function register_routes() {
+		add_action( 'rest_api_init', 'Wordlift\FAQ\FAQ_Rest_Controller::register_route_callback' );
+	}
+
+	public static function register_route_callback() {
+		register_rest_route(
+			WL_REST_ROUTE_DEFAULT_NAMESPACE,
+			'/faq',
+			array(
+				'methods'             => \WP_REST_Server::CREATABLE,
+				'callback'            => 'Wordlift\FAQ\FAQ_Rest_Controller::insert_faq_item',
+				'permission_callback' => function () {
+					return current_user_can( 'publish_posts' );
+				},
+			)
+		);
+	}
+
+	public static function insert_faq_item( $request ) {
 
 	}
 }
--- tests/test-faq-rest-controller.php
+++ tests/test-faq-rest-controller.php
@@ -1,4 +1,7 @@
 <?php
+
+use Wordlift\FAQ\FAQ_Rest_Controller;
+
 /**
  * Tests: Tests the FAQ Rest Controller
  * @since 3.26.0
@@ -8,13 +11,14 @@
  */
 
 class FAQ_REST_Controller_Test extends Wordlift_Unit_Test_Case {
+	private $faq_route = '/wordlift/v1/faq';
 	/**
 	 * @inheritdoc
 	 */
 	public function setUp() {
 		parent::setUp();
 
-		$this->rest_instance = new \Wordlift\FAQ\FAQ_Rest_Controller();
+		$this->rest_instance = new FAQ_Rest_Controller();
 		$this->rest_instance->register_routes();
 		global $wp_rest_server;
 
@@ -26,9 +30,34 @@ class FAQ_REST_Controller_Test extends Wordlift_Unit_Test_Case {
 	public function test_rest_instance_not_null() {
 		$this->assertNotNull( $this->rest_instance );
 	}
-	public function test_given_question_and_answer_can_save_faq() {
-
+	public function test_given_question_and_answer_should_save_it_for_post() {
+		$post_id = $this->factory()->post->create( array('post_title' => 'foo'));
+		$data = array (
+			'post_id'  => $post_id,
+			'faq_items' => array(
+				array(
+					'question' => 'foo question',
+					'answer' => 'foo answer'
+				)
+			)
+		);
+		// Create user with 'publish_posts' capability.
+		$user_id   = $this->factory->user->create( array( 'role' => 'author' ) );
+		wp_set_current_user( $user_id );
+		// insert the data for this post, prepare POST request to FAQ.
+		$request   = new WP_REST_Request( 'POST', $this->faq_route );
+		$request->set_header( 'content-type', 'application/json' );
+		$request->set_body( wp_json_encode( $data ) );
+		$response  = $this->server->dispatch( $request );
+		// Should return 200 response
+		$this->assertEquals( 200, $response->get_status() );
+		$faq_items = get_post_meta($post_id, FAQ_Rest_Controller::FAQ_META_KEY);
+		$this->assertCount( 1, $faq_items );
 	}
+//	public function test_whether_rest_server_has_faq_route() {
+//		$routes = $this->server->get_routes();
+//		$this->assertArrayHasKey( FAQ_Rest_Controller::FAQ_ROUTE, $routes);
+//	}
 
 
 }
