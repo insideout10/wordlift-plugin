65c1f1b7db7bd97de6fa600a0942b3851fc1793b
naveen <kmnaveen101@gmail.com>
2020-02-05 09:21:46 +0530

    see #973: [GREEN] register routes function added

--- src/wordlift/faq/class-faq-rest-controller.php
+++ src/wordlift/faq/class-faq-rest-controller.php
@@ -3,5 +3,7 @@
 namespace Wordlift\FAQ;
 
 class FAQ_Rest_Controller {
+	public function register_routes() {
 
+	}
 }
--- tests/test-faq-rest-controller.php
+++ tests/test-faq-rest-controller.php
@@ -13,7 +13,8 @@ class FAQ_REST_Controller_Test extends Wordlift_Unit_Test_Case {
 	 */
 	public function setUp() {
 		parent::setUp();
-		$this->rest_instance = new Wordlift\FAQ_Rest_Controller();
+
+		$this->rest_instance = new \Wordlift\FAQ\FAQ_Rest_Controller();
 		$this->rest_instance->register_routes();
 		global $wp_rest_server;
 
