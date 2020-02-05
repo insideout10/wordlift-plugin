550cb7a00f892d20d3c868c73bfea7301738e00e
naveen <kmnaveen101@gmail.com>
2020-02-05 09:14:40 +0530

    see #973: [RED] FAQ rest controller not found, test fail

---
+++ tests/test-faq-rest-controller.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Tests: Tests the FAQ Rest Controller
+ * @since 3.26.0
+ * @package wordlift
+ * @subpackage wordlift/tests
+ *
+ */
+
+class FAQ_REST_Controller_Test extends Wordlift_Unit_Test_Case {
+	/**
+	 * @inheritdoc
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->rest_instance = new FAQ_Rest_Controller();
+		$this->rest_instance->register_routes();
+		global $wp_rest_server;
+
+		$wp_rest_server = new WP_REST_Server();
+		$this->server   = $wp_rest_server;
+
+		do_action( 'rest_api_init' );
+	}
+	public function test_rest_instance_not_null() {
+		$this->assertNotNull( $this->rest_instance );
+	}
+	public function test_given_question_and_answer_can_save_faq() {
+
+	}
+
+
+}
