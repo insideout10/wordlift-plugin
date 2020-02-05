1d97ddb8a3739ee17208e8adaae6e420f84dc7ea
naveen <kmnaveen101@gmail.com>
2020-02-05 11:35:08 +0530

    see #973: [GREEN] Rest api for faq items completed

--- src/wordlift/faq/class-faq-rest-controller.php
+++ src/wordlift/faq/class-faq-rest-controller.php
@@ -8,6 +8,8 @@
 
 namespace Wordlift\FAQ;
 
+use WP_REST_Request;
+
 /**
  * Class FAQ_Rest_Controller
  * @package Wordlift\FAQ
@@ -20,6 +22,9 @@ class FAQ_Rest_Controller {
 	}
 
 	public static function register_route_callback() {
+		/**
+		 * Rest route for creating new faq item / updating faq items.
+		 */
 		register_rest_route(
 			WL_REST_ROUTE_DEFAULT_NAMESPACE,
 			'/faq',
@@ -31,12 +36,48 @@ class FAQ_Rest_Controller {
 				},
 			)
 		);
+		/**
+		 * Rest route for getting the faq items.
+		 */
+		register_rest_route(
+			WL_REST_ROUTE_DEFAULT_NAMESPACE,
+			'/faq',
+			array(
+				'methods'             => \WP_REST_Server::READABLE,
+				'callback'            => 'Wordlift\FAQ\FAQ_Rest_Controller::get_faq_items',
+				'permission_callback' => function () {
+					return current_user_can( 'publish_posts' );
+				},
+			)
+		);
+
+	}
+
+	/**
+	 * Get all FAQ items for a post id.
+	 *
+	 * @param $request $request WP_REST_Request $request {@link WP_REST_Request instance}.
+	 *
+	 * @return array Result array, if post id is not given then error array is returned.
+	 */
+	public static function get_faq_items( $request ) {
+		$data = $request->get_params();
+		if ( array_key_exists('post_id', $data ) ) {
+			$post_id = (int) $data['post_id'];
+			return get_post_meta( $post_id, self::FAQ_META_KEY, true );
+		}
+		else {
+			return array(
+				'status'  => 'failure',
+				'message' => __( 'Invalid data, post_id missing', 'wordlift' )
+			);
+		}
 	}
 
 	/**
 	 * Insert a single FAQ item.
 	 *
-	 * @param $request \WP_REST_Request $request {@link WP_REST_Request instance}.
+	 * @param $request WP_REST_Request $request {@link WP_REST_Request instance}.
 	 *
 	 * @return array Associative array whether the faq item is inserted or not
 	 */
@@ -45,15 +86,7 @@ class FAQ_Rest_Controller {
 		if ( array_key_exists('post_id', $post_data) &&
 		     array_key_exists( 'faq_items', $post_data) ) {
 			$post_id = $post_data['post_id'];
-			/**
-			 * Add index as the identifier to the faq_items,
-			 * later to be used for updating or deleting.
-			 */
-			$faq_items = array();
-			foreach ( $post_data['faq_items'] as $index => $faq_item ) {
-				$faq_item['faq_id'] = $index;
-				array_push( $faq_items, $faq_item );
-			}
+			$faq_items = $post_data['faq_items'];
 			add_post_meta( (int) $post_id, self::FAQ_META_KEY, $faq_items);
 			return array(
 				'status' => 'success',
--- tests/test-faq-rest-controller.php
+++ tests/test-faq-rest-controller.php
@@ -32,27 +32,12 @@ class FAQ_REST_Controller_Test extends Wordlift_Unit_Test_Case {
 	}
 	public function test_given_question_and_answer_should_save_it_for_post() {
 		$post_id = $this->factory()->post->create( array('post_title' => 'foo'));
-		$data = array (
-			'post_id'  => $post_id,
-			'faq_items' => array(
-				array(
-					'question' => 'foo question',
-					'answer' => 'foo answer'
-				)
-			)
-		);
-		// Create user with 'publish_posts' capability.
-		$user_id   = $this->factory->user->create( array( 'role' => 'author' ) );
-		wp_set_current_user( $user_id );
-		// insert the data for this post, prepare POST request to FAQ.
-		$request   = new WP_REST_Request( 'POST', $this->faq_route );
-		$request->set_header( 'content-type', 'application/json' );
-		$request->set_body( wp_json_encode( $data ) );
+		$request = $this->get_create_faq_item_request( $post_id );
 		$response  = $this->server->dispatch( $request );
 		// Should return 200 response
 		$this->assertEquals( 200, $response->get_status() );
-		$faq_items = get_post_meta($post_id, FAQ_Rest_Controller::FAQ_META_KEY);
-		$this->assertCount( 1, $faq_items );
+		$faq_items = get_post_meta($post_id, FAQ_Rest_Controller::FAQ_META_KEY, true);
+		$this->assertCount( 2, $faq_items );
 		$response_data = $response->get_data();
 		$this->assertEquals( 'success', $response_data['status'] );
 	}
@@ -69,10 +54,54 @@ class FAQ_REST_Controller_Test extends Wordlift_Unit_Test_Case {
 		$response_data = $response->get_data();
 		$this->assertEquals( 'failure', $response_data['status'] );
 	}
-//	public function test_whether_rest_server_has_faq_route() {
-//		$routes = $this->server->get_routes();
-//		$this->assertArrayHasKey( FAQ_Rest_Controller::FAQ_ROUTE, $routes);
-//	}
+	public function test_whether_rest_server_has_faq_route() {
+		$routes = $this->server->get_routes();
+		$this->assertArrayHasKey( $this->faq_route, $routes);
+	}
+
+	public function test_can_get_faq_items_from_api() {
+		$post_id = $this->factory()->post->create( array('post_title' => 'foo'));
+		$create_faq_items_request = $this->get_create_faq_item_request( $post_id );
+		$this->server->dispatch( $create_faq_items_request );
+		$request   = new WP_REST_Request( 'GET', $this->faq_route );
+		$request->set_query_params(array(
+			'post_id' => $post_id,
+		));
+		$response  = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertCount( 2, $response->get_data() );
+
+	}
+
+	/**
+	 * @param $post_id
+	 *
+	 * @return WP_REST_Request
+	 */
+	private function get_create_faq_item_request( $post_id ) {
+		$data = array(
+			'post_id'   => $post_id,
+			'faq_items' => array(
+				array(
+					'question' => 'foo question 1',
+					'answer'   => 'foo answer 1'
+				),
+				array(
+					'question' => 'foo question 2',
+					'answer'   => 'foo answer 2'
+				)
+			)
+		);
+		// Create user with 'publish_posts' capability.
+		$user_id = $this->factory->user->create( array( 'role' => 'author' ) );
+		wp_set_current_user( $user_id );
+		// insert the data for this post, prepare POST request to FAQ.
+		$request = new WP_REST_Request( 'POST', $this->faq_route );
+		$request->set_header( 'content-type', 'application/json' );
+		$request->set_body( wp_json_encode( $data ) );
+
+		return $request;
+	}
 
 
 }
