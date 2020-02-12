80bbb96b0b132eac075f930a0408088cd545e0e2
naveen <kmnaveen101@gmail.com>
2020-02-12 21:03:44 +0530

    see #953: json ld converter correct format

--- src/wordlift/faq/class-faq-to-jsonld-converter.php
+++ src/wordlift/faq/class-faq-to-jsonld-converter.php
@@ -9,8 +9,33 @@
 
 namespace Wordlift\FAQ;
 
+/**
+ * Class Faq_To_Jsonld_Converter helps to convert the
+ * Faq data for post id to jsonld
+ *
+ * @package Wordlift\FAQ
+ */
 class Faq_To_Jsonld_Converter {
-	public function get_jsonld_for_faq() {
-		return array('@type' => 'FAQPage');
+	/**
+	 * @param $post_id int The id of the post.
+	 *
+	 * @return array Get the converted jsonld data
+	 */
+	public static function get_jsonld_for_faq( $post_id ) {
+		$faq_items = get_post_meta( $post_id, FAQ_Rest_Controller::FAQ_META_KEY);
+		$jsonld_data = array(
+			'@type' => 'FAQPage'
+		);
+		$jsonld_data['mainEntity'] = array();
+		foreach ( $faq_items as $faq_item ) {
+			$faq_data = array();
+			$faq_data['@type'] = 'Question';
+			$faq_data['name'] = $faq_item['question'];
+			$faq_data['acceptedAnswer'] = array();
+			$faq_data['acceptedAnswer']['@type'] = 'Answer';
+			$faq_data['acceptedAnswer']['text'] = $faq_item['answer'];
+			array_push($jsonld_data['mainEntity'], $faq_data );
+		}
+		return $jsonld_data;
 	}
 }
--- tests/test-faq-to-jsonld-converter.php
+++ tests/test-faq-to-jsonld-converter.php
@@ -1,5 +1,6 @@
 <?php
 
+use Wordlift\FAQ\FAQ_Rest_Controller;
 use Wordlift\FAQ\Faq_To_Jsonld_Converter;
 
 /**
@@ -12,10 +13,37 @@ use Wordlift\FAQ\Faq_To_Jsonld_Converter;
 
 class Faq_To_Jsonld_Converter_Test extends Wordlift_Unit_Test_Case {
 	public function test_if_converter_returns_correct_type() {
-		$converter = new Faq_To_Jsonld_Converter();
-		$data = $converter->get_jsonld_for_faq();
+		$post_id = $this->factory()->post->create( array('post_title' => 'foo'));
+		$data = Faq_To_Jsonld_Converter::get_jsonld_for_faq($post_id);
 		$this->assertArrayHasKey( '@type', $data );
 		$this->assertEquals( $data['@type'], 'FAQPage');
+	}
+
+	public function test_given_sample_faq_data_return_correct_jsonld() {
+		$post_id = $this->factory()->post->create( array('post_title' => 'foo'));
+		add_post_meta( $post_id, FAQ_Rest_Controller::FAQ_META_KEY, array(
+			'question' => 'foo1',
+			'answer' => 'bar1'
+		));
+		add_post_meta( $post_id, FAQ_Rest_Controller::FAQ_META_KEY, array(
+			'question' => 'foo2',
+			'answer' => 'bar2'
+		));
+		$data = Faq_To_Jsonld_Converter::get_jsonld_for_faq($post_id);
+		$this->assertArrayHasKey( 'mainEntity', $data );
+		$this->assertCount( 2, $data['mainEntity'] );
+		$single_faq_item = $data['mainEntity'][0];
+		$this->assertArrayHasKey( '@type', $single_faq_item );
+		$this->assertArrayHasKey( 'name', $single_faq_item );
+		$this->assertEquals($single_faq_item['@type'], 'Question');
+		$this->assertEquals($single_faq_item['name'], 'foo1');
+
+		$this->assertArrayHasKey( 'acceptedAnswer', $single_faq_item );
+		$single_answer = $single_faq_item['acceptedAnswer'];
+		$this->assertArrayHasKey( '@type', $single_answer );
+		$this->assertArrayHasKey( 'text', $single_answer );
+		$this->assertEquals($single_answer['@type'], 'Answer');
+		$this->assertEquals($single_answer['text'], 'bar1');
 
 	}
 }
