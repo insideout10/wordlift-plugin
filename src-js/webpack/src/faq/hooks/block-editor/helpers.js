a72b84d268805849e1233e167f6427ce75351a0c
naveen <kmnaveen101@gmail.com>
2020-02-29 16:20:27 +0530

    see #973: altering html extraction function

--- src-js/webpack/src/faq/hooks/block-editor/helpers.js
+++ src-js/webpack/src/faq/hooks/block-editor/helpers.js
@@ -5,18 +5,36 @@
  * @since 3.26.0
  * @author Naveen Muthusamy <naveen@wordlift.io>
  */
+
+function getSingleBlockSelectionHTML() {
+  let html = "";
+  /** Check for selection */
+  if (window.getSelection() !== undefined) {
+    const selection = window.getSelection();
+    const rangeCount = selection.rangeCount;
+    // Loop through all the ranges and append the child.
+    const container = document.createElement("div");
+    for (let i = 0; i < rangeCount; ++i) {
+      container.appendChild(selection.getRangeAt(i).cloneContents());
+    }
+    // once we have appended the child to the dummy container, return the innerHTML
+    html = container.innerHTML;
+  }
+  return html;
+}
+
+
 /**
  * Returns html string from selected blocks.
  * @return {string} HTML String
  */
 export function getCurrentSelectionHTML() {
   let html = "";
-  /** Check if it multi selection */
+  /** Check if it multi selection, if multi selection is done then
+   * the user can only select the contents in the block fully, so we
+   * can append the html in to single string */
   const blocks = wp.data.select('core/block-editor').getMultiSelectedBlocks()
-  if ( blocks.length === 0 ) {
-    // Not a valid selection, return empty html
-    return html
-  }
+
   if ( blocks.length > 1 ) {
     // its a multi selection, loop through blocks and get html.
     for (let block of blocks) {
@@ -25,7 +43,9 @@ export function getCurrentSelectionHTML() {
   }
   else {
     // it is a single selection, get selected blocks original content.
-    html += wp.data.select('core/block-editor').getSelectedBlock().originalContent
+    html += getSingleBlockSelectionHTML()
   }
   return html
 }
+
+
