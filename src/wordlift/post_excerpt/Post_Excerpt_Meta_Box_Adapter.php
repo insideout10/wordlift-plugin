bd8cea91475bdbaa5475125f50dee5bbf926430d
naveen <kmnaveen101@gmail.com>
2020-03-05 05:44:44 +0530

    see #980: creating method definitions for the meta box adapter

---
+++ src/wordlift/post_excerpt/Post_Excerpt_Meta_Box_Adapter.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file hooks to the metaboxes, replaces the post excerpt metabox with
+ * the custom one.
+ *
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ * @since 3.23.0
+ *
+ * @package Wordlift
+ * @subpackage Wordlift\Post_Excerpt
+ */
+namespace Wordlift\Post_Excerpt;
+
+class Post_Excerpt_Meta_Box_Adapter {
+
+	public function __construct() {
+
+	}
+
+	/**
+	 * Removes the registered post excerpt metabox.
+	 */
+	private function remove_default_post_excerpt_meta_box() {
+
+	}
+
+	/**
+	 * Adds the custom post excerpt metabox.
+	 */
+	private function add_custom_post_excerpt_meta_box() {
+
+	}
+
+	/**
+	 * Replaces the default post excerpt meta box with custom post excerpt meta box.
+	 */
+	public function replace_post_excerpt_meta_box() {
+
+	}
+}
