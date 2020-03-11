fee57b60effdd6c9a8ba1525ea7dd58b1bec019e
naveen <kmnaveen101@gmail.com>
2020-03-11 11:42:38 +0530

    see #980: adding test for removing the highlight block editor

--- src-js/webpack/src/faq/tests/block-editor-hook-highlight-handler.test.js
+++ src-js/webpack/src/faq/tests/block-editor-hook-highlight-handler.test.js
@@ -7,13 +7,20 @@
  */
 import BlockEditorHighlightHandler from "../hooks/block-editor/block-editor-highlight-handler";
 import { off, trigger } from "backbone";
-import { FAQ_HIGHLIGHT_TEXT } from "../constants/faq-hook-constants";
-import {FAQ_ANSWER_TAG_NAME} from "../hooks/custom-faq-elements";
-import {blockEditorWithSelectedBlocks, updateBlockAttributesMethod} from "./mock-dependencies/block-editor";
+import {FAQ_HIGHLIGHT_TEXT, FAQ_ITEM_DELETED} from "../constants/faq-hook-constants";
+import {FAQ_ANSWER_TAG_NAME, FAQ_QUESTION_TAG_NAME} from "../hooks/custom-faq-elements";
+import {
+  blockEditorWithHighlightedBlocks,
+  blockEditorWithSelectedBlocks,
+  updateBlockAttributesMethod
+} from "./mock-dependencies/block-editor";
+import {faqEditItemType} from "../components/faq-edit-item";
 
 beforeEach(() => {
   // Reset event handlers before every test.
   off(FAQ_HIGHLIGHT_TEXT);
+  off(FAQ_ITEM_DELETED);
+  updateBlockAttributesMethod.mockReset()
 });
 
 afterEach(() => {
@@ -60,5 +67,27 @@ it("when the event is emitted from store to highlight the multiple block selecti
 
 
 it("when remove highlight event is emitted from store upon deleting a faq item, should remove highlighting correctly", () =>{
+  const handler = new BlockEditorHighlightHandler();
+  global["wp"] = blockEditorWithHighlightedBlocks;
 
+  /**
+   * Step 1: Emit a delete event from store with
+   * correct data.
+   */
+  trigger(FAQ_ITEM_DELETED, {
+    type: faqEditItemType.ANSWER,
+    id: 123
+  });
+  /**
+   * Our hook should made 3 calls to updateBlockAttributes method
+   * with the highlighted html becuase three blocks are selected
+   */
+  expect(updateBlockAttributesMethod.mock.calls).toHaveLength(3)
+  /**
+   * since only answer is removed, answer tags should not be there,
+   * question tags should be present.
+   */
+  const singleCall = updateBlockAttributesMethod.mock.calls[0]
+  expect(singleCall[1].content.includes(FAQ_ANSWER_TAG_NAME)).toEqual(false)
+  expect(singleCall[1].content.includes(FAQ_QUESTION_TAG_NAME)).toEqual(true)
 })
--- src-js/webpack/src/faq/tests/mock-dependencies/block-editor.js
+++ src-js/webpack/src/faq/tests/mock-dependencies/block-editor.js
@@ -34,6 +34,47 @@ const fakeParagraphBlocksData = [
         innerBlocks: []
     }
 ];
+
+
+
+/**
+ * A mock  blocks object obtained from gutenberg with previously highlighted tags, used
+ * for testing the hook externally.
+ */
+const fakeParagraphBlocksHighlightingData = [
+    {
+        clientId: "1595319b-0c37-41b3-addf-93804ded1a68",
+        name: "core/paragraph",
+        isValid: true,
+        attributes: {
+            content: "<wl-faq-answer class='123'>this</wl-faq-answer> is a <wl-faq-question class='123'>answer</wl-faq-question> in first paragraph",
+            dropCap: false
+        },
+        innerBlocks: []
+    },
+    {
+        clientId: "7f122677-7ebd-44bd-80fb-5f9ecdff11f5",
+        name: "core/paragraph",
+        isValid: true,
+        attributes: {
+            content: "this is a answer in second",
+            dropCap: false
+        },
+        innerBlocks: []
+    },
+    {
+        clientId: "cfe47d7b-b5cd-4af2-b4c1-8c747313a6e0",
+        name: "core/paragraph",
+        isValid: true,
+        attributes: {
+            content: "this is answer in third",
+            dropCap: false
+        },
+        innerBlocks: []
+    }
+];
+
+
 export const updateBlockAttributesMethod = jest.fn();
 export const blockEditorWithSelectedBlocks = {
     data: {
@@ -53,3 +94,22 @@ export const blockEditorWithSelectedBlocks = {
         }
     }
 };
+
+export const blockEditorWithHighlightedBlocks = {
+    data: {
+        select: editorString => {
+            if (editorString === "core/block-editor") {
+                return {
+                    getBlocks: () => fakeParagraphBlocksHighlightingData
+                };
+            }
+        },
+        dispatch: editorString => {
+            if (editorString === "core/block-editor") {
+                return {
+                    updateBlockAttributes: updateBlockAttributesMethod
+                };
+            }
+        }
+    }
+}
