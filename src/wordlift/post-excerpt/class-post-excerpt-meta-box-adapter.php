0f5fae05eeecbe51c57c984ddf465c1db01fb6e6
naveen <kmnaveen101@gmail.com>
2020-03-16 13:33:41 +0530

    see #973: copying the default wordpress callback before altering it with html

--- src/wordlift/post-excerpt/class-post-excerpt-meta-box-adapter.php
+++ src/wordlift/post-excerpt/class-post-excerpt-meta-box-adapter.php
@@ -32,70 +32,67 @@ final class Post_Excerpt_Meta_Box_Adapter {
 	 * @var callable|null The default callback used by wordpress to
 	 * echo the post_excerpt contents, defaults to null.
 	 */
-	private $wordpress_excerpt_callback;
+	public $wordpress_excerpt_callback;
 
 	public function __construct() {
-		$this->wordpress_excerpt_callback = NULL;
+		$this->wordpress_excerpt_callback = null;
 	}
 
-	public function print_wordlift_custom_post_excerpt_box() {
-		global $post;
-		// Print the wordpress template for post excerpt
-		$this->print_wordpress_post_excerpt_ui( $post );
+	/**
+	 * Invokes the default callback inside our custom template callback
+	 *
+	 * @param $post array The post array.
+	 */
+	public function print_wordlift_custom_post_excerpt_box( $post ) {
+		call_user_func( $this->wordpress_excerpt_callback, $post );
 		// Invoke our call back to add additional html, the react script will find this id and render the component there.
 		echo "<div id='" . self::WORDLIFT_EXCERPT_DIV_ID . "'></div>";
-		$this->print_wordpress_post_excerpt_custom_text();
-	}
-
-	private function print_wordpress_post_excerpt_ui( $post ) {
-		?>
-        <label class="screen-reader-text" for="excerpt">
-			<?php _e( 'Excerpt' ); ?>
-        </label>
-        <textarea rows="1" cols="40" name="excerpt" id="excerpt">
-			<?php echo $post->post_excerpt; // textarea_escaped ?>
-		</textarea>
-		<?php
-	}
-
-	private function print_wordpress_post_excerpt_custom_text() {
-		?>
-        <p>
-			<?php
-			printf(
-			/* translators: %s: Documentation URL. */
-				__( 'Excerpts are optional hand-crafted summaries of your content that can be used in your theme. <a href="%s">Learn more about manual excerpts</a>.' ),
-				__( 'https://wordpress.org/support/article/excerpt/' )
-			);
-			?>
-        </p>
-		<?php
 	}
 
 	/**
 	 * Replaces the default post excerpt meta box with custom post excerpt meta box.
 	 */
 	public function replace_post_excerpt_meta_box() {
-		$this->remove_default_post_excerpt_meta_box();
-		$this->add_custom_post_excerpt_meta_box();
-		$this->enqueue_post_excerpt_scripts();
+		global $wp_meta_boxes;
+		$core_meta_boxes       = $wp_meta_boxes[ get_post_type() ]["normal"]["core"];
+		$post_excerpt_meta_box = $core_meta_boxes[ self::POST_EXCERPT_META_BOX_KEY ];
+		$callback              = $post_excerpt_meta_box['callback'];
+		/**
+		 * do_meta_boxes action is called 3 times by wordpress for
+		 * different set of metaboxes, so to prevent overwriting our
+		 * callback with null, this check is necessary.
+		 */
+		if ( $callback !== null && $callback !== array(
+				$this,
+				'print_wordlift_custom_post_excerpt_box'
+			) ) {
+			$this->wordpress_excerpt_callback = $callback;
+			$this->remove_default_post_excerpt_meta_box();
+			$this->add_custom_post_excerpt_meta_box( $this->wordpress_excerpt_callback );
+			$this->enqueue_post_excerpt_scripts();
+		}
 	}
 
 	/**
 	 * Removes the registered post excerpt metabox.
 	 */
 	private function remove_default_post_excerpt_meta_box() {
+
 		remove_meta_box( self::POST_EXCERPT_META_BOX_KEY, get_current_screen(), 'normal' );
 	}
 
 	/**
 	 * Adds the custom post excerpt metabox.
 	 */
-	private function add_custom_post_excerpt_meta_box() {
+	private function add_custom_post_excerpt_meta_box( $wordpress_callback ) {
 		add_meta_box(
 			self::POST_EXCERPT_META_BOX_KEY,
 			__( 'Post Excerpt', 'wordlift' ),
-			array( $this, 'print_wordlift_custom_post_excerpt_box' )
+			array( $this, 'print_wordlift_custom_post_excerpt_box' ),
+			get_current_screen(),
+			'normal',
+			'high',
+			array( $wordpress_callback )
 		);
 	}
 
@@ -104,7 +101,7 @@ final class Post_Excerpt_Meta_Box_Adapter {
 			'wl-post-excerpt',
 			plugin_dir_url( dirname( dirname( __FILE__ ) ) ) . 'js/dist/post-excerpt',
 			array( 'react', 'react-dom', 'wp-polyfill' ),
-			TRUE
+			true
 		);
 		wp_enqueue_style(
 			'wl-post-excerpt-style',
@@ -120,7 +117,7 @@ final class Post_Excerpt_Meta_Box_Adapter {
 		return array(
 			'orText'         => __( 'Or use wordlift suggested post excerpt', 'wordlift' ),
 			'generatingText' => __( 'Generating excerpt...', 'wordlift' ),
-			'restUrl'        => get_rest_url( NULL, WL_REST_ROUTE_DEFAULT_NAMESPACE . '/post-excerpt' ),
+			'restUrl'        => get_rest_url( null, WL_REST_ROUTE_DEFAULT_NAMESPACE . '/post-excerpt' ),
 			'nonce'          => wp_create_nonce( 'wp_rest' ),
 			'postId'         => get_the_ID(),
 		);
