53ba221089dee970c2b187ac2534e86b5cc2682e
David Riccitelli <david@wordlift.io>
2020-03-23 11:27:13 +0100

    try to fix the JS.

--- src/js/dist/block-editor.asset.php
+++ src/js/dist/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-rich-text'), 'version' => '1221345657955450fc4213a850f36ed1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-rich-text'), 'version' => '0f7d637cacc37f32dd32178a209ba23b');
