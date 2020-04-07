4bfb33b45b067963fd78b7145a6de6a036de7a13
David Riccitelli <david@wordlift.io>
2020-04-07 08:34:05 +0200

    bump to 3.25.4

---
+++ tags/3.25.4/wordlift/jsonld/class-jsonld-endpoint.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * This file defines the JSON-LD endpoint.
+ *
+ * @author David Riccitelli <david@wordlift.io>
+ * @package Wordlift\Jsonld
+ */
+
+namespace Wordlift\Jsonld;
+
+use DateInterval;
+use DateTime;
+use DateTimeZone;
+use Wordlift_Jsonld_Service;
+use WP_REST_Response;
+
+/**
+ * Class Jsonld_Endpoint
+ *
+ * @package Wordlift\Jsonld
+ */
+class Jsonld_Endpoint {
+
+	/**
+	 * The {@link Wordlift_Jsonld_Service} instance.
+	 *
+	 * @var Wordlift_Jsonld_Service The {@link Wordlift_Jsonld_Service} instance.
+	 */
+	private $jsonld_service;
+	/**
+	 * @var \Wordlift_Entity_Uri_Service
+	 */
+	private $entity_uri_service;
+
+	/**
+	 * Jsonld_Endpoint constructor.
+	 *
+	 * @param \Wordlift_Jsonld_Service $jsonld_service
+	 * @param \Wordlift_Entity_Uri_Service $entity_uri_service
+	 */
+	public function __construct( $jsonld_service, $entity_uri_service ) {
+
+		$this->jsonld_service     = $jsonld_service;
+		$this->entity_uri_service = $entity_uri_service;
+
+		// PHP 5.3 compatibility.
+		$that = $this;
+		add_action( 'rest_api_init', function () use ( $that ) {
+			register_rest_route( WL_REST_ROUTE_DEFAULT_NAMESPACE, '/jsonld/(?P<id>\d+)', array(
+				'methods'  => 'GET',
+				'callback' => array( $that, 'jsonld_using_post_id' ),
+				'args'     => array(
+					'id' => array(
+						'validate_callback' => function ( $param, $request, $key ) {
+							return is_numeric( $param );
+						},
+						'sanitize_callback' => 'absint',
+					),
+				)
+			) );
+
+			register_rest_route( WL_REST_ROUTE_DEFAULT_NAMESPACE, '/jsonld/(?P<post_type>.*)/(?P<post_name>.*)', array(
+				'methods'  => 'GET',
+				'callback' => array( $that, 'jsonld_using_get_page_by_path' ),
+			) );
+
+			register_rest_route( WL_REST_ROUTE_DEFAULT_NAMESPACE, '/jsonld/http/(?P<item_id>.*)', array(
+				'methods'  => 'GET',
+				'callback' => array( $that, 'jsonld_using_item_id' ),
+			) );
+		} );
+
+	}
+
+	/**
+	 * Callback for the JSON-LD request.
+	 *
+	 * @param array $request {
+	 *  The request array.
+	 *
+	 * @type int $id The post id.
+	 * }
+	 *
+	 * @return WP_REST_Response
+	 * @throws \Exception
+	 */
+	public function jsonld_using_post_id( $request ) {
+
+		$post_id     = $request['id'];
+		$is_homepage = ( 0 === $post_id );
+
+		// Send the generated JSON-LD.
+		$data     = $this->jsonld_service->get_jsonld( $is_homepage, $post_id );
+		$response = new WP_REST_Response( $data );
+
+		$cache_in_seconds = 86400;
+		$date_timezone    = new DateTimeZone( 'GMT' );
+		$date_now         = new DateTime( 'now', $date_timezone );
+		$date_interval    = new DateInterval( "PT{$cache_in_seconds}S" );
+		$expires          = $date_now->add( $date_interval )->format( 'D, j M Y H:i:s T' );
+
+		$response->set_headers( array(
+			'Content-Type'  => 'application/ld+json; charset=' . get_option( 'blog_charset' ),
+			'Cache-Control' => "max-age=$cache_in_seconds",
+			'Expires'       => $expires
+		) );
+
+		return $response;
+	}
+
+	/**
+	 * Provide a JSON-LD given the itemId.
+	 *
+	 * @param array $request {
+	 *  The request array.
+	 *
+	 * @type string $item_id The entity item id.
+	 * }
+	 *
+	 * @return WP_REST_Response
+	 * @throws \Exception
+	 */
+	public function jsonld_using_item_id( $request ) {
+
+		$item_id = 'http://' . $request['item_id'];
+		$post    = $this->entity_uri_service->get_entity( $item_id );
+
+		if ( ! is_a( $post, 'WP_Post' ) ) {
+			return new WP_REST_Response( esc_html( "$item_id not found." ), 404, array( 'Content-Type' => 'text/html' ) );
+		}
+
+		return $this->jsonld_using_post_id( array( 'id' => $post->ID, ) );
+	}
+
+	public function jsonld_using_get_page_by_path( $request ) {
+
+		$post_name = $request['post_name'];
+		$post_type = $request['post_type'];
+
+		global $wpdb;
+
+		$sql     = "
+			SELECT ID
+			FROM $wpdb->posts
+			WHERE post_name = %s
+			 AND post_type = %s
+		";
+
+		$post_id = $wpdb->get_var( $wpdb->prepare( $sql, $post_name, $post_type ) );
+
+		if ( is_null( $post_id ) ) {
+			return new WP_REST_Response( esc_html( "$post_name of type $post_type not found." ), 404, array( 'Content-Type' => 'text/html' ) );
+		}
+
+		return $this->jsonld_using_post_id( array( 'id' => $post_id, ) );
+	}
+
+}
---
+++ tags/3.25.4/wordlift/mappings/class-jsonld-converter.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Define the Wordlift_Mapping_Jsonld_Converter class to add JSON-LD generated from mappings.
+ *
+ * @since   3.25.0
+ * @package Wordlift
+ * @subpackage Wordlift\Mappings
+ */
+
+namespace Wordlift\Mappings;
+
+/**
+ * This class takes the output from json-ld service and alter depends on the
+ * rule and properties defined in sync mappings.
+ *
+ * @since 3.25.0
+ */
+class Jsonld_Converter {
+	/**
+	 * Enumerations for the field types.
+	 * Enumerations for the field types.
+	 */
+	const FIELD_TYPE_TEXT_FIELD = 'text';
+	const FIELD_TYPE_CUSTOM_FIELD = 'custom_field';
+	const FIELD_TYPE_ACF = 'acf';
+	/**
+	 * The {@link Mappings_Validator} instance to test.
+	 *
+	 * @since  3.25.0
+	 * @access private
+	 * @var Mappings_Validator $validator The {@link Mappings_Validator} instance.
+	 */
+	private $validator;
+
+	/**
+	 * The {@link Mappings_Transform_Functions_Registry} instance.
+	 *
+	 * @since  3.25.0
+	 * @access private
+	 * @var Mappings_Transform_Functions_Registry $transform_functions_registry The {@link Mappings_Transform_Functions_Registry} instance.
+	 */
+	private $transform_functions_registry;
+
+	/**
+	 * Initialize all dependencies required.
+	 *
+	 * @param Mappings_Validator $validator A {@link Mappings_Validator} instance.
+	 * @param Mappings_Transform_Functions_Registry $transform_functions_registry
+	 */
+	public function __construct( $validator, $transform_functions_registry ) {
+
+		$this->validator                    = $validator;
+		$this->transform_functions_registry = $transform_functions_registry;
+
+		// Hook to refactor the JSON-LD.
+		add_filter( 'wl_post_jsonld_array', array( $this, 'wl_post_jsonld_array' ), 11, 2 );
+		add_filter( 'wl_entity_jsonld_array', array( $this, 'wl_post_jsonld_array' ), 11, 3 );
+
+	}
+
+	/**
+	 * Hook to `wl_post_jsonld_array` and `wl_entity_jsonld_array`.
+	 *
+	 * Receive the JSON-LD and the references in the array along with the post ID and transform them according to
+	 * the configuration.
+	 *
+	 * @param array $value {
+	 *      The array containing the JSON-LD and the references.
+	 *
+	 * @type array $jsonld The JSON-LD array.
+	 * @type int[] $references An array of post ID referenced by the JSON-LD (will be expanded by the converter).
+	 * }
+	 *
+	 * @param int $post_id The post ID.
+	 *
+	 * @return array An array with the updated JSON-LD and references.
+	 */
+	public function wl_post_jsonld_array( $value, $post_id ) {
+
+		$jsonld     = $value['jsonld'];
+		$references = $value['references'];
+
+		return array(
+			'jsonld'     => $this->wl_post_jsonld( $jsonld, $post_id, $references ),
+			'references' => $references,
+		);
+	}
+
+	/**
+	 * Returns JSON-LD data after applying transformation functions.
+	 *
+	 * @param array $jsonld The JSON-LD structure.
+	 * @param int $post_id The {@link WP_Post} id.
+	 * @param array $references An array of post references.
+	 *
+	 * @return array the new refactored array structure.
+	 * @since 3.25.0
+	 */
+	private function wl_post_jsonld( $jsonld, $post_id, &$references ) {
+
+		// @@todo I think there's an issue here with the Validator, because you're changing the instance state and the
+		// instance may be reused afterwards.
+
+		$properties = $this->validator->validate( $post_id );
+
+		foreach ( $properties as $property ) {
+			$transform_instance = $this->transform_functions_registry->get_transform_function( $property['transform_function'] );
+			$data               = $this->get_data_from_data_source( $post_id, $property );
+			if ( null !== $transform_instance ) {
+				$transform_data = $transform_instance->transform_data( $data, $jsonld, $references, $post_id );
+				if ( null !== $transform_data ) {
+					$jsonld[ $property['property_name'] ] = $this->make_single( $transform_data );
+				}
+			} else {
+				$jsonld[ $property['property_name'] ] = $this->make_single( $data );
+			}
+		}
+
+		return $jsonld;
+	}
+
+	/**
+	 * Returns data from data source.
+	 *
+	 * @param int $post_id Id of the post.
+	 * @param array $property_data The property data for the post_id.
+	 *
+	 * @return array Returns key, value array, if the value is not found, then it returns null.
+	 */
+	final public function get_data_from_data_source( $post_id, $property_data ) {
+		$value = $property_data['field_name'];
+
+		// Do 1 to 1 mapping and return result.
+		switch ( $property_data['field_type'] ) {
+			case self::FIELD_TYPE_ACF:
+				if ( ! function_exists( 'get_field' ) ) {
+					return array();
+				}
+
+				return get_field( $property_data['field_name'], $post_id );
+
+			case self::FIELD_TYPE_CUSTOM_FIELD:
+
+				return array_map( 'wp_strip_all_tags', get_post_meta( $post_id, $value ) );
+
+			default:
+				return $value;
+		}
+
+	}
+
+	private function make_single( $value ) {
+
+		$values = (array) $value;
+
+		if ( empty( $values ) ) {
+			return null;
+		}
+
+		if ( 1 === count( $values ) && 0 === key( $values ) ) {
+			return current( $values );
+		}
+
+		return $values;
+	}
+
+}
