1bfae406b498e290c8635a8457d5de8360625c7a
Akshay Raje <akshay.raje@gmail.com>
2020-06-01 12:58:03 +0530

    Code clean-up

--- src/public/class-wordlift-faceted-search-shortcode.php
+++ src/public/class-wordlift-faceted-search-shortcode.php
@@ -78,7 +78,7 @@ class Wordlift_Faceted_Search_Shortcode extends Wordlift_Shortcode {
 		$limit        = $shortcode_atts['limit'];
 		$faceted_id   = $shortcode_atts['uniqid'];
 
-		$rest_url     = $post ? rest_url(sprintf("wordlift/v1/faceted-search/network?post_id=%s&limit=%s", $post->ID, $limit)) : false;
+		$rest_url     = $post ? rest_url(sprintf("wordlift/v1/faceted-search?post_id=%s&limit=%s", $post->ID, $limit)) : false;
 
 		// avoid building the widget when no valid $rest_url
 		if ( ! $rest_url ) {
@@ -99,6 +99,15 @@ class Wordlift_Faceted_Search_Shortcode extends Wordlift_Shortcode {
 		);
 	}
 
+	/**
+	 * Function in charge of diplaying the [wl-faceted-search] in amp mode.
+	 *
+	 * @since 3.20.0
+	 *
+	 * @param array $atts Shortcode attributes.
+	 *
+	 * @return string Shortcode HTML for amp
+	 */
 	private function amp_shortcode( $atts ) {
 
 		// attributes extraction and boolean filtering
@@ -113,7 +122,7 @@ class Wordlift_Faceted_Search_Shortcode extends Wordlift_Shortcode {
 		$limit        = $shortcode_atts['limit'];
 		$faceted_id   = $shortcode_atts['uniqid'];
 
-		$rest_url     = $post ? rest_url(sprintf("wordlift/v1/faceted-search/network?post_id=%s&limit=%s", $post->ID, $limit)) : false;
+		$rest_url     = $post ? rest_url(sprintf("wordlift/v1/faceted-search?post_id=%s&limit=%s", $post->ID, $limit)) : false;
 
 		// avoid building the widget when no valid $rest_url
 		if ( ! $rest_url ) {
@@ -130,14 +139,14 @@ class Wordlift_Faceted_Search_Shortcode extends Wordlift_Shortcode {
 		if ( ! empty( $shortcode_atts['template_id'] ) ) {
 			$template_id = $shortcode_atts['template_id'];
 		} else {
-			$template_id = "template-cards-" . $faceted_id;
+			$template_id = "template-" . $faceted_id;
 			// Enqueue amp specific styles
 			wp_enqueue_style( 'wordlift-amp-custom', plugin_dir_url( dirname( __FILE__ ) ) . '/css/wordlift-amp-custom.min.css' );
 		}
 
 		return <<<HTML
 		<div id="{$faceted_id}" class="wl-amp-faceted" style="width: 100%">
-			<h3 class="title">{$shortcode_atts['title']}</h3>
+			<h3 class="wl-headline">{$shortcode_atts['title']}</h3>
 			<amp-state id="referencedPosts">
 				<script type="application/json">
 					[]
@@ -151,7 +160,6 @@ class Wordlift_Faceted_Search_Shortcode extends Wordlift_Shortcode {
 			<amp-state id="allPostsEntities" src="{$rest_url}"></amp-state>
 			<section class="chips">
 				<amp-list 
-					width="auto"
 					height="50"
 					layout="fixed-height"
 					src="{$rest_url}"
@@ -167,203 +175,27 @@ class Wordlift_Faceted_Search_Shortcode extends Wordlift_Shortcode {
 			</section>
 			<section class="cards">
 				<amp-list 
-					width="auto"
 					height="240"
+					[height]="240 * ceil(allPostsEntities.posts.length / 4)"
 					layout="fixed-height"
 					src="{$rest_url}"
 					[src]="allPostsEntities.posts.sort((a, b) => referencedPosts.includes(a.ID) ? -1 : 1)"
 					template="{$template_id}"
 					items="posts">
+					<template type="amp-mustache" id="template-{$faceted_id}">
+						<article class="card">
+							<a href="{{permalink}}">
+								<amp-img
+			                        width="800"
+			                        height="450"
+									layout="responsive"
+			                        src="{{thumbnail}}"></amp-img>
+								<div class="card-content"><h3 class="title">{{post_title}}</h3></div>
+							</a>
+						</article>
+					</template>
 				</amp-list>
-				<template type="amp-mustache" id="template-cards-{$faceted_id}">
-					<article class="card">
-						<a href="{{permalink}}">
-							<amp-img
-		                        width="800"
-		                        height="450"
-								layout="responsive"
-		                        src="{{thumbnail}}"></amp-img>
-							<div class="card-content"><h3 class="title">{{post_title}}</h3></div>
-						</a>
-					</article>
-				</template>		
 			</section>	
-		</div>
-HTML;
-	}
-
-	/**
-	 * Function in charge of diplaying the [wl-faceted-search] in amp mode.
-	 *
-	 * @since 3.20.0
-	 *
-	 * @param array $atts Shortcode attributes.
-	 *
-	 * @return string Shortcode HTML for amp
-	 */
-	private function amp_shortcode_old( $atts ) {
-
-		// attributes extraction and boolean filtering
-		$shortcode_atts = $this->make_shortcode_atts( $atts );
-
-		// If the current post is not an entity and has no related entities
-		// than the shortcode cannot be rendered
-		// TODO Add an alert visibile only for connected admin users.
-		$current_post = get_post();
-
-		$entity_service = Wordlift_Entity_Service::get_instance();
-		$entity_ids     = $entity_service->is_entity( $current_post->ID ) ?
-			array( $current_post->ID ) :
-			wl_core_get_related_entity_ids( $current_post->ID );
-
-		// Bail if there are no entity ids.
-		if ( 0 === count( $entity_ids ) ) {
-			return '';
-		}
-
-		$div_id = 'wordlift-faceted-entity-search-widget';
-
-		// Enqueue amp specific styles
-		wp_enqueue_style( 'wordlift-amp-custom', plugin_dir_url( dirname( __FILE__ ) ) . '/css/wordlift-amp-custom.min.css' );
-
-		$wp_json_base = get_rest_url() . WL_REST_ROUTE_DEFAULT_NAMESPACE;
-
-		$query_posts = array(
-			'post_id' => $current_post->ID,
-			'limit'   => apply_filters( 'wl_faceted_search_limit', $shortcode_atts['limit'] ),
-			'type'    => 'posts',
-		);
-
-		$query_facets = array(
-			'post_id' => $current_post->ID,
-			'limit'   => apply_filters( 'wl_faceted_search_limit', $shortcode_atts['limit'] ),
-			'type'    => 'facets',
-			'l10n'    => array(
-				'what'  => _x( 'What', 'Faceted Search Widget', 'wordlift' ),
-				'who'   => _x( 'Who', 'Faceted Search Widget', 'wordlift' ),
-				'where' => _x( 'Where', 'Faceted Search Widget', 'wordlift' ),
-				'when'  => _x( 'When', 'Faceted Search Widget', 'wordlift' ),
-			),
-		);
-
-		if ( strpos( $wp_json_base, 'wp-json/' . WL_REST_ROUTE_DEFAULT_NAMESPACE ) ) {
-			$delimiter = '?';
-		} else {
-			$delimiter = '&';
-		}
-
-		// Use a protocol-relative URL as amp-list spec says that URL's protocol must be HTTPS.
-		// This is a hackish way, but this works for http and https URLs
-		$wp_json_url_posts  = str_replace( array(
-				'http:',
-				'https:',
-			), '', $wp_json_base ) . '/faceted-search/amp' . $delimiter . http_build_query( $query_posts );
-		$wp_json_url_facets = str_replace( array(
-				'http:',
-				'https:',
-			), '', $wp_json_base ) . '/faceted-search/amp' . $delimiter . http_build_query( $query_facets );
-
-		/*
-		 * Notes about amp code:
-		 *
-		 * amp-list src is the only way to do an xhr fetch so we need it for both facets and posts
-		 * amp-list src needs to have items array which amp-list renders within template
-		 * amp-list does not support dynamic resizing. It needs a layout but as of not we have not specified any layout for .wl-facets amp-list
-		 * CSS classes and most styles are kept consistant with web layout
-		 * amp-state currentEntities is an array of to be shown entities
-		 * amp-state allPosts is a remote fetched state of all posts
-		 * amp-state filteredPosts is a filtered version of allPosts using currentEntities
-		 * amp-bind-macro getCurrentEntities is a re-used function within amp
-		 * All JS code in amp has to be done using limited JS expressions `see https://www.ampproject.org/docs/reference/components/amp-bind#expressions`
-		 */
-
-		return <<<HTML
-		<div id="{$div_id}" class="wl-amp-fs">
-			<amp-state id="currentEntities">
-				<script type="application/json">
-					[]
-				</script>
-			</amp-state>
-			<amp-state id="allPosts" src="{$wp_json_url_posts}"></amp-state>
-			<amp-bind-macro id="getCurrentEntities" arguments="currentEntity" expression="currentEntities.includes(currentEntity) ? currentEntities.filter(entity => entity != currentEntity) : currentEntities.concat(currentEntity)" />
-			<amp-accordion>
-				<section expanded>
-					<h3 class="wl-headline">{$shortcode_atts['title']}</h3>
-					<amp-list 
-						class="wl-facets" 
-						src="{$wp_json_url_facets}"
-						layout="fixed-height"
-						height="150">
-						<template type="amp-mustache">  
-							<div class="wl-facets-container">
-								<h5>{{l10n}}</h5>
-								<ul>
-								{{#data}}
-									<li on="tap:AMP.setState({
-										currentEntities: getCurrentEntities('{{id}}'),
-										filteredPosts: {
-											values: allPosts.items[0].values.filter( posts => getCurrentEntities('{{id}}').length == 0 ? true : ( getCurrentEntities('{{id}}').some(entity => posts.entities.includes(entity)) ) )
-										}
-									})">{{label}}</li>
-								{{/data}}
-								</ul>
-							</div>	
-						</template>
-					</amp-list>
-				</section>
-			</amp-accordion>
-			<amp-list 
-				media="(min-width: 461px)"
-				width="auto"
-				height="250"
-				layout="fixed-height"
-				src="{$wp_json_url_posts}"
-				[src]="filteredPosts">
-				<template type="amp-mustache">  
-					<amp-carousel 
-						class="wl-amp-carousel"
-						height="220"
-						layout="fixed-height"
-						type="carousel">
-					{{#values}}
-						<div class="wl-card">		
-							<amp-img src="{{thumbnail}}"
-								width="2"
-								height="1"
-								layout="responsive"
-								alt="{{post_title}}"></amp-img>
-							<div class="wl-card-title"><a href="{{permalink}}">{{post_title}}</a></div> 
-						</div>	
-					{{/values}}
-					</amp-carousel>
-				</template>
-			</amp-list>
-			<amp-list 
-				media="(max-width: 460px)"
-				width="auto"
-				height="300"
-				layout="fixed-height"
-				src="{$wp_json_url_posts}"
-				[src]="filteredPosts">
-				<template type="amp-mustache">  
-					<amp-carousel 
-						class="wl-amp-carousel"
-						height="270"
-						layout="fixed-height"
-						type="slides">
-					{{#values}}
-						<div class="wl-card">
-							<amp-img src="{{thumbnail}}"
-								width="2"
-								height="1"
-								layout="responsive"
-								alt="{{post_title}}"></amp-img>
-							<div class="wl-card-title"><a href="{{permalink}}">{{post_title}}</a></div> 
-						</div>	
-					{{/values}}
-					</amp-carousel>
-				</template>
-			</amp-list>			
 		</div>
 HTML;
 	}
