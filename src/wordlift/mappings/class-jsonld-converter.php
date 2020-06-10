9aa026dd7f400a5e84aaf1ffe049f2838b4250f8
naveen <kmnaveen101@gmail.com>
2020-06-10 11:24:05 +0530

    fix how to mapping

--- src/wordlift/mappings/class-jsonld-converter.php
+++ src/wordlift/mappings/class-jsonld-converter.php
@@ -236,8 +236,13 @@ class Jsonld_Converter {
 		// only process if it is a repeater field, else return the data.
 		if ( is_array( $field_data ) && array_key_exists( 'type', $field_data )
 		     && $field_data['type'] === 'repeater' ) {
-			// check if we have only one sub field, currently we only support one subfield.
-			if ( is_array( $data ) && count( $data ) > 0 && count( array_keys( $data[0] ) === 1 ) ) {
+			/**
+			 * check if we have only one sub field, currently we only support one subfield,
+			 * so each repeater item should be checked if there is a single sub field.
+			 */
+			if ( is_array( $data ) &&
+			     count( $data ) > 0 &&
+			     count( array_keys( $data[0] ) ) === 1 ) {
 				$repeater_formatted_data = array();
 				foreach ( $data as $item ) {
 					$repeater_formatted_data = array_merge( $repeater_formatted_data, array_values( $item ) );
