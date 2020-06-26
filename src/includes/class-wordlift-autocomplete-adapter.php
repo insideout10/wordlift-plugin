d2e3337efa22dc7c92cde3821bfe1ed022693214
naveen <kmnaveen101@gmail.com>
2020-06-26 14:57:39 +0530

    see #1074: filter_var code review change

--- src/includes/class-wordlift-autocomplete-adapter.php
+++ src/includes/class-wordlift-autocomplete-adapter.php
@@ -79,7 +79,7 @@ class Wordlift_Autocomplete_Adapter {
 		if ( isset( $_REQUEST['show_local_entities'] )
 		     && ! empty( $_REQUEST['show_local_entities'] ) ) // Make request.
 		{
-			$show_local_entities = $_REQUEST['show_local_entities'] === 'true';
+			$show_local_entities = filter_var( $_REQUEST['show_local_entities'], FILTER_VALIDATE_BOOLEAN );
 		}
 
 		// Add the filter to check if we need to show local entities or not.
