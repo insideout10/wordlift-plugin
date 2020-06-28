bd10c12fd92429765a97f0bfb25d7453788c75ef
Akshay Raje <akshay.raje@gmail.com>
2020-06-28 10:49:30 +0530

    #1039 manually-annotating-entity-fails fix

--- src-js/webpack/src/Edit/services/LinkService.js
+++ src-js/webpack/src/Edit/services/LinkService.js
@@ -10,7 +10,7 @@
 /**
  * Internal dependencies
  */
-import EditorService from './EditorService'
+import EditorService from "./EditorService";
 
 /**
  * Define the `LinkService` class.
@@ -18,16 +18,15 @@ import EditorService from './EditorService'
  * @since 3.11.0
  */
 class LinkService {
-
   /**
    * Create an `LinkService` instance.
    *
    * @since 3.13.0
    * @param {boolean} linkByDefault Whether to link by default.
    */
-  constructor (linkByDefault) {
+  constructor(linkByDefault) {
     // Set the `link by default` setting.
-    this.linkByDefault = linkByDefault
+    this.linkByDefault = linkByDefault;
   }
 
   /**
@@ -38,16 +37,15 @@ class LinkService {
    *     elements).
    * @param {boolean} value True to enable linking or false to disable it.
    */
-  setLink (occurrences, value) {
+  setLink(occurrences, value) {
     // If the request is to enable links, remove the `wl-no-link` class on
     // all the occurrences.
     if (value) {
-      occurrences.forEach(x => this.setYesLink(x))
-    }
-    else {
+      occurrences.forEach(x => this.setYesLink(x));
+    } else {
       // If the request is to disable links, add the `wl-no-link` class to
       // all occurrences.
-      occurrences.forEach(x => this.setNoLink(x))
+      occurrences.forEach(x => this.setNoLink(x));
     }
   }
 
@@ -57,10 +55,10 @@ class LinkService {
    * @since 3.13.0
    * @param {object} elem A DOM element.
    */
-  setYesLink (elem) {
-    const dom = EditorService.get().dom
-    dom.removeClass(elem, 'wl-no-link')
-    dom.addClass(elem, 'wl-link')
+  setYesLink(elem) {
+    const dom = EditorService.get().dom;
+    dom.removeClass(elem, "wl-no-link");
+    dom.addClass(elem, "wl-link");
   }
 
   /**
@@ -69,10 +67,10 @@ class LinkService {
    * @since 3.13.0
    * @param {object} elem A DOM element.
    */
-  setNoLink (elem) {
-    const dom = EditorService.get().dom
-    dom.removeClass(elem, 'wl-link')
-    dom.addClass(elem, 'wl-no-link')
+  setNoLink(elem) {
+    const dom = EditorService.get().dom;
+    dom.removeClass(elem, "wl-link");
+    dom.addClass(elem, "wl-no-link");
   }
 
   /**
@@ -84,21 +82,40 @@ class LinkService {
    * @return {boolean} True if at least one occurrences enables linking,
    *     otherwise false.
    */
-  getLink (occurrences) {
+  getLink(occurrences) {
     const ed = EditorService.get();
 
-    return ed && occurrences.reduce((acc, id) => {
-      const dom = EditorService.get().dom
+    if (ed) {
+      // Handle classic editor
+      return occurrences.reduce((acc, id) => {
+        const dom = ed.dom;
 
-      return acc || (
-        this.linkByDefault
-          ? !dom.hasClass(id, 'wl-no-link')
-          : dom.hasClass(id, 'wl-link')
-      )
-    }, false)
+        return acc || (this.linkByDefault ? !dom.hasClass(id, "wl-no-link") : dom.hasClass(id, "wl-link"));
+      }, false);
+    } else {
+      // Handle block editor
+      const divElem = document.createElement("div");
+      divElem.innerHTML = wp.data
+        .select("core/editor")
+        .getBlocks()
+        .map(block => block.originalContent)
+        .join();
+
+      return occurrences.reduce((acc, id) => {
+        return (
+          acc ||
+          (this.linkByDefault
+            ? !(
+                divElem.querySelector(`[id="${id}"]`) &&
+                divElem.querySelector(`[id="${id}"]`).classList.contains("wl-no-link")
+              )
+            : divElem.querySelector(`[id="${id}"]`) &&
+              divElem.querySelector(`[id="${id}"]`).classList.contains("wl-link"))
+        );
+      }, false);
+    }
   }
-
 }
 
 // Finally export the `LinkService`.
-export default new LinkService('1' === wlSettings.link_by_default)
+export default new LinkService("1" === wlSettings.link_by_default);
