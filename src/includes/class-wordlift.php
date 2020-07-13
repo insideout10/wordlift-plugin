a52753df78bf8ef1a9f41410debc75894e65c57f
Akshay Raje <akshay.raje@gmail.com>
2020-07-13 17:31:30 +0530

    Release 3.26.3

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -749,7 +749,7 @@ class Wordlift {
 		self::$instance = $this;
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.26.2';
+		$this->version     = '3.26.3';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
