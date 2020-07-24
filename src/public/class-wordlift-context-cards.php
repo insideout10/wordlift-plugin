28eba0136bebba985264df3590e30d02a7e0582d
Akshay Raje <akshay.raje@gmail.com>
2020-07-24 11:13:31 +0530

    Discount percent

--- src/public/class-wordlift-context-cards.php
+++ src/public/class-wordlift-context-cards.php
@@ -42,7 +42,7 @@ class Wordlift_Context_Cards_Service {
 						array(
 							'regular_price'   => $product->get_regular_price(),
 							'currency_symbol' => get_woocommerce_currency_symbol(),
-							'discount'        => ($product->get_sale_price() && $product->get_regular_price() > 0) ? round( 1 - ( $product->get_sale_price() / $product->get_regular_price() ), 2 ) : 0,
+							'discount_pc'     => ($product->get_sale_price() && ($product->get_regular_price() > 0)) ? round( 1 - ( $product->get_sale_price() / $product->get_regular_price() ), 2 ) * 100 : 0,
 							'average_rating'  => $product->get_average_rating(),
 							'rating_count'    => $product->get_rating_count(),
 							'rating_html'     => wc_get_rating_html( $product->get_average_rating(), $product->get_rating_count() )
