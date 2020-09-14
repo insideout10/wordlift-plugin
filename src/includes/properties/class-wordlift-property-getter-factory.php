66836d772afd885e45a42c3aaaaf703fbdf0cc0e
Akshay Raje <akshay.raje@gmail.com>
2020-09-14 15:08:37 +0530

    Reverting field address

--- src/includes/properties/class-wordlift-property-getter-factory.php
+++ src/includes/properties/class-wordlift-property-getter-factory.php
@@ -11,7 +11,6 @@ require_once( 'class-wordlift-property-getter.php' );
 require_once( 'class-wordlift-simple-property-service.php' );
 require_once( 'class-wordlift-entity-property-service.php' );
 require_once( 'class-wordlift-location-property-service.php' );
-require_once( 'class-wordlift-address-property-service.php' );
 require_once( 'class-wordlift-url-property-service.php' );
 require_once( 'class-wordlift-double-property-service.php' );
 require_once( 'class-wordlift-duration-property-service.php' );
@@ -51,9 +50,6 @@ class Wordlift_Property_Getter_Factory {
 		$property_getter->register( new Wordlift_Location_Property_Service( $entity_service ), array(
 			Wordlift_Schema_Service::FIELD_LOCATION,
 		) );
-//		$property_getter->register( new Wordlift_Address_Property_Service( $entity_service ), array(
-//			Wordlift_Schema_Service::FIELD_ADDRESS,
-//		) );
 		$property_getter->register( new Wordlift_Url_Property_Service(), array( Wordlift_Url_Property_Service::META_KEY ) );
 		$property_getter->register( new Wordlift_Double_Property_Service(), array(
 			Wordlift_Schema_Service::FIELD_GEO_LATITUDE,
