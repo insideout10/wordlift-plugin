9104a7b8c4ca8211390e47930a568dc8a15a70f8
naveen <kmnaveen101@gmail.com>
2020-12-02 17:39:17 +0530

    see #1214: cron test failing

--- tests/test-admin-top-entities.php
+++ tests/test-admin-top-entities.php
@@ -3,3 +3,15 @@
  * @since 3.27.7
  * @author Naveen Muthusamy <naveen@wordlift.io>
  */
+
+/**
+ * Class Top_Entities_Test
+ * @group admin
+ */
+class Top_Entities_Test extends Wordlift_Unit_Test_Case {
+
+	public function test_cron_should_be_registered_for_top_entities() {
+		$this->assertNotFalse( wp_next_scheduled( 'wl_admin_dashboard_top_entities' ) );
+	}
+
+}
