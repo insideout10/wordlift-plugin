fcd339a5a010ffccc64bd874d74332a1bcac2009
naveen <kmnaveen101@gmail.com>
2020-12-03 09:46:26 +0530

    see #1214 : [RED] test_a_single_function_should_be_present_in_action_hook test failing

--- tests/test-admin-top-entities.php
+++ tests/test-admin-top-entities.php
@@ -25,7 +25,11 @@ class Top_Entities_Test extends Wordlift_Unit_Test_Case {
 	public function test_upon_deactivation_should_remove_the_cron() {
 		deactivate_wordlift();
 		$this->assertFalse( wp_next_scheduled( 'wl_admin_dashboard_top_entities' ), "Cron should be removed" );
+	}
 
+	public function test_a_single_function_should_be_present_in_action_hook() {
+		global $wp_filter;
+		$this->assertArrayHasKey( Top_Entities::CRON_ACTION, $wp_filter, "Atleast a single callback should be present on the cron hook" );
 	}
 
 }
