221ea54d5ac79ac63cfa7e86699f0b604bdeea9c
David Riccitelli <david@wordlift.io>
2020-12-07 14:11:06 +0100

    see #1136: add integration test skeleton

---
+++ tests/test-dataset-ng-integration-tests.php
@@ -0,0 +1,32 @@
+<?php
+
+use Wordlift\Dataset\Sync_Object_Adapter_Factory;
+use Wordlift\Dataset\Sync_Post_Adapter;
+use Wordlift\Dataset\Sync_Post_Hooks;
+use Wordlift\Dataset\Sync_User_Adapter;
+use Wordlift\Object_Type_Enum;
+
+/**
+ * Test Hooks.
+ *
+ * @group dataset-ng
+ * @group integration-tests
+ */
+class Test_Dataset_Ng_Integration_Tests extends Wordlift_Unit_Test_Case {
+
+	/**
+	 * @var PHPUnit_Framework_MockObject_MockObject
+	 */
+	private $sync_service_mock;
+
+	function setUp() {
+		parent::setUp();
+
+		// These tests make sense only if `WL_FEATURES__DATASET_NG` is enabled.
+		if ( ! wp_validate_boolean( getenv( 'WL_FEATURES__DATASET_NG' ) ) ) {
+			$this->markTestSkipped( '`WL_FEATURES__DATASET_NG` not enabled.' );
+		}
+
+	}
+
+}
