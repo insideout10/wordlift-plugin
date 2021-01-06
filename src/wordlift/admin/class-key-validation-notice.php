3e303cb27f8a41790cb91dc054f06763bb513f7c
naveen <kmnaveen101@gmail.com>
2021-01-06 10:47:10 +0530

    see #1248: admin instance test passing

---
+++ src/wordlift/admin/class-key-validation-notice.php
@@ -0,0 +1,12 @@
+<?php
+namespace Wordlift\Admin;
+
+/**
+ * @since 3.28.0
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ */
+
+class Key_Validation_Notice {
+
+}
+
--- tests/test-admin-key-validation-notice.php
+++ tests/test-admin-key-validation-notice.php
@@ -10,6 +10,7 @@ class Admin_Key_Notice_Test extends Wordlift_Unit_Test_Case {
 
 	public function setUp() {
 		parent::setUp();
+		$this->instance = new \Wordlift\Admin\Key_Validation_Notice();
 	}
 
 	public function test_instance_not_null() {
