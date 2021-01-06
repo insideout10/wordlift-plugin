e340882d3f58878765ec7051ebdcec054c710398
naveen <kmnaveen101@gmail.com>
2021-01-06 11:09:17 +0530

    see #1248: validation error failing

--- tests/test-admin-key-validation-notice.php
+++ tests/test-admin-key-validation-notice.php
@@ -1,4 +1,7 @@
 <?php
+
+use Wordlift\Admin\Key_Validation_Notice;
+
 /**
  * @since 3.27.8
  * @author Naveen Muthusamy <naveen@wordlift.io>
@@ -10,11 +13,23 @@ class Admin_Key_Notice_Test extends Wordlift_Unit_Test_Case {
 
 	public function setUp() {
 		parent::setUp();
-		$this->instance = new \Wordlift\Admin\Key_Validation_Notice();
+		global $wp_filter;
+		$wp_filter = array();
+		$this->instance = new Key_Validation_Notice();
 	}
 
 	public function test_instance_not_null() {
 		$this->assertNotNull( $this->instance );
 	}
+
+	public function test_when_key_validation_is_errored_should_show_error() {
+		ob_start();
+		do_action( 'admin_notices' );
+		$html = ob_get_contents();
+		ob_end_clean();
+		$this->assertNotNull( $html );
+		$this->assertTrue( strlen( $html ) !== 0 );
+	}
+
 
 }
