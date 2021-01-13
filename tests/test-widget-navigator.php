a9731feacd1b2b540c400704c13d6cf8fe35ab96
naveen <kmnaveen101@gmail.com>
2021-01-13 10:42:52 +0530

    Reenable test

--- tests/test-widget-navigator.php
+++ tests/test-widget-navigator.php
@@ -167,31 +167,31 @@ class Navigator_Widget_Test extends Wordlift_Unit_Test_Case {
 
 	}
 
-//	public function test_when_the_post_type_supplied_should_restrict_by_post_type() {
-//		// Create an entity and link all the posts to post_1.
-//		$entity = $this->factory()->post->create( array( 'post_type' => 'entity' ) );
-//		// Lets create 2 posts and 2 pages.
-//		$post_1 = $this->create_navigator_post( $entity );
-//		$post_2 = $this->create_navigator_post( $entity );
-//		$post_3 = $this->create_navigator_post( $entity );
-//		$page_1 = $this->create_navigator_post( $entity, 'page' );
-//		$page_2 = $this->create_navigator_post( $entity, 'page' );
-//		// But we will restrict by post type.
-//		$_GET['post_id']    = $post_1;
-//		$_GET['uniqid']     = "random_id";
-//		$_GET['post_types'] = 'post,some-random-post-type';
-//		$posts               = _wl_navigator_get_data();
-//
-//		$expected_post_ids = array( $post_2, $post_3, $page_1, $page_2 );
-//
-//		$returned_post_ids = array($posts[0]['post']['id'], $posts[1]['post']['id'], $posts[2]['post']['id'], $posts[3]['post']['id']);
-//		// the first 2 returned posts should have post type post
-//		$this->assertEquals( $expected_post_ids, $returned_post_ids );
-//
-//		// we expect 4 posts since filler posts would be added.
-//		$this->assertEquals( 4, count( $posts ) );
-//
-//	}
+	public function test_when_the_post_type_supplied_should_restrict_by_post_type() {
+		// Create an entity and link all the posts to post_1.
+		$entity = $this->factory()->post->create( array( 'post_type' => 'entity' ) );
+		// Lets create 2 posts and 2 pages.
+		$post_1 = $this->create_navigator_post( $entity );
+		$post_2 = $this->create_navigator_post( $entity );
+		$post_3 = $this->create_navigator_post( $entity );
+		$page_1 = $this->create_navigator_post( $entity, 'page' );
+		$page_2 = $this->create_navigator_post( $entity, 'page' );
+		// But we will restrict by post type.
+		$_GET['post_id']    = $post_1;
+		$_GET['uniqid']     = "random_id";
+		$_GET['post_types'] = 'post,some-random-post-type';
+		$posts               = _wl_navigator_get_data();
+
+		$expected_post_ids = array( $post_2, $post_3, $page_1, $page_2 );
+
+		$returned_post_ids = array($posts[0]['post']['id'], $posts[1]['post']['id'], $posts[2]['post']['id'], $posts[3]['post']['id']);
+		// the first 2 returned posts should have post type post
+		$this->assertEquals( $expected_post_ids, $returned_post_ids, $posts );
+
+		// we expect 4 posts since filler posts would be added.
+		$this->assertEquals( 4, count( $posts ) );
+
+	}
 
 
 	public function test_when_post_type_not_supplied_in_navigator_shortcode_should_return_correctly_for_entities() {
