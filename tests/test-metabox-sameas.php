8b36d733e46760282865807bc62b35d481cb9fcd
naveen <kmnaveen101@gmail.com>
2021-01-14 10:35:08 +0530

    see #1017: task should be shown if uri present fails

---
+++ tests/test-metabox-sameas.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * @since 3.29.0
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ * @group metabox
+ */
+class Metabox_Sameas_Test extends Wordlift_Unit_Test_Case {
+
+	/**
+	 * @var WL_Metabox_Field_sameas
+	 */
+	private $sameas_metabox_instance;
+
+	private $instance;
+
+	public function setUp() {
+		parent::setUp();
+		$config                        = array(
+			'sameas' =>
+				array(
+					'entity_same_as' =>
+						array(
+							'predicate'   => 'http://schema.org/sameAs',
+							'type'        => 'uri',
+							'export_type' => 'http://schema.org/Thing',
+							'constraints' =>
+								array(
+									'cardinality' => INF,
+								),
+							'input_field' => 'sameas',
+						),
+				),
+		);
+		$this->sameas_metabox_instance = new WL_Metabox_Field_sameas( $config );
+		$this->instance = new Sameas_Metabox_Task_Validator();
+	}
+
+
+//	public function test_on_plugin_upgrade_should_show_clean_up_task_in_dashboard_if_invalid_sameas_present() {
+//		do_action( 'upgrader_process_complete' );
+//
+//	}
+
+
+	public function test_many_posts_with_same_dataset_uris_should_find_only_one_post() {
+
+		$post_ids = $this->factory()->post->create_many( 100, array(
+			'post_type' => 'entity',
+		), array(
+			'post_title' => new WP_UnitTest_Generator_Sequence( 'Sameas task test %s' ),
+		) );
+
+		// we pick a post id and add an invalid same as.
+		$dataset_uri = $this->configuration_service->get_dataset_uri();
+
+		$invalid_dataset_uris = array(
+			$dataset_uri . "/aaa",
+			$dataset_uri . "/bbb",
+		);
+
+		$sample_post_ids = array_slice( $post_ids, 0, 5 );
+
+		foreach ( $sample_post_ids as $post_id ) {
+			$_POST['post_ID'] = $post_id;
+			$this->sameas_metabox_instance->save_data( $invalid_dataset_uris );
+		}
+
+		// Check if we need to add a cleanup task
+		$this->assertTrue( $this->instance->is_cleanup_task_should_be_shown() );
+	}
+
+
+}
