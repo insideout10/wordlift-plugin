f23cd811bb88bb503ce66dce779d5c032ea50df4
naveen <kmnaveen101@gmail.com>
2021-01-14 17:13:02 +0530

    see #1017: refactoring

--- src/install/class-wordlift-install-3-28-0.php
+++ src/install/class-wordlift-install-3-28-0.php
@@ -15,19 +15,21 @@ class Wordlift_Install_3_28_0 extends Wordlift_Install {
 
 		global $wpdb;
 
-		$dataset_uri          = Wordlift_Configuration_Service::get_instance()->get_dataset_uri();
+		$dataset_uri = Wordlift_Configuration_Service::get_instance()->get_dataset_uri();
 
 		if ( ! $dataset_uri ) {
 			// Dont run the query, running it would delete all the values.
 			return;
 		}
-
 		$post_meta_table_name = $wpdb->postmeta;
-		$dataset_uri          = $dataset_uri . '%';
-		$meta_key             = Wordlift_Schema_Service::FIELD_SAME_AS;
-		$sql                  = "DELETE FROM $post_meta_table_name WHERE meta_key='$meta_key' 
-AND ( ( meta_value NOT LIKE 'https://%' AND meta_value NOT LIKE 'http://%' )
-OR meta_value LIKE '$dataset_uri' )";
+		$meta_key = Wordlift_Schema_Service::FIELD_SAME_AS;
+
+		$http        = $wpdb->esc_like( 'http://' ) . '%';
+		$https       = $wpdb->esc_like( 'https://' ) . '%';
+		$dataset_uri = $wpdb->esc_like( $dataset_uri ) . '%';
+		$sql         = $wpdb->prepare( "DELETE FROM $post_meta_table_name WHERE meta_key='$meta_key' 
+AND ( ( meta_value NOT LIKE %s AND meta_value NOT LIKE %s )
+OR meta_value LIKE %s )", $http, $https, $dataset_uri );
 		$wpdb->query( $sql );
 
 	}
