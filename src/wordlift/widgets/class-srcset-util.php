d3cae03198172d5add311bf7c4a69bdc138816d9
naveen <kmnaveen101@gmail.com>
2021-01-16 11:18:44 +0530

    see #1250: adding srcset filter

--- src/wordlift/widgets/class-srcset-util.php
+++ src/wordlift/widgets/class-srcset-util.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Wordlift\Widgets;
 /**
  * @since 3.28.0
@@ -13,14 +14,16 @@ class Srcset_Util {
 	public static function get_srcset( $post_id, $widget_name ) {
 
 		$srcset = "";
-		$small       = get_the_post_thumbnail_url( $post_id, 'small' );
-		$medium      = get_the_post_thumbnail_url( $post_id, 'medium' );
+		$small  = get_the_post_thumbnail_url( $post_id, 'small' );
+		$medium = get_the_post_thumbnail_url( $post_id, 'medium' );
 		if ( $small ) {
-			$srcset  .= $small;
+			$srcset .= $small;
 		}
 		if ( $medium ) {
 			$srcset .= " $medium";
 		}
+
+		$srcset = apply_filters( "wordlift_${widget_name}_thumbnail_srcset", $srcset );
 
 		return $srcset;
 
