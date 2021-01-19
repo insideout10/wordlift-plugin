e3334b9195431d285b6ac8c251d3959bd7fcd51c
naveen <kmnaveen101@gmail.com>
2021-01-19 15:25:30 +0530

    see #1033: two referenced entities to be present test passing

--- src/wordlift/jsonld/class-jsonld-homepage.php
+++ src/wordlift/jsonld/class-jsonld-homepage.php
@@ -7,8 +7,19 @@ namespace Wordlift\Jsonld;
  * @author Naveen Muthusamy <naveen@wordlift.io>
  */
 class Jsonld_Homepage {
+	/**
+	 * @var \Wordlift_Relation_Service
+	 */
+	private $relation_service;
 
-	public function __construct() {
+	/**
+	 * Jsonld_Homepage constructor.
+	 *
+	 * @param $relation_service \Wordlift_Relation_Service
+	 */
+	public function __construct( $relation_service ) {
+
+		$this->relation_service = $relation_service;
 
 		add_filter( 'wl_website_jsonld', array( $this, 'add_mentions_if_singular' ) );
 
@@ -17,7 +28,7 @@ class Jsonld_Homepage {
 	public function add_mentions_if_singular( $jsonld ) {
 
 		if ( is_singular() && get_post_type( get_the_ID() ) !== 'entity' ) {
-			$jsonld['mentions'] = array();
+			$jsonld['mentions'] = $this->relation_service->get_objects( get_the_ID(), 'ids', null, 'publish' );
 		}
 
 		return $jsonld;
