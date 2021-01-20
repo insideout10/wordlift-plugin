2ee3807c4bd3e9f3f63f820e9746a93fb5cd4752
naveen <kmnaveen101@gmail.com>
2021-01-20 17:17:03 +0530

    see #1131: test passing for yoast flag

--- src/wordlift/entity/class-entity-no-index-flag.php
+++ src/wordlift/entity/class-entity-no-index-flag.php
@@ -13,6 +13,14 @@ class Entity_No_Index_Flag {
 	public function __construct() {
 
 		add_action( 'wp_insert_post', function ( $post_id, $post, $update ) {
+
+			$post_type = get_post_type( $post_id );
+
+			if ( $post_type !== \Wordlift_Entity_Service::TYPE_NAME ) {
+				// Dont set this flag for any other post types.
+				return;
+			}
+
 			// We need to set this flag only on entity creation.
 			if ( ! $update ) {
 				update_post_meta( $post_id, self::YOAST_POST_NO_INDEX_FLAG, 1 );
