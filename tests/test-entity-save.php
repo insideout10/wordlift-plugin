6549df6413a53f3750ebbbb366a6b3dde71dd65a
naveen <kmnaveen101@gmail.com>
2021-01-20 17:11:00 +0530

    see #1131: test failing for flag removal

--- tests/test-entity-save.php
+++ tests/test-entity-save.php
@@ -29,4 +29,21 @@ class Entity_Save_Test extends Wordlift_Unit_Test_Case {
 	}
 
 
+
+	public function test_when_entity_is_updated_should_remove_yoast_no_index_flag() {
+
+		new Entity_No_Index_Flag();
+
+		$entity = $this->factory()->post->create( array( 'post_type' => 'entity' ) );
+
+		// update the entity
+		wp_update_post(array('ID' => $entity, 'post_content' => 'test'));
+
+		$result = get_post_meta( $entity, '_yoast_wpseo_meta-robots-noindex', true );
+
+		$this->assertNull($result, 'The no index flag should not be present');
+
+	}
+
+
 }
