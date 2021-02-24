b61ee624d7d0beeb6cbd5552fc94b60b81f420c7
naveen <kmnaveen101@gmail.com>
2021-02-24 14:36:39 +0530

    see #1033: fix tests

--- tests/test-jsonld-static-home-page.php
+++ tests/test-jsonld-static-home-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @since 3.27.9
+ * @since 3.30.0
  * @author Naveen Muthusamy <naveen@wordlift.io>
  */
 
@@ -123,7 +123,7 @@ class Jsonld_Static_Home_Page_Test extends Wordlift_Unit_Test_Case {
 		$this->assertArrayHasKey( 'isPartOf', $jsonld );
 		$this->assertTrue( is_array( $jsonld['isPartOf'] ) );
 		$this->assertArrayHasKey( '@id', $jsonld['isPartOf'], '@id should be present for entity' );
-		$this->assertEquals( get_home_url() . '#website', $jsonld['isPartOf']['@id'] );
+		$this->assertEquals( get_home_url() . '/#website', $jsonld['isPartOf']['@id'] );
 		$this->assertTrue( array_key_exists( 'mainEntity', $jsonld ), 'Should have mainEntity property in the  jsonld' );
 		$this->assertTrue( is_array( $jsonld['mainEntity'] ) );
 
