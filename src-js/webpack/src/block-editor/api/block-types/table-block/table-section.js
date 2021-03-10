4801ac3893df83eaf43be8ee8ea0c6be2ada4ccf
naveen <kmnaveen101@gmail.com>
2021-03-10 10:34:26 +0530

    see #1311: table section test failing

---
+++ src-js/webpack/src/block-editor/api/block-types/table-block/table-section.js
@@ -0,0 +1,15 @@
+/**
+ * Represents a single section of table.
+ * @since 3.29.1
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ */
+export default class TableSection {
+
+    /**
+     * @param tableSectionData An array of table rows data
+     */
+    constructor(tableSectionData) {
+
+    }
+
+}
---
+++ src-js/webpack/src/block-editor/tests/table-section-block.test.js
@@ -0,0 +1,34 @@
+import TableSection from "../api/block-types/table-block/table-section";
+
+test("when a table section given, should create html content separated by delimiter", () => {
+
+    const mockTableSectionData = [
+        {
+            "cells": [
+                {
+                    "content": "<span id=\"urn:local-annotation-520881\" class=\"textannotation disambiguated\" itemid=\"http://data.wordlift.io/wl040/entity/my_entity\">My entity</span>",
+                    "tag": "td"
+                },
+                {
+                    "content": "",
+                    "tag": "td"
+                }
+            ]
+        },
+        {
+            "cells": [
+                {
+                    "content": "<span id=\"urn:local-annotation-253356\" class=\"textannotation disambiguated\" itemid=\"http://data.wordlift.io/wl040/entity/foo_bar\">foo bar</span><br>test",
+                    "tag": "td"
+                },
+                {
+                    "content": "",
+                    "tag": "td"
+                }
+            ]
+        }
+    ]
+    const tableSection = new TableSection(mockTableSectionData);
+    expect(tableSection.rows).toEqual(2)
+
+})
