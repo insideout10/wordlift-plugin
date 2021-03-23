0ff5191158cc1787091d73f6bca41be38401310e
naveen <kmnaveen101@gmail.com>
2021-03-23 09:35:21 +0530

    see #940: badge generator generate html test pass

--- src/wordlift/vocabulary/menu/badge/class-badge-generator.php
+++ src/wordlift/vocabulary/menu/badge/class-badge-generator.php
@@ -17,11 +17,19 @@ class Badge_Generator {
 	 */
 	public static function round_to_nearest_hundred( $number ) {
 
+		$number = (int) $number;
+
 		if ( $number < 100 ) {
 			return $number;
 		}
 
 		return floor( $number / 100 ) * 100;
+	}
+
+	public static function generate_html( $number ) {
+		$round = self::round_to_nearest_hundred( $number );
+
+		return "<span class=\"count-300\"><span class=\"pending-term-count\">$round+</span></span>";
 	}
 
 
--- tests/test-vocabulary-badge.php
+++ tests/test-vocabulary-badge.php
@@ -25,5 +25,12 @@ class Vocabulary_Badge_Test extends \Wordlift_Vocabulary_Unit_Test_Case {
 	}
 
 
+	public function test_badge_generated_html() {
+		$result = Badge_Generator::generate_html( 340 );
+		$expected_html = "<span class=\"count-300\"><span class=\"pending-term-count\">300+</span></span>";
+		$this->assertEquals( $result, $expected_html );
+	}
+
+
 
 }
