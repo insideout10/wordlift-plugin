004cfa51efb10f4162e828e56e3a9ddfc3c21948
naveen <kmnaveen101@gmail.com>
2021-03-24 16:16:43 +0530

    see #940: test pass for legacy jsonld

--- src/wordlift/vocabulary/jsonld/class-post-jsonld.php
+++ src/wordlift/vocabulary/jsonld/class-post-jsonld.php
@@ -10,17 +10,17 @@ use Wordlift\Vocabulary\Api\Entity_Rest_Endpoint;
 
 class Post_Jsonld {
 
-	public function enhance_post_jsonld(){
+	public function enhance_post_jsonld() {
 		add_filter( 'wl_post_jsonld_array', array( $this, 'wl_post_jsonld_array' ), 11, 2 );
 		add_filter( 'wl_after_get_jsonld', array( $this, 'wl_after_get_jsonld' ), 11, 2 );
 	}
 
-	public function wl_post_jsonld_array($arr, $post_id){
+	public function wl_post_jsonld_array( $arr, $post_id ) {
 
 		$jsonld     = $arr['jsonld'];
 		$references = $arr['references'];
 
-		$this->add_mentions($post_id, $jsonld, $references);
+		$this->add_mentions( $post_id, $jsonld, $references );
 
 		return array(
 			'jsonld'     => $jsonld,
@@ -37,14 +37,14 @@ class Post_Jsonld {
 			// Loop through the tags and push it to references.
 			foreach ( $tags as $tag ) {
 
-				$tag_is_external_entity = get_term_meta( $tag->term_id, Entity_Rest_Endpoint::EXTERNAL_ENTITY_META_KEY, true );
+				$is_matched = intval( get_term_meta( $tag->term_id, Entity_Rest_Endpoint::IGNORE_TAG_FROM_LISTING, true ) ) === 1;
 
-				if($tag_is_external_entity == 1){
+				if ( $is_matched ) {
 					$jsonld['mentions'][] = array(
 						'@id'           => get_term_link( $tag->term_id ) . '#id',
 						'@type'         => get_term_meta( $tag->term_id, Entity_Rest_Endpoint::TYPE_META_KEY, true ),
 						'name'          => $tag->name,
-						'description'   => !empty($tag->description) ?: get_term_meta( $tag->term_id, Entity_Rest_Endpoint::DESCRIPTION_META_KEY, true ),
+						'description'   => ! empty( $tag->description ) ? $tag->description : get_term_meta( $tag->term_id, Entity_Rest_Endpoint::DESCRIPTION_META_KEY, true ),
 						'sameAs'        => get_term_meta( $tag->term_id, Entity_Rest_Endpoint::SAME_AS_META_KEY ),
 						'alternateName' => get_term_meta( $tag->term_id, Entity_Rest_Endpoint::ALTERNATIVE_LABEL_META_KEY )
 					);
@@ -55,23 +55,23 @@ class Post_Jsonld {
 
 	}
 
-	public function wl_after_get_jsonld($jsonld, $post_id){
+	public function wl_after_get_jsonld( $jsonld, $post_id ) {
 
 		if ( ! is_array( $jsonld ) || count( $jsonld ) === 0 ) {
 			return $jsonld;
 		}
 
 		foreach ( $jsonld as $key => $value ) {
-			if($value['@type'] === 'Article' && isset($value['image'])){
+			if ( $value['@type'] === 'Article' && isset( $value['image'] ) ) {
 				$image = $value['image'];
 			}
-			if($value['@type'] === 'Recipe' && !isset($value['image'])){
+			if ( $value['@type'] === 'Recipe' && ! isset( $value['image'] ) ) {
 				$index = $key;
 			}
 		}
 
-		if(isset($index) && !empty($image)){
-			$jsonld[$index]['image'] = $image;
+		if ( isset( $index ) && ! empty( $image ) ) {
+			$jsonld[ $index ]['image'] = $image;
 		}
 
 		return $jsonld;
--- tests/test-vocabulary-jsonld.php
+++ tests/test-vocabulary-jsonld.php
@@ -1,4 +1,7 @@
 <?php
+
+use Wordlift\Vocabulary\Api\Entity_Rest_Endpoint;
+
 /**
  * @since 3.30.0
  * @group vocabulary
@@ -7,6 +10,48 @@
 class Vocabulary_Jsonld_Test extends \Wordlift_Vocabulary_Unit_Test_Case {
 
 	public function test_given_post_with_legacy_term_match_data_should_generate_valid_jsonld() {
+		$term_id = $this->create_unmatched_tag( "foo" );
+		wp_update_term( $term_id, 'post_tag', array(
+			'description' => 'test_term_description'
+		) );
+		$term = get_term( $term_id );
+		update_term_meta( $term_id, Entity_Rest_Endpoint::IGNORE_TAG_FROM_LISTING, 1 );
+		$post_id = $this->factory()->post->create();
+		$tags    = array( $term->slug );
+		wp_add_post_tags( $post_id, $tags );
+		$same_as          = array( 'https://google.com', 'https://foo.com' );
+		$alternate_labels = array( 'label 1', 'label 2' );
+
+		foreach ( $same_as as $item ) {
+
+			add_term_meta( $term_id, Entity_Rest_Endpoint::SAME_AS_META_KEY, $item );
+		}
+
+		foreach ( $alternate_labels as $item ) {
+			add_term_meta( $term_id, Entity_Rest_Endpoint::ALTERNATIVE_LABEL_META_KEY, $item );
+		}
+
+		update_term_meta( $term_id, Entity_Rest_Endpoint::DESCRIPTION_META_KEY, 'test_entity_description' );
+		update_term_meta( $term_id, Entity_Rest_Endpoint::TYPE_META_KEY, 'Product' );
+
+		$data   = array(
+			'jsonld'     => array(),
+			'references' => array()
+		);
+		$result = apply_filters( 'wl_post_jsonld_array', $data, $post_id );
+		$jsonld = $result['jsonld'];
+		$this->assertArrayHasKey( 'mentions', $jsonld );
+		$mentioned_entity = $jsonld['mentions'][0];
+
+		$result_same_as = $mentioned_entity['sameAs'];
+
+
+		$this->assertEquals( $result_same_as, $same_as );
+
+
+		$this->assertEquals( $mentioned_entity['alternateName'], $alternate_labels );
+		$this->assertEquals( $mentioned_entity['description'], 'test_term_description' );
+		$this->assertEquals( $mentioned_entity['@type'], 'Product' );
 
 	}
 
