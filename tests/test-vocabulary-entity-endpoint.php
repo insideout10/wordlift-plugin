47ca205aadb5b897c2d0bce7faf76870e5396331
naveen <kmnaveen101@gmail.com>
2021-03-30 09:41:47 +0530

    see #940: refactor tests

--- tests/test-vocabulary-entity-endpoint.php
+++ tests/test-vocabulary-entity-endpoint.php
@@ -47,11 +47,11 @@ class Accept_Reject_Entity_Endpoint_Test extends \Wordlift_Vocabulary_Unit_Test_
 
 		$user_id = $this->factory()->user->create( array( 'role' => 'administrator' ) );
 		wp_set_current_user( $user_id );
-		$request = new WP_REST_Request( 'POST', $this->accept_route );
-		$request->set_header( 'content-type', 'application/json' );
-		$json_data = json_encode( array( 'entity' => $entity, 'term_id' => $term_id ) );
-		$request->set_body( $json_data );
-		$response = $this->server->dispatch( $request );
+
+
+		$response = $this->send_accept_entity_request( $entity, $term_id );
+
+
 		$this->assertEquals( 200, $response->get_status(), 'Accept endpoint should be registered' );
 
 		$entity         = Entity_Factory::get_instance( $term_id );
@@ -77,13 +77,11 @@ class Accept_Reject_Entity_Endpoint_Test extends \Wordlift_Vocabulary_Unit_Test_
 
 		$user_id = $this->factory()->user->create( array( 'role' => 'administrator' ) );
 		wp_set_current_user( $user_id );
-		$request = new WP_REST_Request( 'POST', $this->accept_route );
-		$request->set_header( 'content-type', 'application/json' );
-		$json_data = json_encode( array( 'entity' => $entity, 'term_id' => $term_id ) );
-		$request->set_body( $json_data );
-		$response = $this->server->dispatch( $request );
+
+		$response = $this->send_accept_entity_request( $entity, $term_id );
 		// Dispatch the request twice, we should have 2 entities in meta by now.
-		$response = $this->server->dispatch( $request );
+		$this->send_accept_entity_request( $entity, $term_id );
+
 		$this->assertEquals( 200, $response->get_status(), 'Accept endpoint should be registered' );
 
 		$entity   = Entity_Factory::get_instance( $term_id );
@@ -99,51 +97,6 @@ class Accept_Reject_Entity_Endpoint_Test extends \Wordlift_Vocabulary_Unit_Test_
 		//$this->assertNotNull( 1, get_term_meta( $term_id, Entity_Rest_Endpoint::EXTERNAL_ENTITY_META_KEY ) );
 		$this->assertEquals( 1, get_term_meta( $term_id, Entity_Rest_Endpoint::IGNORE_TAG_FROM_LISTING, true ) );
 	}
-
-
-	public function test_reject_endpoint_should_return_200_and_remove_all_the_data() {
-
-		// create a term
-		$term_data = wp_insert_term( 'foo', 'post_tag' );
-		$term_id   = $term_data['term_id'];
-
-		$entity = $this->getMockEntityData();
-
-		$user_id = $this->factory()->user->create( array( 'role' => 'administrator' ) );
-		wp_set_current_user( $user_id );
-
-
-		$request = new WP_REST_Request( 'POST', $this->accept_route );
-		$request->set_header( 'content-type', 'application/json' );
-		$json_data = json_encode( array( 'entity' => $entity, 'term_id' => $term_id ) );
-		$request->set_body( $json_data );
-		$response = $this->server->dispatch( $request );
-
-
-		// Insert a term meta for no match entity uri.
-		add_term_meta( $term_id, Entity_Rest_Endpoint::IGNORE_TAG_FROM_LISTING, 'https://google.com' );
-
-
-		$request = new WP_REST_Request( 'POST', $this->reject_route );
-		$request->set_header( 'content-type', 'application/json' );
-		$json_data = json_encode( array( 'term_id' => $term_id ) );
-		$request->set_body( $json_data );
-		$response = $this->server->dispatch( $request );
-		$this->assertEquals( 200, $response->get_status(), 'Reject endpoint should be registered' );
-		// Check if we have removed all values in meta
-		$this->assertCount( 0, get_term_meta( $term_id, Entity_Rest_Endpoint::SAME_AS_META_KEY ) );
-		$this->assertCount( 0, get_term_meta( $term_id, Entity_Rest_Endpoint::ALTERNATIVE_LABEL_META_KEY ) );
-		$this->assertCount( 0, get_term_meta( $term_id, Entity_Rest_Endpoint::DESCRIPTION_META_KEY ) );
-		$this->assertCount( 0, get_term_meta( $term_id, Entity_Rest_Endpoint::TYPE_META_KEY ) );
-		$this->assertCount( 0, get_term_meta( $term_id, Entity_Rest_Endpoint::EXTERNAL_ENTITY_META_KEY ) );
-		$this->assertCount( 0, get_term_meta( $term_id, Entity_Rest_Endpoint::IGNORE_TAG_FROM_LISTING ), 'No match should be cleared on undo' );
-		$this->assertEquals( '', get_term_meta( $term_id, Entity_Rest_Endpoint::IGNORE_TAG_FROM_LISTING, true ) );
-	}
-
-	public function test_should_store_multiple_entity_data_in_meta() {
-
-	}
-
 
 	public function testMarkedAsNoMatchShoulReturn200() {
 		$user_id = $this->factory()->user->create( array( 'role' => 'administrator' ) );
@@ -165,6 +118,7 @@ class Accept_Reject_Entity_Endpoint_Test extends \Wordlift_Vocabulary_Unit_Test_
 		$json_data = json_encode( array( 'term_id' => $term_id ) );
 		$request->set_body( $json_data );
 		$response = $this->server->dispatch( $request );
+
 		$this->assertEquals( 200, $response->get_status(), 'No match endpoint should be registered' );
 		$results = get_term_meta( $term_id, Entity_Rest_Endpoint::IGNORE_TAG_FROM_LISTING );
 		$this->assertCount( 2, $results );
@@ -206,7 +160,26 @@ class Accept_Reject_Entity_Endpoint_Test extends \Wordlift_Vocabulary_Unit_Test_
 
 
 	public function test_when_the_entity_is_rejected_should_remove_only_the_entity_from_jsonld() {
+		// accept 2 entities.
 
+
+		// reject 1 entity
+	}
+
+	/**
+	 * @param array $entity
+	 * @param $term_id
+	 *
+	 * @return mixed
+	 */
+	private function send_accept_entity_request( array $entity, $term_id ) {
+		$request = new WP_REST_Request( 'POST', $this->accept_route );
+		$request->set_header( 'content-type', 'application/json' );
+		$json_data = json_encode( array( 'entity' => $entity, 'term_id' => $term_id ) );
+		$request->set_body( $json_data );
+		$response = $this->server->dispatch( $request );
+
+		return $response;
 	}
 
 }
