fab96bf7ebdc3f090c85b4fad58ed94c0678b108
Akshay Raje <akshay.raje@gmail.com>
2021-04-02 15:27:27 +0530

    'Vocabulary' Widget to be renamed 'Glossary'

--- src/js/dist/block-editor.asset.php
+++ src/js/dist/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-rich-text'), 'version' => '1b292df0118015104b05ca0b4d8ac240');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-rich-text'), 'version' => 'c35c0e126f242a602896016f5fc8cf79');
