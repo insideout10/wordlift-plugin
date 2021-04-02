27b1906c3ab2ce3e904d0ce08f2b01ff0337a577
naveen <kmnaveen101@gmail.com>
2021-04-02 10:13:46 +0530

    see #1131: php 5.3 compat

--- src/wordlift/entity/class-entity-no-index-flag.php
+++ src/wordlift/entity/class-entity-no-index-flag.php
@@ -12,7 +12,9 @@ class Entity_No_Index_Flag {
 
 	public function __construct() {
 
-		add_action( 'wp_insert_post', function ( $post_id, $post, $update ) {
+		$no_index_flag = self::YOAST_POST_NO_INDEX_FLAG;
+
+		add_action( 'wp_insert_post', function ( $post_id, $post, $update ) use ( $no_index_flag ) {
 
 			$post_type = get_post_type( $post_id );
 
@@ -23,18 +25,18 @@ class Entity_No_Index_Flag {
 
 			// We need to set this flag only on entity creation.
 			if ( ! $update ) {
-				update_post_meta( $post_id, self::YOAST_POST_NO_INDEX_FLAG, 1 );
+				update_post_meta( $post_id, $no_index_flag, 1 );
 			}
 
 		}, PHP_INT_MAX, 3 );
 
 
-		add_action( 'post_updated', function ( $post_id ) {
+		add_action( 'post_updated', function ( $post_id ) use ( $no_index_flag ) {
 			if ( get_post_type( $post_id ) !== \Wordlift_Entity_Service::TYPE_NAME ) {
 				return;
 			}
 			// if the post is updated, remove this flag
-			delete_post_meta( $post_id, self::YOAST_POST_NO_INDEX_FLAG );
+			delete_post_meta( $post_id, $no_index_flag );
 		}, PHP_INT_MAX );
 
 	}
