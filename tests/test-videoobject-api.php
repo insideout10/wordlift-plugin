14c2c02c26616c1f17d6a87b819508df2f169888
naveen <kmnaveen101@gmail.com>
2021-04-12 17:06:22 +0530

    see #1344: vimeo test fail

--- tests/test-videoobject-api.php
+++ tests/test-videoobject-api.php
@@ -8,7 +8,7 @@ use Wordlift\Videoobject\Provider\Youtube;
  * Class Videoobject_Youtube_Api_Test
  * @group videoobject
  */
-class Videoobject_Youtube_Api_Test extends Wordlift_Videoobject_Unit_Test_Case {
+class Videoobject_Api_Test extends Wordlift_Videoobject_Unit_Test_Case {
 
 	public function setUp() {
 		parent::setUp();
@@ -42,6 +42,33 @@ EOF;
 		$this->assertNotNull( $video->duration );
 		$this->assertNotNull( $video->content_url );
 		$this->assertNotNull( $video->embed_url );
+	}
+
+	public function test_on_save_post_with_vimeo_video_should_store_it() {
+		$post_content = <<<EOF
+<!-- wp:embed {"url":"https://vimeo.com/162427937","type":"video","providerNameSlug":"vimeo","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-vimeo wp-block-embed-vimeo wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://vimeo.com/162427937
+</div></figure>
+<!-- /wp:embed -->
+EOF;
+		$post_id      = $this->create_post_with_content( $post_content );
+		$videos       = Video_Storage_Factory::get_storage()->get_all_videos( $post_id );
+		// we should have 1 video on the storage.
+		$this->assertCount( 1, $videos );
+		/**
+		 * @var $video Video
+		 */
+		$video = $videos[0];
+		// check all of the properties are not null.
+		$this->assertNotNull( $video->name );
+		$this->assertNotNull( $video->description );
+		$this->assertNotNull( $video->thumbnail_urls );
+		$this->assertNotNull( $video->upload_date );
+		$this->assertNotNull( $video->duration );
+		$this->assertNotNull( $video->content_url );
+		$this->assertNotNull( $video->embed_url );
+
 	}
 
 }
