cabb06c6b6eafa881cb1b04a822d5d72fa62350d
naveen <kmnaveen101@gmail.com>
2021-04-12 16:44:46 +0530

    see #1344: youtube api data test pass

--- tests/test-videoobject-youtube-api.php
+++ tests/test-videoobject-youtube-api.php
@@ -27,21 +27,21 @@ https://www.youtube.com/watch?v=fJAPDAK4GiI
 <!-- /wp:embed -->
 EOF;
 		$post_id      = $this->create_post_with_content( $post_content );
-		$videos = Video_Storage_Factory::get_storage()->get_all_videos( $post_id );
+		$videos       = Video_Storage_Factory::get_storage()->get_all_videos( $post_id );
 		// we should have 1 video on the storage.
-		$this->assertCount( 1,  $videos );
+		$this->assertCount( 1, $videos );
 		/**
 		 * @var $video Video
 		 */
 		$video = $videos[0];
 		// check all of the properties are not null.
-		$this->assertNotNull($video->name);
-		$this->assertNotNull($video->description);
-		$this->assertNotNull($video->thumbnail_urls);
-		$this->assertNotNull($video->upload_date);
-		$this->assertNotNull($video->duration);
-		$this->assertNotNull($video->content_url);
-		$this->assertNotNull($video->embed_url);
+		$this->assertNotNull( $video->name );
+		$this->assertNotNull( $video->description );
+		$this->assertNotNull( $video->thumbnail_urls );
+		$this->assertNotNull( $video->upload_date );
+		$this->assertNotNull( $video->duration );
+		$this->assertNotNull( $video->content_url );
+		$this->assertNotNull( $video->embed_url );
 	}
 
 }
