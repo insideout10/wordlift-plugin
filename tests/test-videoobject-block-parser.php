97af9f113d3906a0cd0e9f948abafe8b1309d576
naveen <kmnaveen101@gmail.com>
2021-04-12 09:49:12 +0530

    see #1344: vimeo url correctly parsed

--- tests/test-videoobject-block-parser.php
+++ tests/test-videoobject-block-parser.php
@@ -5,8 +5,7 @@ use Wordlift\Videoobject\Parser\Parser_Factory;
 class Videoobject_Block_Parser_Test extends \Wordlift_Unit_Test_Case {
 
 
-	public function test_should_get_video_url_from_embed_block() {
-
+	public function test_should_get_video_url_from_youtube_block() {
 		$post_content = <<<EOF
 <!-- wp:embed {"url":"https://www.youtube.com/watch?v=fJAPDAK4GiI","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
 <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
@@ -14,16 +13,36 @@ https://www.youtube.com/watch?v=fJAPDAK4GiI
 </div></figure>
 <!-- /wp:embed -->
 EOF;
-
 		$post_id = $this->factory()->post->create( array(
 			'post_content' => $post_content
 		) );
-
 		$parser     = Parser_Factory::get_parser( Parser_Factory::BLOCK_EDITOR );
 		$video_urls = $parser->get_videos( $post_id );
 		$this->assertCount( 1, $video_urls );
 		$this->assertEquals( 'https://www.youtube.com/watch?v=fJAPDAK4GiI', $video_urls[0]->get_url() );
 	}
+
+
+	public function test_should_get_video_from_vimeo_block() {
+		$post_content = <<<EOF
+<!-- wp:embed {"url":"https://vimeo.com/162427937","type":"video","providerNameSlug":"vimeo","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-vimeo wp-block-embed-vimeo wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://vimeo.com/162427937
+</div></figure>
+<!-- /wp:embed -->
+EOF;
+		$post_id = $this->factory()->post->create( array(
+			'post_content' => $post_content
+		) );
+		$parser     = Parser_Factory::get_parser( Parser_Factory::BLOCK_EDITOR );
+		$video_urls = $parser->get_videos( $post_id );
+		$this->assertCount( 1, $video_urls );
+		$this->assertEquals( 'https://vimeo.com/162427937', $video_urls[0]->get_url() );
+
+	}
+
+	// test video block
+	// test videopress block
 
 
 }
