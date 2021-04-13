b85eb7a7a672e129b956683ccd09833cdeaee9b4
naveen <kmnaveen101@gmail.com>
2021-04-13 10:57:08 +0530

    see #1344: vimeo class first iteration complete

--- src/wordlift/videoobject/provider/class-vimeo.php
+++ src/wordlift/videoobject/provider/class-vimeo.php
@@ -6,6 +6,8 @@
 
 namespace Wordlift\Videoobject\Provider;
 
+use Wordlift\Videoobject\Data\Video\Video;
+
 /**
  * Class Vimeo
  * @package Wordlift\Videoobject\Provider
@@ -40,16 +42,93 @@ class Vimeo implements Provider {
 		return '/videos/' . $matches[3];
 	}
 
+	private function get_api_key() {
+		return get_option( self::API_FIELD_NAME, false );
+	}
 
 	public function get_videos_data( $videos ) {
+
+		$key = $this->get_api_key();
+
+		if ( ! $key ) {
+			return array();
+		}
+
 		$urls = array_map( function ( $video ) {
 			return $video->get_url();
 		}, $videos );
-		$ids  = $this->get_video_ids_for_api( $urls );
-		wp_remote_get( add_query_arg( array(
+
+		$ids = $this->get_video_ids_for_api( $urls );
+
+		$api_url = add_query_arg( array(
 			'uris'   => $ids,
 			'fields' => 'name,description,link,uri,duration,release_time,pictures'
-		) ) );
+		), self::API_URL );
+
+		$response = wp_remote_get( $api_url, array(
+			'headers' => array(
+				'bearer ' . $key
+			)
+		) );
+
+		$response_body = wp_remote_retrieve_body( $response );
+		// we need to parse the body.
+		if ( ! array_key_exists( 'body', $response ) ||
+		     ! array_key_exists( 'data', $response['body'] ) ) {
+			return array();
+		}
+
+		$video_list = $response['body']['data'];
+
+		if ( ! is_array( $video_list ) ) {
+			// Return if we cant parse the response.
+			return array();
+		}
+
+		return array_map( array( $this, 'get_video_from_video_data' ), $video_list );
 
 	}
+
+	public function get_video_from_video_data( $vimeo_video_data ) {
+		if ( ! $vimeo_video_data ) {
+			// If valid data not supplied dont init the object.
+			return;
+		}
+		$video              = new Video();
+		$video->name        = $vimeo_video_data['name'];
+		$video->description = $vimeo_video_data['description'];
+
+		$video->content_url = $vimeo_video_data['link'];
+		$video->embed_url   = "https://player.vimeo.com/video/" . $this->get_id( $vimeo_video_data );
+		if ( is_numeric( $vimeo_video_data['duration'] ) ) {
+			$video->duration = "PT" . $vimeo_video_data['duration'] . "S";
+		}
+		$video->upload_date    = $vimeo_video_data['release_time'];
+		$video->thumbnail_urls = $this->set_thumbnail_urls( $vimeo_video_data );
+	}
+
+	private function get_id( $api_response_data ) {
+		return str_replace( "/videos/", "", $api_response_data['uri'] );
+	}
+
+
+	private function set_thumbnail_urls( $api_response_data ) {
+
+		if ( ! array_key_exists( 'pictures', $api_response_data ) || ! array_key_exists( 'sizes',
+				$api_response_data['pictures'] ) ) {
+			return array();
+		}
+		if ( ! is_array( $api_response_data['pictures']['sizes'] ) ) {
+			return array();
+		}
+		$pictures = $api_response_data['pictures']['sizes'];
+
+		return array_map( function ( $picture_data ) {
+			return $picture_data['link'];
+		},
+			$pictures );
+
+	}
+
+
 }
