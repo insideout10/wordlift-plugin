ca52542fb9f0f165e6438195cb4a4131a19a590c
naveen <kmnaveen101@gmail.com>
2021-04-14 14:17:19 +0530

    see #1344: build ui for settings screen

--- src/admin/partials/wordlift-admin-settings-videoobject-settings-page.php
+++ src/admin/partials/wordlift-admin-settings-videoobject-settings-page.php
@@ -1,21 +1,49 @@
 <?php
 ?>
-    <h1><?php _e( 'API Settings', 'wordlift' ); ?></h1>
-    <p><?php _e( 'To let WordLift access metadata from Youtube or Vimeo you will need to add here your API Key.' ); ?></p>
-    <table>
-        <tr>
-            <td>
-                Youtube API Key
-            </td>
-            <td>
-	            <?php
-	            $element = new Wordlift_Admin_Input_Element();
-	            $element->render( array(
-		            'id'          => 'wordlift_videoobject_youtube_api_key',
-		            'name'        => 'wordlift_videoobject_youtube_api_key',
-                    'value' => get_option( )
-	            ) );
-	            ?>
-            </td>
-        </tr>
-    </table>
+<h1><?php use Wordlift\Videoobject\Provider\Client\Vimeo_Client;
+	use Wordlift\Videoobject\Provider\Client\Youtube_Client;
+
+	_e( 'API Settings', 'wordlift' ); ?></h1>
+<p><?php _e( 'To let WordLift access metadata from Youtube or Vimeo you will need to add here your API Key.' ); ?></p>
+<table>
+    <tr>
+        <td>
+            Youtube API Key
+        </td>
+        <td>
+			<?php
+			$element = new Wordlift_Admin_Input_Element();
+			$element->render( array(
+				'id'    => 'wordlift_videoobject_youtube_api_key',
+				'name'  => 'wordlift_videoobject_youtube_api_key',
+				'value' => Youtube_Client::get_api_key()
+			) );
+			?>
+        </td>
+        <td>
+			<?php _e( 'here', 'wordlift' );
+			_e( ' is how to get it', 'wordlift' ); ?>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            Vimeo API Key
+        </td>
+        <td>
+			<?php
+			$element = new Wordlift_Admin_Input_Element();
+			$element->render( array(
+				'id'    => 'wordlift_videoobject_vimeo_api_key',
+				'name'  => 'wordlift_videoobject_vimeo_api_key',
+				'value' => Vimeo_Client::get_api_key()
+			) );
+			?>
+        </td>
+        <td>
+			<?php _e( 'here', 'wordlift' );
+			_e( ' is how to get it', 'wordlift' ); ?>
+        </td>
+    </tr>
+
+</table>
