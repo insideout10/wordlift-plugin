7a5ced7fdb098ec1a4a3ec5bbeb380b4b6da7a60
naveen <kmnaveen101@gmail.com>
2021-04-15 15:33:49 +0530

    see #1344: create react component structure

---
+++ src-js/webpack/src/videoobject/actions/index.js
@@ -0,0 +1,4 @@
+import {createAction} from "redux-actions";
+
+export const getAllVideos = createAction("GET_ALL_VIDEOS_FROM_NETWORK");
+
---
+++ src-js/webpack/src/videoobject/api/index.js
@@ -0,0 +1,17 @@
+export  function getVideosFromApi(offset, limit, apiConfig) {
+
+    const {restUrl, nonce, postId} = apiConfig;
+
+    return fetch(restUrl, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "X-WP-Nonce": nonce
+        },
+        body: JSON.stringify({
+            post_id: postId,
+        })
+    })
+        .then(response => response.json())
+        .then(json => json);
+}
