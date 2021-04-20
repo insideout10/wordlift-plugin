859bc7450ded6fc10ff67160b902b41d57db310e
naveen <kmnaveen101@gmail.com>
2021-04-20 14:34:10 +0530

    see #1344: fix: tests on 4.4

--- tests/test-videoobject-parser-factory.php
+++ tests/test-videoobject-parser-factory.php
@@ -10,7 +10,10 @@ use Wordlift\Videoobject\Parser\Parser_Factory;
  */
 class Videoobject_Parser_Factory_Test extends \Wordlift_Unit_Test_Case {
 
-	public function test_should_provide_classic_editor_parser_if_no_blocks_in_post_content() {
+	public function test_should_provide_block_editor_parser_if_no_blocks_in_post_content() {
+		if ( ! function_exists('parse_blocks') ) {
+			$this->markTestSkipped('Skipped because WP < 5.0 doesnt have parse_blocks function');
+		}
 		$post_content = <<<EOF
 <!-- wp:embed {"url":"https://vimeo.com/162427937","type":"video","providerNameSlug":"vimeo","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
 <figure class="wp-block-embed is-type-video is-provider-vimeo wp-block-embed-vimeo wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
@@ -23,7 +26,7 @@ EOF;
 		$this->assertTrue( $parser instanceof Block_Editor_Parser );
 	}
 
-	public function test_should_provide_block_editor_parser_if_blocks_in_post_content() {
+	public function test_should_provide_classic_editor_parser_if_no_blocks_in_post_content() {
 		$post_content = <<<EOF
 https://www.youtube.com/watch?v=fJAPDAK4GiI
 EOF;
