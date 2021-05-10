5e663b0a2b90d7a03487a6bca8e88c97c6209ded
naveen <kmnaveen101@gmail.com>
2021-05-10 16:18:15 +0530

    see #1352: fix get_edit_term_link

--- src/wordlift/vocabulary/data/term-data/class-default-term-data.php
+++ src/wordlift/vocabulary/data/term-data/class-default-term-data.php
@@ -29,7 +29,8 @@ class Default_Term_Data implements Term_Data {
 			'tagId'          => $this->term->term_id,
 			'tagName'        => $this->term->name,
 			'tagDescription' => $this->term->description,
-			'tagLink'        => get_edit_term_link( $this->term->term_id ),
+			// Before 4.5.0 taxonomy parameter **must** be passed to this function.
+			'tagLink'        => get_edit_term_link( $this->term->term_id, $this->term->taxonomy ),
 			'tagPostCount'   => $this->term->count,
 			'tagTaxonomy'    => get_taxonomy( $this->term->taxonomy )->label,
 			'entities'       => $this->entities,
