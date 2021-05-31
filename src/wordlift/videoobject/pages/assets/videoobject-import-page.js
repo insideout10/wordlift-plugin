6eaae30b584700cfe2f1d727ae3f9bad77892f48
naveen <kmnaveen101@gmail.com>
2021-05-31 16:39:02 +0530

    see #1382: switching to fetch api

--- src/wordlift/videoobject/pages/assets/videoobject-import-page.js
+++ src/wordlift/videoobject/pages/assets/videoobject-import-page.js
@@ -1,47 +1,48 @@
 window.addEventListener("load", function () {
-  const startBtn = document.getElementById("wl-start-btn");
-  const stopBtn = document.getElementById("wl-stop-btn");
+    const startBtn = document.getElementById("wl-start-btn");
+    const stopBtn = document.getElementById("wl-stop-btn");
 
-  const updateProgressBar = function () {
-    wp.apiRequest({ path: "wordlift/v1/videos/background/get_state", method: "POST" }).success(
-      (data) => {
-        let percentage = (data.index * 100.0) / data.count;
-        if ( percentage > 100 ) {
-          percentage = 100
-        }
-        document.querySelector(".wl-task__progress__bar").style.width =
-           percentage + "%";
+    const updateProgressBar = function () {
+        fetch("wordlift/v1/videos/background/get_state", {method: "POST"})
+            .then(response => response.json())
+            .then((data) => {
+                    let percentage = (data.index * 100.0) / data.count;
+                    if (percentage > 100) {
+                        percentage = 100
+                    }
+                    document.querySelector(".wl-task__progress__bar").style.width =
+                        percentage + "%";
 
-        if ("started" === data.state) {
-          if (!startBtn.classList.contains("hidden"))
-            startBtn.classList.add("hidden");
-          if (stopBtn.classList.contains("hidden"))
-            stopBtn.classList.remove("hidden");
-        } else {
-          if (startBtn.classList.contains("hidden"))
-            startBtn.classList.remove("hidden");
-          if (!stopBtn.classList.contains("hidden"))
-            stopBtn.classList.add("hidden");
-        }
+                    if ("started" === data.state) {
+                        if (!startBtn.classList.contains("hidden"))
+                            startBtn.classList.add("hidden");
+                        if (stopBtn.classList.contains("hidden"))
+                            stopBtn.classList.remove("hidden");
+                    } else {
+                        if (startBtn.classList.contains("hidden"))
+                            startBtn.classList.remove("hidden");
+                        if (!stopBtn.classList.contains("hidden"))
+                            stopBtn.classList.add("hidden");
+                    }
 
-        setTimeout(updateProgressBar, 1000);
-      }
-    );
-  };
+                    setTimeout(updateProgressBar, 1000);
+                }
+            );
+    };
 
-  setTimeout(updateProgressBar, 1000);
+    setTimeout(updateProgressBar, 1000);
 
-  startBtn.addEventListener("click", function () {
-    wp.apiRequest({
-      method: "POST",
-      path: "wordlift/v1/videos/background/start",
+    startBtn.addEventListener("click", function () {
+        fetch("wordlift/v1/videos/background/start", {
+            method: "POST"
+        }).then(() => {
+        });
     });
-  });
 
-  stopBtn.addEventListener("click", function () {
-    wp.apiRequest({
-      method: "POST",
-      path: "wordlift/v1/videos/background/stop",
+    stopBtn.addEventListener("click", function () {
+        fetch("wordlift/v1/videos/background/stop", {
+            method: "POST"
+        }).then(() => {
+        });
     });
-  });
 });
