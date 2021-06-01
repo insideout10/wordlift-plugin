3988b4e9a5d48a40ca7087cc28ce76a82ddd47cc
Akshay Raje <akshay.raje@gmail.com>
2021-06-01 17:17:30 +0530

    bump to 3.31.4

---
+++ tags/3.31.4/wordlift_manual_translations.php
@@ -0,0 +1,13 @@
+<?php
+
+// File created to manage some dynamic translation used in the code
+// See http://keithdevon.com/using-variables-wordpress-translation-functions/
+
+__( 'There are no related posts for the current entity.', 'wordlift' );
+__( 'This entity has not description.', 'wordlift' );
+__( 'There are no related entities for the current entity.', 'wordlift' );
+__( 'This entity is not published. It will not appear within analysis results.', 'wordlift' );
+__( 'This entity has no featured image yet.', 'wordlift' );
+__( 'There are no sameAs configured for this entity.', 'wordlift' );
+__( 'Schema.org metadata for this entity are not completed.', 'wordlift' );
+
