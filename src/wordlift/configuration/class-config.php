93836e2a2e2c6883dac66553338ed37eb6903140
naveen <kmnaveen101@gmail.com>
2021-06-04 11:08:50 +0530

    see #1393: update configuration

--- src/wordlift/configuration/class-config.php
+++ src/wordlift/configuration/class-config.php
@@ -27,10 +27,15 @@ class Config {
 
 	}
 
-
-	public function config() {
-
-		$account_info = $this->key_validation_service->get_account_info( (string) $_POST['license'] );
+	/**
+	 * Check if the key is valid and also not bound to any domain.
+	 *
+	 * @param $key string
+	 *
+	 * @return bool
+	 */
+	private function is_key_valid_and_not_bound_to_any_domain( $key ) {
+		$account_info = $this->key_validation_service->get_account_info( $key );
 
 		/**
 		 * we need to check if the key is not associated with any account
@@ -38,7 +43,7 @@ class Config {
 		 */
 		if ( is_wp_error( $account_info )
 		     || wp_remote_retrieve_response_code( $account_info ) !== 200 ) {
-			return;
+			return false;
 		}
 
 		$account_info_json = $account_info['body'];
@@ -47,31 +52,66 @@ class Config {
 
 		if ( ! $account_info_data ) {
 			// Invalid json returned by api.
-			return;
+			return false;
 		}
 
 		if ( $account_info_data['url'] !== null ) {
 			// key already associated with another account.
+			return false;
+		}
+
+		return true;
+	}
+
+
+	public function config() {
+
+		// Perform validation check for all the parameters.
+		$required_fields = array(
+			'diagnostic',
+			'vocabulary',
+			'language',
+			'country',
+			'publisherName',
+			'publisher',
+			'license'
+		);
+
+		// validate all the fields before processing
+		foreach ( $required_fields as $field ) {
+			if ( ! array_key_exists( $field, $_POST ) ) {
+				wp_send_json_error( sprintf( __( 'Field %s is required', 'wordlift' ), $field ) );
+				return;
+			}
+		}
+
+		$key = (string) $_POST['license'];
+
+		if ( ! $this->is_key_valid_and_not_bound_to_any_domain( $key ) ) {
+			wp_send_json_error( __( 'Key is not valid or associated with other domain', 'wordlift' ) );
+
+			// exit if not valid.
 			return;
 		}
 
-		$image_string = (string) $_POST['image'];
+		$params = $this->get_params( $key );
 
-		$image_decoded_string = base64_decode( $image_string );
+		$this->admin_setup->save_configuration( $params );
 
-		$upload_dir = wp_upload_dir();
 
-		$image_ext = (string) $_POST['imageExtension'];
+	}
 
-		$file_path = $upload_dir['path'] . DIRECTORY_SEPARATOR . md5( $image_string ) . "." . $image_ext;
+	/**
+	 * @param $key
+	 *
+	 * @return array
+	 */
+	private function get_params( $key ) {
 
-		file_put_contents( $file_path, $image_decoded_string );
-
-		$attachment_id = wp_insert_attachment( array(), $file_path );
+		$attachment_id = $this->may_be_get_attachment_id();
 
 		$params = array(
-			'share-diagnostic' => $_POST['diagnostic'],
-			'key'              => $_POST['license'],
+			'key'              => $key,
 			'vocabulary'       => $_POST['vocabulary'],
 			'wl-site-language' => $_POST['language'],
 			'wl-country-code'  => $_POST['country'],
@@ -80,9 +120,39 @@ class Config {
 			'logo'             => $attachment_id
 		);
 
-		$this->admin_setup->save_configuration( $params );
+		if ( (bool) $_POST['diagnostic'] ) {
+			$params['share-diagnostic'] = 'yes';
+		}
 
+		return $params;
+	}
 
+	/**
+	 * @return int | bool
+	 */
+	private function may_be_get_attachment_id() {
+		// if image or image extension not posted then return false.
+		if ( ! isset( $_POST['image'] ) || ! isset( $_POST['imageExtension'] ) ) {
+			return false;
+		}
+
+		$allowed_extensions = array( 'png', 'jpeg', 'jpg' );
+		$image_string       = (string) $_POST['image'];
+		$image_ext          = (string) $_POST['imageExtension'];
+
+		if ( ! in_array( $image_ext, $allowed_extensions ) ) {
+			return false;
+		}
+
+		$image_decoded_string = base64_decode( $image_string );
+
+		$upload_dir = wp_upload_dir();
+
+		$file_path = $upload_dir['path'] . DIRECTORY_SEPARATOR . md5( $image_string ) . "." . $image_ext;
+
+		file_put_contents( $file_path, $image_decoded_string );
+
+		return wp_insert_attachment( array(), $file_path );
 	}
 
 }
