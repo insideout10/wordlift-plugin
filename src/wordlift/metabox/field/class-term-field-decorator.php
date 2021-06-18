e3fe97758e0a59116070a260c6e242bb7a31a30c
naveen <kmnaveen101@gmail.com>
2021-06-18 10:52:28 +0530

    see #1406: term field decorator complete

--- src/wordlift/metabox/field/class-term-field-decorator.php
+++ src/wordlift/metabox/field/class-term-field-decorator.php
@@ -8,12 +8,43 @@ namespace Wordlift\Metabox\Field;
 
 class Term_Field_Decorator implements Field {
 
+	private $term_id;
+
+	private $field;
+
+	/**
+	 * Post_Field_Decorator constructor.
+	 *
+	 * @param $term_id
+	 * @param $field Wl_Metabox_Field
+	 */
+	public function __construct( $term_id, $field ) {
+		$this->term_id = $term_id;
+		$this->field   = $field;
+	}
+
 	public function get_data() {
-		// TODO: Implement get_data() method.
+		$meta_key = $this->field->meta_name;
+		return get_post_meta( $this->term_id, $meta_key );
 	}
 
 	public function save_data( $values ) {
-		// TODO: Implement save_data() method.
+
+		$values    = $this->field->sanitize_data( $values );
+		$entity_id = intval( $_POST['post_ID'] );
+		$meta_key = $this->field->meta_name;
+		// Take away old values.
+		delete_term_meta( $entity_id, $meta_key );
+
+		// insert new values, respecting cardinality.
+		$single = ( 1 === $this->field->cardinality );
+		foreach ( $values as $value ) {
+			$this->field->log->trace( "Saving $value to $meta_key for term $entity_id..." );
+			// To avoid duplicate values
+			delete_term_meta( $entity_id, $meta_key, $value );
+			$meta_id = add_term_meta( $entity_id, $meta_key, $value, $single );
+			$this->field->log->debug( "$value to $meta_key for term $entity_id saved with id $meta_id." );
+		}
 	}
 
 }
