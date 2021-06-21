030c4c8e1474fe17588007bf8e3e845ebdd06952
naveen <kmnaveen101@gmail.com>
2021-06-21 17:01:39 +0530

    see #1406: fix tests

--- tests/test-entity-url-property-service.php
+++ tests/test-entity-url-property-service.php
@@ -27,7 +27,7 @@ class Wordlift_Url_Property_Service_Test extends Wordlift_Unit_Test_Case {
 		$property = new Wordlift_Url_Property_Service();
 
 		$post_id = $this->factory()->post->create();
-		$values  = $property->get( $post_id, 'abc123' );
+		$values  = $property->get( $post_id, 'abc123', Wordlift_Property_Getter::POST );
 
 		$this->assertTrue( is_array( $values ), '`$values` must be an array, instead got ' . var_export( $values, true ) );
 		$this->assertEquals( array( get_permalink( $post_id ) ), $values, '`$values` only contain the post permalink.' );
@@ -47,7 +47,7 @@ class Wordlift_Url_Property_Service_Test extends Wordlift_Unit_Test_Case {
 
 		$post_id = $this->factory()->post->create();
 
-		$values = $property->get( $post_id, 'abc123' );
+		$values = $property->get( $post_id, 'abc123', Wordlift_Property_Getter::POST );
 
 		$this->assertTrue( is_array( $values ), '`$values` must be an array.' );
 		$this->assertEquals( array( 'http://example.org/production_permalink' ), $values, '`$values` only contain the post permalink.' );
@@ -59,11 +59,11 @@ class Wordlift_Url_Property_Service_Test extends Wordlift_Unit_Test_Case {
 	/**
 	 * Filter to the `wl_production_permalink` hook.
 	 *
-	 * @since 3.20.0
-	 *
 	 * @param string $permalink The default permalink.
 	 *
 	 * @return string The new permalink.
+	 * @since 3.20.0
+	 *
 	 */
 	public function _production_permalink( $permalink ) {
 
