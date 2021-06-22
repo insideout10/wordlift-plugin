92cbd21ee407971e7d2d04e9b84ab07b4ece3139
naveen <kmnaveen101@gmail.com>
2021-06-22 13:10:36 +0530

    see #1406: fix id

--- tests/test-vocabulary-terms-jsonld.php
+++ tests/test-vocabulary-terms-jsonld.php
@@ -9,8 +9,8 @@ class Vocabulary_Terms_Jsonld extends \Wordlift_Unit_Test_Case {
 
 
 	public function test_when_term_saved_should_generate_entity_uri() {
-		$term_id    = wp_insert_term( 'vocabulary_term_test', 'category' );
-		$term_id = $term_id['term_id'];
+		$term_id    = wp_insert_term( 'vocabulary_term_test_1', 'category' );
+		$term_id    = $term_id['term_id'];
 		$entity_uri = get_term_meta( $term_id, 'entity_url', true );
 		$this->assertNotEmpty( $entity_uri, 'Entity uri should be set upon term save' );
 	}
@@ -18,11 +18,13 @@ class Vocabulary_Terms_Jsonld extends \Wordlift_Unit_Test_Case {
 
 	public function test_should_generate_id_for_term_correctly() {
 
-		$term   = $this->factory()->term->create();
-		$jsonld = Wordlift_Term_JsonLd_Adapter::get_instance()->get( $term );
+		$term_id    = wp_insert_term( 'vocabulary_term_test_2', 'category' );
+		$term_id    = $term_id['term_id'];
+		$jsonld = Wordlift_Term_JsonLd_Adapter::get_instance()->get( $term_id );
 		// Since there are no types set, we should default it to Thing,
 		$this->assertSame( $jsonld[0]['@type'], array( 'Thing' ) );
-		//$this->assertArrayHasKey( '@id', $jsonld[0] );
+		$this->assertArrayHasKey( '@id', $jsonld[0] );
+		$this->assertSame( wl_get_term_entity_uri( $term_id ), $jsonld[0]['@id'] );
 	}
 
 
