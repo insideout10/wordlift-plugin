6bcd30cb6e63380b1a1c4bd16d83533576d4cc34
naveen <kmnaveen101@gmail.com>
2021-06-23 10:32:39 +0530

    see #1406: fix sameas search box

--- src/wordlift/vocabulary-terms/class-term-metabox.php
+++ src/wordlift/vocabulary-terms/class-term-metabox.php
@@ -23,9 +23,25 @@ class Term_Metabox extends Wl_Abstract_Metabox {
 	 * @param $term \WP_Term
 	 */
 	public function render_ui( $term ) {
+		var_dump("render ui called");
 		$this->instantiate_fields( $term->term_id, Wl_Abstract_Metabox::TERM );
 		$this->html();
 		$this->enqueue_scripts_and_styles();
+		$plugin = \Wordlift::get_instance();
+		wp_enqueue_script(
+			'wl-autocomplete-select',
+			plugin_dir_url( dirname( dirname( __FILE__ ) ) ) . 'js/dist/autocomplete-select.js',
+			array(),
+			$plugin->get_version(),
+			true
+		);
+
+		wp_enqueue_style(
+			'wl-autocomplete-select',
+			plugin_dir_url( dirname( dirname( __FILE__ ) ) ) . 'js/dist/autocomplete-select.css',
+			array(),
+			$plugin->get_version()
+		);
 	}
 
 	public function save_field( $term_id ) {
