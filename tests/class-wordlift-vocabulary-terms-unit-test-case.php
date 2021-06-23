8276bf153334342312b41d02e8920e805bd9e55b
naveen <kmnaveen101@gmail.com>
2021-06-23 10:50:20 +0530

    see #1406: add tests

--- tests/class-wordlift-vocabulary-terms-unit-test-case.php
+++ tests/class-wordlift-vocabulary-terms-unit-test-case.php
@@ -1,5 +1,6 @@
 <?php
 
+use Wordlift\Features\Features_Registry;
 use Wordlift\Vocabulary\Analysis_Background_Service;
 use Wordlift\Vocabulary\Vocabulary_Loader;
 use Wordlift\Vocabulary_Terms\Vocabulary_Terms_Loader;
@@ -22,6 +23,8 @@ abstract class Wordlift_Vocabulary_Terms_Unit_Test_Case extends Wordlift_Unit_Te
 		add_filter( 'wl_feature__enable__no-vocabulary-terms', '__return_true' );
 		// vocabulary terms feature should now be enabled.
 		run_wordlift();
+		$features_registry = Features_Registry::get_instance();
+		$features_registry->initialize_all_features();
 
 	}
 }
--- tests/test-vocabulary-terms-jsonld.php
+++ tests/test-vocabulary-terms-jsonld.php
@@ -2,14 +2,14 @@
 
 /**
  * @since 3.31.7
- * @group vocabulary_terms
+ * @group no_vocabulary_terms
  * @author Naveen Muthusamy <naveen@wordlift.io>
  */
-class Vocabulary_Terms_Jsonld extends \Wordlift_Unit_Test_Case {
+class Vocabulary_Terms_Jsonld extends \Wordlift_Vocabulary_Terms_Unit_Test_Case {
 
 
 	public function test_when_term_saved_should_generate_entity_uri() {
-		$term_id    = wp_insert_term( 'vocabulary_term_test_1', 'category' );
+		$term_id    = wp_insert_term( 'vocabulary_term_test_1', 'no_vocabulary_terms' );
 		$term_id    = $term_id['term_id'];
 		$entity_uri = get_term_meta( $term_id, 'entity_url', true );
 		$this->assertNotEmpty( $entity_uri, 'Entity uri should be set upon term save' );
@@ -18,7 +18,7 @@ class Vocabulary_Terms_Jsonld extends \Wordlift_Unit_Test_Case {
 
 	public function test_should_generate_id_for_term_correctly() {
 
-		$term_id    = wp_insert_term( 'vocabulary_term_test_2', 'category' );
+		$term_id    = wp_insert_term( 'vocabulary_term_test_2', 'no_vocabulary_terms' );
 		$term_id    = $term_id['term_id'];
 		$jsonld = Wordlift_Term_JsonLd_Adapter::get_instance()->get( $term_id );
 		// Since there are no types set, we should default it to Thing,
