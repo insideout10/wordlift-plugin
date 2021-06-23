8cd5efe282d1e97d9fe0e5bc4eece011e6a319a4
naveen <kmnaveen101@gmail.com>
2021-06-23 14:49:21 +0530

    see #1406: add types of references

---
+++ src/wordlift/jsonld/class-reference.php
@@ -0,0 +1,24 @@
+<?php
+namespace Wordlift\Jsonld;
+/**
+ * This interface represents a reference
+ *
+ * @since 3.31.7
+ * @package Wordlift\Jsonld
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ */
+
+interface Reference {
+
+	/**
+	 * @return int
+	 */
+	public function get_type();
+
+	/**
+	 * @return int Identifier
+	 */
+	public function get_id();
+
+}
+
---
+++ src/wordlift/jsonld/class-term-reference.php
@@ -0,0 +1,18 @@
+<?php
+namespace Wordlift\Jsonld;
+use Wordlift\Object_Type_Enum;
+
+/**
+ * This class represents an Term reference
+ *
+ * @since 3.31.7
+ * @package Wordlift\Jsonld
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ */
+
+class Term_Reference extends Abstract_Reference {
+
+	function get_type() {
+		return Object_Type_Enum::POST;
+	}
+}
