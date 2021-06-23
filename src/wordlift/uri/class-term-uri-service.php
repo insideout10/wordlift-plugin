2e6a16dbce29fa81b9e74d50073a244522587541
naveen <kmnaveen101@gmail.com>
2021-06-23 17:39:15 +0530

    see #1406: fix the term uri service

---
+++ src/wordlift/uri/class-term-uri-service.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @since 3.31.7
+ * @author Naveen Muthusamy <naveen@wordlift.io>
+ * This class returns the term by URI.
+ */
+
+namespace Wordlift\Uri;
+
+use Wordlift\Vocabulary\Terms_Compat;
+
+class Term_Uri {
+
+	/**
+	 * @param $uri string
+	 *
+	 * @return \WP_Term | bool
+	 */
+	public static function get_term( $uri ) {
+
+		$selected_terms = Terms_Compat::get_terms( '', array(
+			'fields'     => 'all',
+			'get'        => 'all',
+			'number'     => 1,
+			'meta_query' => array(
+				array(
+					'key'   => 'entity_url',
+					'value' => $uri,
+				),
+			),
+			'orderby'    => 'term_id',
+			'order'      => 'ASC',
+		) );
+
+		if ( count( $selected_terms ) === 0 ) {
+			return false;
+		}
+
+		return $selected_terms[0];
+	}
+
+}
