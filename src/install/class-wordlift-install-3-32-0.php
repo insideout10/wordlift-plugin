c9b621f9629f3b3987d29eecec7570086bef5ced
naveen <kmnaveen101@gmail.com>
2021-07-06 11:12:34 +0530

    see #1406: do check for column exists

--- src/install/class-wordlift-install-3-32-0.php
+++ src/install/class-wordlift-install-3-32-0.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @since 3.28.0
  * @author Naveen Muthusamy <naveen@wordlift.io>
@@ -10,6 +11,13 @@ class Wordlift_Install_3_32_0 extends Wordlift_Install {
 	 */
 	protected static $version = '3.32.0';
 
+	public static function is_column_exists( $column_name ) {
+		global $wpdb;
+		$table_name = WL_DB_RELATION_INSTANCES_TABLE_NAME;
+
+		return $wpdb->get_results( "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name ='$table_name' AND column_name = '$column_name'" );
+
+	}
 
 	public function install() {
 		global $wpdb;
@@ -23,14 +31,17 @@ class Wordlift_Install_3_32_0 extends Wordlift_Install {
 		// we add 0 as default since we want to add compat between old and new values.
 		$query_template = $this->get_query_template();
 
-		// Add object_type column
-		$object_type_column_query = sprintf( $query_template, $wpdb->prefix . WL_DB_RELATION_INSTANCES_TABLE_NAME, 'object_type' );
-		$wpdb->query( $object_type_column_query );
+		if (  ! self::is_column_exists('object_type') ) {
+			// Add object_type column
+			$object_type_column_query = sprintf( $query_template, $wpdb->prefix . WL_DB_RELATION_INSTANCES_TABLE_NAME, 'object_type' );
+			$wpdb->query( $object_type_column_query );
+		}
 
-		// Add subject_type column.
-		$subject_type_column_query = sprintf( $query_template, $wpdb->prefix . WL_DB_RELATION_INSTANCES_TABLE_NAME, 'subject_type' );
-		$wpdb->query( $subject_type_column_query );
-
+		if (  ! self::is_column_exists('subject_type') ) {
+			// Add subject_type column.
+			$subject_type_column_query = sprintf( $query_template, $wpdb->prefix . WL_DB_RELATION_INSTANCES_TABLE_NAME, 'subject_type' );
+			$wpdb->query( $subject_type_column_query );
+		}
 	}
 
 	/**
