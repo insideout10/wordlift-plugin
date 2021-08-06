d2bdf0f0763fdcdd13253270f8c1d0664b37bd55
naveen <kmnaveen101@gmail.com>
2021-08-06 13:35:30 +0530

    Fix output issues

--- src/admin/partials/wordlift-admin-settings-videoobject-settings-page.php
+++ src/admin/partials/wordlift-admin-settings-videoobject-settings-page.php
@@ -5,7 +5,7 @@ use Wordlift\Videoobject\Provider\Client\Youtube_Client;
 
 
 if ( isset( $_POST['wordlift_videoobject_youtube_api_key'] )
-     || isset( $_POST['wordlift_videoobject_vimeo_api_key'] ) ) {
+	 || isset( $_POST['wordlift_videoobject_vimeo_api_key'] ) ) {
 
 	/**
 	 * @todo: does this fields need to be encrypted before saving ?
@@ -24,12 +24,12 @@ if ( isset( $_POST['wordlift_videoobject_youtube_api_key'] )
 
 if ( isset( $_POST['submit'] ) ) {
 	if ( isset( $_POST['wl_enable_video_sitemap'] ) ) {
-		update_option( "_wl_video_sitemap_generation", 1 );
-		// flush the rewrite rules
+		update_option( '_wl_video_sitemap_generation', 1 );
+		// flush the rewrite rules.
 		flush_rewrite_rules();
 	} else {
-		update_option( "_wl_video_sitemap_generation", 0 );
-		// flush the rewrite rules
+		update_option( '_wl_video_sitemap_generation', 0 );
+		// flush the rewrite rules.
 		flush_rewrite_rules();
 	}
 }
@@ -38,63 +38,69 @@ if ( isset( $_POST['submit'] ) ) {
 <h1><?php _e( 'API Settings', 'wordlift' ); ?></h1>
 <p><?php _e( 'To let WordLift access metadata from YouTube or Vimeo you will need to add here your API Key.', 'wordlift' ); ?></p>
 <form method="post">
-    <table>
-        <tr>
-            <td>
+	<table>
+		<tr>
+			<td>
 				<?php _e( 'YouTube API Key', 'wordlift' ); ?>
-            </td>
-            <td>
+			</td>
+			<td>
 				<?php
 				$element = new Wordlift_Admin_Input_Element();
-				$element->render( array(
-					'id'    => 'wordlift_videoobject_youtube_api_key',
-					'name'  => 'wordlift_videoobject_youtube_api_key',
-					'value' => Youtube_Client::get_api_key()
-				) );
+				$element->render(
+					array(
+						'id'    => 'wordlift_videoobject_youtube_api_key',
+						'name'  => 'wordlift_videoobject_youtube_api_key',
+						'value' => Youtube_Client::get_api_key(),
+					)
+				);
 				?>
-            </td>
-            <td>
-                <a href="https://developers.google.com/youtube/registering_an_application"><?php _e( 'Click here', 'wordlift' ); ?></a>
+			</td>
+			<td>
+				<a href="https://developers.google.com/youtube/registering_an_application"><?php _e( 'Click here', 'wordlift' ); ?></a>
 				<?php _e( ' for instructions on getting your YouTube API Key', 'wordlift' ); ?>
-            </td>
-        </tr>
+			</td>
+		</tr>
 
-        <tr>
-            <td>
+		<tr>
+			<td>
 				<?php _e( 'Vimeo API Key', 'wordlift' ); ?>
-            </td>
-            <td>
+			</td>
+			<td>
 				<?php
 				$element = new Wordlift_Admin_Input_Element();
-				$element->render( array(
-					'id'    => 'wordlift_videoobject_vimeo_api_key',
-					'name'  => 'wordlift_videoobject_vimeo_api_key',
-					'value' => Vimeo_Client::get_api_key()
-				) );
+				$element->render(
+					array(
+						'id'    => 'wordlift_videoobject_vimeo_api_key',
+						'name'  => 'wordlift_videoobject_vimeo_api_key',
+						'value' => Vimeo_Client::get_api_key(),
+					)
+				);
 				?>
-            </td>
-            <td>
-                <a href="https://developer.vimeo.com/api/guides/start"><?php _e( 'Click here', 'wordlift' ); ?></a>
+			</td>
+			<td>
+				<a href="https://developer.vimeo.com/api/guides/start"><?php _e( 'Click here', 'wordlift' ); ?></a>
 				<?php _e( ' for instructions on getting your Vimeo API Key', 'wordlift' ); ?>
-            </td>
-        </tr>
+			</td>
+		</tr>
 
-    </table>
-    <h1><?php _e( 'Video Sitemap', 'wordlift' ); ?></h1>
-    <p>
-		<?php _e( 'The Video Sitemap works like any other XML Sitemap. Search engines will use it to display rich snippets in result pages.', 'wordlift'  ); ?>
-    </p>
-	<?php $is_sitemap_enabled = get_option( '_wl_video_sitemap_generation', false ) ? 'checked' : '' ?>
-    <p> <?php _e( 'Enable Video Sitemap' ); ?>
-        <input type="checkbox" name="wl_enable_video_sitemap" value="1" <?php echo $is_sitemap_enabled; ?> ></p>
-    <p> <?php
-		if ( $is_sitemap_enabled ) {
-			$sitemap_link = get_home_url( null, 'wl-video-sitemap.xml' );
-			printf( __( 'Here is <a href="%s">link</a> to your Video Sitemap. Add it now, to Google Search Console.', 'wordlift' ), $sitemap_link );
-		}
-		?></p>
+	</table>
+	<h1><?php _e( 'Video Sitemap', 'wordlift' ); ?></h1>
+	<p>
+		<?php _e( 'The Video Sitemap works like any other XML Sitemap. Search engines will use it to display rich snippets in result pages.', 'wordlift' ); ?>
+	</p>
+	<?php $is_sitemap_enabled = get_option( '_wl_video_sitemap_generation', false ) ? 'checked' : ''; ?>
+	<p> <?php _e( 'Enable Video Sitemap' ); ?>
+		<input type="checkbox" name="wl_enable_video_sitemap" value="1" <?php echo $is_sitemap_enabled; ?> ></p>
+	<p> 
+	<?php
+	if ( $is_sitemap_enabled ) {
+		$sitemap_link = get_home_url( null, 'wl-video-sitemap.xml' );
+		printf( __( 'Here is <a href="%s">link</a> to your Video Sitemap. Add it now, to Google Search Console.', 'wordlift' ), $sitemap_link );
+	}
+	?>
+		</p>
 
-    <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary"
-                             value="Save Changes">
-    </p>
+	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary"
+							 value="Save Changes">
+	</p>
 </form>
