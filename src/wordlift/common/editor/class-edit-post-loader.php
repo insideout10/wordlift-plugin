41c359c8a3d1b8c0f4c1909f2b2af3c89c02154a
naveen <kmnaveen101@gmail.com>
2021-08-23 17:33:39 +0530

    Fix script loading on the page

--- src/wordlift/common/editor/class-edit-post-loader.php
+++ src/wordlift/common/editor/class-edit-post-loader.php
@@ -9,8 +9,16 @@ namespace Wordlift\Common\Editor;
 abstract  class Edit_Post_Loader {
 
 	public function init() {
-		add_action( 'load-post.php', array( $this, 'run_on_edit_post_screen') );
-		add_action( 'load-post-new.php', array( $this, 'run_on_edit_post_screen') );
+
+		add_action('admin_enqueue_scripts', array( $this, 'post_edit_screen'));
+	}
+
+	public function post_edit_screen( $hook ) {
+
+		if (  'post.php' === $hook || 'post-new.php' === $hook ) {
+			$this->run_on_edit_post_screen();
+		}
+
 	}
 
 	abstract public function run_on_edit_post_screen();
