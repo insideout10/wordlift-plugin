7db50488a1e4e14e4087999f6a2cd8795709c0b8
naveen <kmnaveen101@gmail.com>
2021-09-06 10:42:15 +0530

    Fix dependency error for style

--- src/wordlift/no-editor-analysis/class-edit-post-scripts.php
+++ src/wordlift/no-editor-analysis/class-edit-post-scripts.php
@@ -38,7 +38,6 @@ class Edit_Post_Scripts extends Edit_Post_Loader {
 		wp_enqueue_style(
 			self::HANDLE,
 			plugin_dir_url( dirname( __DIR__ ) ) . 'js/dist/no-editor-analysis.full.css',
-			array( 'wp-components' ),
 			true
 		);
 
