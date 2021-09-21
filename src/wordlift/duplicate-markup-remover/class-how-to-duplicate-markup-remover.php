27debc5b0df48d1f7823af505bcb15d2802f0a6b
naveen <kmnaveen101@gmail.com>
2021-09-21 13:31:59 +0530

    see #1472: Fix tests

--- src/wordlift/duplicate-markup-remover/class-how-to-duplicate-markup-remover.php
+++ src/wordlift/duplicate-markup-remover/class-how-to-duplicate-markup-remover.php
@@ -6,6 +6,9 @@ class How_To_Duplicate_Markup_Remover extends Abstract_Duplicate_Markup_Remover 
 
 	public function __construct() {
 
+		// 'name', 'description' are omitted intentionally, as they belong to
+		// type http://schema.org/Thing, we remove HowTo if its the only schema type in the page
+		// if it's combined with other schema type, they would need Thing Properties.
 		$properties_to_remove = array(
 			'estimatedCost',
 			'totalTime',
--- tests/test-jsonld-duplicate-howto-markup.php
+++ tests/test-jsonld-duplicate-howto-markup.php
@@ -12,218 +12,218 @@ class Duplicate_How_To_Markup_Test extends Wordlift_Unit_Test_Case {
 
 	function setUp() {
 		parent::setUp();
-		$this->mock_how_to_data = array (
+		$this->mock_how_to_data = array(
 			'estimatedCost' =>
-				array (
-					'@type' => 'MonetaryAmount',
+				array(
+					'@type'    => 'MonetaryAmount',
 					'currency' => 'USD',
-					'value' => '100',
+					'value'    => '100',
 				),
-			'supply' =>
-				array (
+			'supply'        =>
+				array(
 					0 =>
-						array (
+						array(
 							'@type' => 'HowToSupply',
-							'name' => 'tiles',
+							'name'  => 'tiles',
 						),
 					1 =>
-						array (
+						array(
 							'@type' => 'HowToSupply',
-							'name' => 'thin-set mortar',
+							'name'  => 'thin-set mortar',
 						),
 					2 =>
-						array (
+						array(
 							'@type' => 'HowToSupply',
-							'name' => 'tile grout',
+							'name'  => 'tile grout',
 						),
 					3 =>
-						array (
+						array(
 							'@type' => 'HowToSupply',
-							'name' => 'grout sealer',
+							'name'  => 'grout sealer',
 						),
 				),
-			'tool' =>
-				array (
+			'tool'          =>
+				array(
 					0 =>
-						array (
+						array(
 							'@type' => 'HowToTool',
-							'name' => 'notched trowel',
+							'name'  => 'notched trowel',
 						),
 					1 =>
-						array (
+						array(
 							'@type' => 'HowToTool',
-							'name' => 'bucket',
+							'name'  => 'bucket',
 						),
 					2 =>
-						array (
+						array(
 							'@type' => 'HowToTool',
-							'name' => 'large sponge',
+							'name'  => 'large sponge',
 						),
 				),
-			'step' =>
-				array (
+			'step'          =>
+				array(
 					0 =>
-						array (
-							'@type' => 'HowToStep',
-							'url' => 'https://example.com/kitchen#step1',
-							'name' => 'Prepare the surfaces',
+						array(
+							'@type'           => 'HowToStep',
+							'url'             => 'https://example.com/kitchen#step1',
+							'name'            => 'Prepare the surfaces',
 							'itemListElement' =>
-								array (
+								array(
 									0 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Turn off the power to the kitchen and then remove everything that is on the wall, such as outlet covers, switchplates, and any other item in the area that is to be tiled.',
+											'text'  => 'Turn off the power to the kitchen and then remove everything that is on the wall, such as outlet covers, switchplates, and any other item in the area that is to be tiled.',
 										),
 									1 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Then clean the surface thoroughly to remove any grease or other debris and tape off the area.',
+											'text'  => 'Then clean the surface thoroughly to remove any grease or other debris and tape off the area.',
 										),
 								),
-							'image' =>
-								array (
-									'@type' => 'ImageObject',
-									'url' => 'https://example.com/photos/1x1/photo-step1.jpg',
+							'image'           =>
+								array(
+									'@type'  => 'ImageObject',
+									'url'    => 'https://example.com/photos/1x1/photo-step1.jpg',
 									'height' => '406',
-									'width' => '305',
+									'width'  => '305',
 								),
 						),
 					1 =>
-						array (
-							'@type' => 'HowToStep',
-							'name' => 'Plan your layout',
-							'url' => 'https://example.com/kitchen#step2',
+						array(
+							'@type'           => 'HowToStep',
+							'name'            => 'Plan your layout',
+							'url'             => 'https://example.com/kitchen#step2',
 							'itemListElement' =>
-								array (
+								array(
 									0 =>
-										array (
+										array(
 											'@type' => 'HowToTip',
-											'text' => 'The creases created up until this point will be guiding lines for creating the four walls of your planter box.',
+											'text'  => 'The creases created up until this point will be guiding lines for creating the four walls of your planter box.',
 										),
 									1 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Lift one side at a 90-degree angle, and fold it in place so that the point on the paper matches the other two points already in the center.',
+											'text'  => 'Lift one side at a 90-degree angle, and fold it in place so that the point on the paper matches the other two points already in the center.',
 										),
 									2 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Repeat on the other side.',
+											'text'  => 'Repeat on the other side.',
 										),
 								),
-							'image' =>
-								array (
-									'@type' => 'ImageObject',
-									'url' => 'https://example.com/photos/1x1/photo-step2.jpg',
+							'image'           =>
+								array(
+									'@type'  => 'ImageObject',
+									'url'    => 'https://example.com/photos/1x1/photo-step2.jpg',
 									'height' => '406',
-									'width' => '305',
+									'width'  => '305',
 								),
 						),
 					2 =>
-						array (
-							'@type' => 'HowToStep',
-							'name' => 'Prepare your and apply mortar (or choose adhesive tile)',
-							'url' => 'https://example.com/kitchen#step3',
+						array(
+							'@type'           => 'HowToStep',
+							'name'            => 'Prepare your and apply mortar (or choose adhesive tile)',
+							'url'             => 'https://example.com/kitchen#step3',
 							'itemListElement' =>
-								array (
+								array(
 									0 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Follow the instructions on your thin-set mortar to determine the right amount of water to fill in your bucket. Once done, add the powder gradually and make sure it is thoroughly mixed.',
+											'text'  => 'Follow the instructions on your thin-set mortar to determine the right amount of water to fill in your bucket. Once done, add the powder gradually and make sure it is thoroughly mixed.',
 										),
 									1 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Once mixed, let it stand for a few minutes before mixing it again. This time do not add more water. Double check your thin-set mortar instructions to make sure the consistency is right.',
+											'text'  => 'Once mixed, let it stand for a few minutes before mixing it again. This time do not add more water. Double check your thin-set mortar instructions to make sure the consistency is right.',
 										),
 									2 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Spread the mortar on a small section of the wall with a trowel.',
+											'text'  => 'Spread the mortar on a small section of the wall with a trowel.',
 										),
 									3 =>
-										array (
+										array(
 											'@type' => 'HowToTip',
-											'text' => 'Thinset and other adhesives set quickly so make sure to work in a small area.',
+											'text'  => 'Thinset and other adhesives set quickly so make sure to work in a small area.',
 										),
 									4 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Once it\'s applied, comb over it with a notched trowel.',
+											'text'  => 'Once it\'s applied, comb over it with a notched trowel.',
 										),
 								),
-							'image' =>
-								array (
-									'@type' => 'ImageObject',
-									'url' => 'https://example.com/photos/1x1/photo-step3.jpg',
+							'image'           =>
+								array(
+									'@type'  => 'ImageObject',
+									'url'    => 'https://example.com/photos/1x1/photo-step3.jpg',
 									'height' => '406',
-									'width' => '305',
+									'width'  => '305',
 								),
 						),
 					3 =>
-						array (
-							'@type' => 'HowToStep',
-							'name' => 'Add your tile to the wall',
-							'url' => 'https://example.com/kitchen#step4',
+						array(
+							'@type'           => 'HowToStep',
+							'name'            => 'Add your tile to the wall',
+							'url'             => 'https://example.com/kitchen#step4',
 							'itemListElement' =>
-								array (
+								array(
 									0 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Place the tile sheets along the wall, making sure to add spacers so the tiles remain lined up.',
+											'text'  => 'Place the tile sheets along the wall, making sure to add spacers so the tiles remain lined up.',
 										),
 									1 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Press the first piece of tile into the wall with a little twist, leaving a small (usually one-eight inch) gap at the countertop to account for expansion. use a rubber float to press the tile and ensure it sets in the adhesive.',
+											'text'  => 'Press the first piece of tile into the wall with a little twist, leaving a small (usually one-eight inch) gap at the countertop to account for expansion. use a rubber float to press the tile and ensure it sets in the adhesive.',
 										),
 									2 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Repeat the mortar and tiling until your wall is completely tiled, Working in small sections.',
+											'text'  => 'Repeat the mortar and tiling until your wall is completely tiled, Working in small sections.',
 										),
 								),
-							'image' =>
-								array (
-									'@type' => 'ImageObject',
-									'url' => 'https://example.com/photos/1x1/photo-step4.jpg',
+							'image'           =>
+								array(
+									'@type'  => 'ImageObject',
+									'url'    => 'https://example.com/photos/1x1/photo-step4.jpg',
 									'height' => '406',
-									'width' => '305',
+									'width'  => '305',
 								),
 						),
 					4 =>
-						array (
-							'@type' => 'HowToStep',
-							'name' => 'Apply the grout',
-							'url' => 'https://example.com/kitchen#step5',
+						array(
+							'@type'           => 'HowToStep',
+							'name'            => 'Apply the grout',
+							'url'             => 'https://example.com/kitchen#step5',
 							'itemListElement' =>
-								array (
+								array(
 									0 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Allow the thin-set mortar to set. This usually takes about 12 hours. Don\'t mix the grout before the mortar is set, because you don\'t want the grout to dry out!',
+											'text'  => 'Allow the thin-set mortar to set. This usually takes about 12 hours. Don\'t mix the grout before the mortar is set, because you don\'t want the grout to dry out!',
 										),
 									1 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'To apply, cover the area thoroughly with grout and make sure you fill all the joints by spreading it across the tiles vertically, horizontally, and diagonally. Then fill any remaining voids with grout.',
+											'text'  => 'To apply, cover the area thoroughly with grout and make sure you fill all the joints by spreading it across the tiles vertically, horizontally, and diagonally. Then fill any remaining voids with grout.',
 										),
 									2 =>
-										array (
+										array(
 											'@type' => 'HowToDirection',
-											'text' => 'Then, with a moist sponge, sponge away the excess grout and then wipe clean with a towel. For easier maintenance in the future, think about applying a grout sealer.',
+											'text'  => 'Then, with a moist sponge, sponge away the excess grout and then wipe clean with a towel. For easier maintenance in the future, think about applying a grout sealer.',
 										),
 								),
-							'image' =>
-								array (
-									'@type' => 'ImageObject',
-									'url' => 'https://example.com/photos/1x1/photo-step5.jpg',
+							'image'           =>
+								array(
+									'@type'  => 'ImageObject',
+									'url'    => 'https://example.com/photos/1x1/photo-step5.jpg',
 									'height' => '406',
-									'width' => '305',
+									'width'  => '305',
 								),
 						),
 				),
-			'totalTime' => 'P2D',
+			'totalTime'     => 'P2D',
 		);
 	}
 
@@ -235,9 +235,9 @@ class Duplicate_How_To_Markup_Test extends Wordlift_Unit_Test_Case {
 	public function test_when_the_referenced_entity_is_howto_markup_should_be_removed() {
 
 		$post_jsonld = array(
-			"@context"   => "https://schema.org",
-			'@type'      => 'HowTo',
-		) + $this->mock_how_to_data ;
+			               "@context" => "https://schema.org",
+			               '@type'    => 'HowTo',
+		               ) + $this->mock_how_to_data;
 
 		$jsonld_array = array(
 
@@ -245,7 +245,7 @@ class Duplicate_How_To_Markup_Test extends Wordlift_Unit_Test_Case {
 			$post_jsonld,
 			// entity jsonld.
 			array(
-				'@type'      => 'HowTo',
+				'@type' => 'HowTo',
 			) + $this->mock_how_to_data,
 		);
 
@@ -268,9 +268,9 @@ class Duplicate_How_To_Markup_Test extends Wordlift_Unit_Test_Case {
 
 
 		$post_jsonld = array(
-			"@context"   => "https://schema.org",
-			'@type'      => 'HowTo',
-		) + $this->mock_how_to_data;
+			               "@context" => "https://schema.org",
+			               '@type'    => 'HowTo',
+		               ) + $this->mock_how_to_data;
 
 		$jsonld_array = array(
 
@@ -278,8 +278,8 @@ class Duplicate_How_To_Markup_Test extends Wordlift_Unit_Test_Case {
 			$post_jsonld,
 			// entity jsonld.
 			array(
-				'@type'      => array( 'HowTo', 'Thing' ),
-				'foo'        => 'bar',
+				'@type' => array( 'HowTo', 'Thing' ),
+				'foo'   => 'bar',
 			) + $this->mock_how_to_data,
 		);
 
@@ -300,18 +300,18 @@ class Duplicate_How_To_Markup_Test extends Wordlift_Unit_Test_Case {
 
 	public function test_if_referenced_entities_have_howto_page_markup_should_remove_it() {
 
-		$input                 = array(
+		$input = array(
 			array(
 				"@context" => "https://schema.org",
 				'@type'    => 'Article',
 			),
 			array(
-				'@type'      => 'HowTo',
-				'foo1'       => 'bar1'
+				'@type' => 'HowTo',
+				'foo1'  => 'bar1'
 			) + $this->mock_how_to_data,
 			array(
-				'@type'      => array( 'HowTo', 'Article' ),
-				'foo2'       => 'bar2'
+				'@type' => array( 'HowTo', 'Article' ),
+				'foo2'  => 'bar2'
 			) + $this->mock_how_to_data
 		);
 
@@ -326,6 +326,40 @@ class Duplicate_How_To_Markup_Test extends Wordlift_Unit_Test_Case {
 			)
 		);
 
+		$result = apply_filters( 'wl_after_get_jsonld', $input, null, Jsonld_Context_Enum::FAQ );
+		$this->assertEquals( $expected_output, $result );
+	}
+
+	public function test_if_referenced_entities_have_name_and_description_property_then_it_should_be_retained() {
+
+		$input = array(
+			array(
+				"@context" => "https://schema.org",
+				'@type'    => 'Article',
+			),
+			array(
+				'@type'       => 'HowTo',
+				'name'        => 'name',
+				'description' => 'description'
+			) + $this->mock_how_to_data,
+			array(
+				'@type' => array( 'HowTo', 'Article' ),
+				'name'        => 'name',
+				'description' => 'description'
+			) + $this->mock_how_to_data
+		);
+
+		$expected_output = array(
+			array(
+				"@context" => "https://schema.org",
+				'@type'    => 'Article',
+			),
+			array(
+				'@type' => array( 'Article' ),
+				'name'        => 'name',
+				'description' => 'description'
+			)
+		);
 		$result = apply_filters( 'wl_after_get_jsonld', $input, null, Jsonld_Context_Enum::FAQ );
 		$this->assertEquals( $expected_output, $result );
 	}
