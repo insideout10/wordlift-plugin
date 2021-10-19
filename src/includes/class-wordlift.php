f0593c96f2107bc92502be712d12e8a8e8bb4a9d
naveen <kmnaveen101@gmail.com>
2021-10-19 16:25:10 +0530

    Bump to 3.33.3

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -775,7 +775,7 @@ class Wordlift {
 		self::$instance = $this;
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.33.2';
+		$this->version     = '3.33.3';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
