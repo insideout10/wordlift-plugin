de4ee199024c140e8c2297b6aa1f9e0705ffc2f7
Nishit Kumar <nisitkumar@gmail.com>
2021-11-07 19:57:40 +0530

    Incorporated review suggestions

--- src/wordlift/webhooks/api/class-rest-controller.php
+++ src/wordlift/webhooks/api/class-rest-controller.php
@@ -34,22 +34,10 @@ class Rest_Controller {
 
         foreach( $webhook_urls as $webhook_url ) {
 
-            $data = array();
-            if( is_array( $payloads ) ) {
-                foreach( $payloads as $payload ) {
-                    $data = array_merge( $data, explode( ",", $payload ) );
-                }
-            }
-            else {
-                $data = explode( ",", $payloads );
-            }
-
-            $encoded_data = json_encode( $data );
-
             $result = wp_remote_request( $webhook_url,
                 array(
                     'method'    => 'POST',
-                    'body'      => $encoded_data
+                    'body'      => json_encode( $payloads )
                 )
             );
 
