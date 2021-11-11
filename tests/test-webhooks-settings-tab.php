a12e8b83e3824e41c1829b1b0a52fbdad80b8288
Nishit Kumar <nisitkumar@gmail.com>
2021-11-11 15:21:12 +0530

    Minor modification of existing code for  1496

--- tests/test-webhooks-settings-tab.php
+++ tests/test-webhooks-settings-tab.php
@@ -20,9 +20,7 @@ class Webhooks_Settings_Tab_Test extends Wordlift_Unit_Test_Case {
 
 	public function setUp() {
 		parent::setUp();
-		if ( ! taxonomy_exists( 'post_tag' ) ) {
-			register_taxonomy( 'post_tag', 'post' );
-		}
+
 		// Reset all global filters.
 		global $wp_filter, $wp_scripts, $wp_styles;
 		$wp_filter  = array();
