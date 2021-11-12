b675a5797fa8b74a30726db474a0c0a8c25eb554
naveen <kmnaveen101@gmail.com>
2021-11-12 11:53:15 +0530

    Bump to 3.33.5

--- src/admin/js/wordlift_shortcode_tinymce_plugin.js
+++ src/admin/js/wordlift_shortcode_tinymce_plugin.js
@@ -192,7 +192,7 @@
           title: "WordLift Widgets",
           image:
             tinymce.plugins.wl_shortcodes.pluginUrl +
-            "/../images/svg/wl-logo-icon.svg?ver=3.33.4"
+            "/../images/svg/wl-logo-icon.svg?ver=3.33.5"
           // If SplitButton, must define onclick to avoid error
           //onclick: function(){ /* OPEN YOURSELF ?? */  }
         });
