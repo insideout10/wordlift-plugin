af12e914c27d22e4e8d5e1e6ed12e92c3068556a
naveen <kmnaveen101@gmail.com>
2021-12-02 11:52:28 +0530

    Revert "Install recipe maker by default"
    This reverts commit 446ba0961dcd01076552e51b819be893c4167da6.

--- manifests/entrypoint_4.7.sh
+++ manifests/entrypoint_4.7.sh
@@ -216,7 +216,6 @@ EOPHP
 	done
 fi
 wp --allow-root core install --url=https://qa-dev.wordlift.io/4.7/ --title="4.7" --admin_name=wordlift --admin_password=8xSnT65T4KM2jWgX --admin_email=you@example.com
-wp plugin install wp-recipe-maker
 #wp --allow-root config set WORDLIFT_API_URL https://api-dev.wordlift.io --path=/var/www/html/4.7/ --anchor='?php' --placement=after
 chown www-data:www-data -R /var/www/html/
 exec "$@"
