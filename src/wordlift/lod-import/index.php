833ef9b70316e877613aff81748bc19b985e8dd4
naveen <kmnaveen101@gmail.com>
2022-02-22 13:23:19 +0530

    disable lod-import by default

--- src/wordlift/lod-import/index.php
+++ src/wordlift/lod-import/index.php
@@ -2,7 +2,7 @@
 
 use Wordlift\Lod_Import\Lod_Import;
 
-if ( apply_filters( 'wl_feature__enable__lod-import', true ) ) {
+if ( apply_filters( 'wl_feature__enable__lod-import', false ) ) {
 	$lod_import = new Lod_Import();
 	$lod_import->register_hooks();
 }
