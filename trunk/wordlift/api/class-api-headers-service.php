7ca4f20d3a3eabc6a8ca549883a65720ea4e3547
naveen <kmnaveen101@gmail.com>
2022-02-22 16:12:11 +0530

    bump to 3.35.1

---
+++ trunk/wordlift/api/class-api-headers-service.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wordlift\Api;
+
+class Api_Headers_Service {
+
+	private static $instance = null;
+
+	protected function __construct() {
+
+	}
+
+	/**
+	 * This function is used to append WordPress endpoint data to every request made.
+	 * @return array
+	 */
+	public function get_wp_headers() {
+		return array(
+			'X-Wordlift-Plugin-Wp-Admin' => untrailingslashit( get_admin_url() ),
+			'X-Wordlift-Plugin-Wp-Json'  => untrailingslashit( get_rest_url() )
+		);
+	}
+
+	/**
+	 * @return Api_Headers_Service
+	 */
+	public static function get_instance() {
+		if ( self::$instance === null ) {
+			self::$instance = new Api_Headers_Service();
+		}
+
+		return self::$instance;
+	}
+
+
+}
