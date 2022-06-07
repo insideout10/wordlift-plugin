9a95635bc5d3b7940a4bacbcdce44e3a15f5fdb2
naveen <kmnaveen101@gmail.com>
2022-06-07 14:21:44 +0530

    see #1552: add autoloader

---
+++ src/modules/acf4so/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8003311d131987e78bda4716aaf49533340814e7',
+    'name' => 'wordlift/plugin-installer',
+  ),
+  'versions' => 
+  array (
+    'wordlift/plugin-installer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8003311d131987e78bda4716aaf49533340814e7',
+    ),
+  ),
+);
