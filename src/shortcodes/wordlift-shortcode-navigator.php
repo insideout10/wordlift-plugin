340b01ca669c398c5fc0f8205280bc8d93273d8c
naveen <kmnaveen101@gmail.com>
2022-09-01 13:22:15 +0530

    fix WordPress.Security.NonceVerification.Recommended class

--- src/shortcodes/wordlift-shortcode-navigator.php
+++ src/shortcodes/wordlift-shortcode-navigator.php
@@ -47,42 +47,6 @@ function wl_shortcode_navigator_data() {
 	return $results;
 }
 
-/**
- *
- * Network navigator callback function used by network-navigator endpoint
- *
- * @param $request
- *
- * @return array
- * @since 3.22.6
- */
-function wl_network_navigator_wp_json( $request ) {
-
-	// Create the cache key.
-	$cache_key_params = $_REQUEST;
-	unset( $cache_key_params['uniqid'] );
-	$cache_key = array( 'request_params' => $cache_key_params );
-
-	// Create the TTL cache and try to get the results.
-	$cache         = new Ttl_Cache( 'network-navigator', 8 * 60 * 60 ); // 8 hours.
-	$cache_results = $cache->get( $cache_key );
-
-	if ( isset( $cache_results ) ) {
-		header( 'X-WordLift-Cache: HIT' );
-
-		return $cache_results;
-	}
-
-	header( 'X-WordLift-Cache: MISS' );
-
-	$results = _wl_network_navigator_get_data( $request );
-
-	// Put the result before sending the json to the client, since sending the json will terminate us.
-	$cache->put( $cache_key, $results );
-
-	return $results;
-
-}
 
 function _wl_navigator_get_data() {
 
@@ -221,155 +185,8 @@ function _wl_navigator_get_data() {
 	return $results;
 }
 
-function _wl_network_navigator_get_data( $request ) {
 
-	// Limit the results (defaults to 4)
-	$navigator_length = isset( $request['limit'] ) ? intval( $request['limit'] ) : 4;
-	$navigator_offset = isset( $request['offset'] ) ? intval( $request['offset'] ) : 0;
-	$navigator_id     = $request['uniqid'];
-	$order_by         = isset( $_GET['sort'] ) ? sanitize_sql_orderby( wp_unslash( $_GET['sort'] ) ) : 'ID DESC';
 
-	$entities = $request['entities'];
-
-	// Post ID has to match an existing item
-	if ( ! isset( $entities ) || empty( $entities ) ) {
-		wp_send_json_error( 'No valid entities provided' );
-	}
-
-	$referencing_posts = _wl_network_navigator_get_results(
-		$entities,
-		array(
-			'ID',
-			'post_title',
-		),
-		$order_by,
-		$navigator_length,
-		$navigator_offset
-	);
-
-	// loop over them and take the first one which is not already in the $related_posts
-	$results = array();
-	foreach ( $referencing_posts as $referencing_post ) {
-		$serialized_entity = wl_serialize_entity( $referencing_post->entity_id );
-
-		/**
-		 * Use the thumbnail.
-		 *
-		 * @see https://github.com/insideout10/wordlift-plugin/issues/825 related issue.
-		 * @see https://github.com/insideout10/wordlift-plugin/issues/837
-		 *
-		 * @since 3.19.3 We're using the medium size image.
-		 */
-		$thumbnail = get_the_post_thumbnail_url( $referencing_post, 'medium' );
-
-		$result = array(
-			'post'   => array(
-				'permalink' => get_permalink( $referencing_post->ID ),
-				'title'     => $referencing_post->post_title,
-				'thumbnail' => $thumbnail,
-			),
-			'entity' => array(
-				'label'     => $serialized_entity['label'],
-				'mainType'  => $serialized_entity['mainType'],
-				'permalink' => get_permalink( $referencing_post->entity_id ),
-			),
-		);
-
-		$result['post']   = apply_filters( 'wl_network_navigator_data_post', $result['post'], intval( $referencing_post->ID ), $navigator_id );
-		$result['entity'] = apply_filters( 'wl_network_navigator_data_entity', $result['entity'], intval( $referencing_post->entity_id ), $navigator_id );
-
-		$results[] = $result;
-
-	}
-
-	if ( count( $results ) < $navigator_length ) {
-		$results = apply_filters( 'wl_network_navigator_data_placeholder', $results, $navigator_id, $navigator_offset, $navigator_length );
-	}
-
-	$results = apply_filters( 'wl_network_navigator_results', $results, $navigator_id );
-
-	return $results;
-
-}
-
-function _wl_network_navigator_get_results(
-	$entities, $fields = array(
-		'ID',
-		'post_title',
-	), $order_by = 'ID DESC', $limit = 10, $offset = 0
-) {
-	global $wpdb;
-
-	$select = implode(
-		', ',
-		array_map(
-			function ( $item ) {
-				return "p.$item AS $item";
-			},
-			(array) $fields
-		)
-	);
-
-	$order_by = implode(
-		', ',
-		array_map(
-			function ( $item ) {
-				return "p.$item";
-			},
-			(array) $order_by
-		)
-	);
-
-	$entities_in = implode(
-		',',
-		array_map(
-			function ( $item ) {
-				$entity = Wordlift_Entity_Service::get_instance()->get_entity_post_by_uri( urldecode( $item ) );
-				if ( isset( $entity ) ) {
-					  return $entity->ID;
-				}
-			},
-			$entities
-		)
-	);
-// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPress.DB.PreparedSQLPlaceholders.UnquotedComplexPlaceholder
-	/** @noinspection SqlNoDataSourceInspection */
-	return $wpdb->get_results(
-		$wpdb->prepare(
-			"
-SELECT %3\$s, p2.ID as entity_id
- FROM {$wpdb->prefix}wl_relation_instances r1
-	-- get the ID of the post entity in common between the object and the subject 2. 
-    INNER JOIN {$wpdb->posts} p2
-        ON p2.ID = r1.object_id
-            AND p2.post_status = 'publish'
-    INNER JOIN {$wpdb->posts} p
-        ON p.ID = r1.subject_id
-            AND p.post_status = 'publish'
-    INNER JOIN {$wpdb->term_relationships} tr
-     	ON tr.object_id = p.ID
-    INNER JOIN {$wpdb->term_taxonomy} tt
-     	ON tt.term_taxonomy_id = tr.term_taxonomy_id
-      	    AND tt.taxonomy = 'wl_entity_type'
-    INNER JOIN {$wpdb->terms} t
-        ON t.term_id = tt.term_id
-            AND t.slug = 'article'
-    -- select only posts with featured images.
- WHERE r1.object_id IN ({$entities_in})
- -- avoid duplicates.
- GROUP BY p.ID
- ORDER BY %4\$s
- LIMIT %1\$d
- OFFSET %2\$d
-",
-			$limit,
-			$offset,
-			$select,
-			$order_by
-		)
-	);
-// phpcs:enable
-}
 
 /**
  * The Navigator Ajax function.
@@ -423,23 +240,6 @@ add_action(
 	}
 );
 
-/**
- * Adding `rest_api_init` action for backend of network navigator
- */
-add_action(
-	'rest_api_init',
-	function () {
-		register_rest_route(
-			WL_REST_ROUTE_DEFAULT_NAMESPACE,
-			'/network-navigator',
-			array(
-				'methods'             => 'GET',
-				'callback'            => 'wl_network_navigator_wp_json',
-				'permission_callback' => '__return_true',
-			)
-		);
-	}
-);
 
 /**
  * Optimizations: disable unneeded plugins on Navigator AJAX call. WPSeo is slowing down the responses quite a bit.
@@ -449,7 +249,7 @@ add_action(
 add_action(
 	'plugins_loaded',
 	function () {
-		$action = array_key_exists( 'action', $_REQUEST ) ? sanitize_text_field( wp_unslash( (string) $_REQUEST['action'] ) ) : '';
+		$action = array_key_exists( 'action', $_REQUEST ) ? sanitize_text_field( wp_unslash( (string) $_REQUEST['action'] ) ) : ''; //phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		if ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX || 'wl_navigator' !== $action ) {
 			return;
 		}
@@ -464,7 +264,7 @@ add_action(
 add_action(
 	'init',
 	function () {
-		$action = array_key_exists( 'action', $_REQUEST ) ? sanitize_text_field( wp_unslash( (string) $_REQUEST['action'] ) ) : '';
+		$action = array_key_exists( 'action', $_REQUEST ) ? sanitize_text_field( wp_unslash( (string) $_REQUEST['action'] ) ) : ''; //phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		if ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX || 'wl_navigator' !== $action ) {
 			return;
 		}
