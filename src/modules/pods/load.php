c4d285c634679e97f79cac5b18fe293d1fc5eaaa
naveen <kmnaveen101@gmail.com>
2022-09-06 17:32:59 +0530

    see #1574: todo: make entity field work

--- src/modules/pods/load.php
+++ src/modules/pods/load.php
@@ -28,23 +28,47 @@ function __wl_pods_load() {
 	$loader            = new YamlFileLoader( $container_builder, new FileLocator( __DIR__ ) );
 	$loader->load( 'services.yml' );
 	$container_builder->compile();
-
-	pods_register_related_object( 'wlentity', 'WordLift Entity', array() );
+	pods_register_related_object( 'wlentity', 'WordLift Entity', array( 'simple' => false ) );
 }
 
-//add_filter( 'pods_field_pick_data_ajax', function ( $arg1, $field_name, $arg2, $field, $pod, $id) {
-//
-//	var_dump($field_name);
-//	var_dump($pod);
-//	var_dump($field);
-//
-//} , 10, 6 );
+add_filter( 'pods_form_ui_field_pick_ajax', function ( $item, $name, $value, $field_options ) {
+	return true;
 
-
-add_filter('pods_form_ui_field_pick_ajax', function ( $item, $name, $value, $field_options ) {
 	return isset( $field_options['pick_object'] ) && is_string( $field_options['pick_object'] ) &&
 	       'wlentity' === $field_options['pick_object'];
-}, 10, 4);
+}, 10, 4 );
+
+add_filter( 'pods_api_get_table_info', function ( $info, $object_type, $object, $name, $pod, $field ) {
+	// @TODO: filter based on field here.
+	return array( 'foo' => 'bar' );
+}, 10, 6 );
+
+add_filter( 'pods_field_pick_data_ajax', function ( $data, $name, $_, $field ) {
+
+	if ( ( ! $field instanceof Pods\Whatsit\Field ) || $field->get_arg( 'pick_object', false ) !== 'wlentity' ) {
+		return $data;
+	}
+
+	$query    = sanitize_text_field( wp_unslash( $_REQUEST['query'] ) );
+	$entities = wl_entity_get_by_title( $query, true );
+
+	return array_map( function ( $item ) {
+//		var_dump($item);
+		/**
+		 * @var $item \WP_Post
+		 */
+		return array(
+			'id'        => $item->title ,
+			'icon'      => "https:\/\/wordlift.localhost\/wp-content\/plugins\/wordlift\/images\/svg\/wl-vocabulary-icon.svg",
+			'name'      => $item->title . " (" . $item->schema_type_name . ")",
+//			'edit_link' => get_edit_post_link( $item->ID ),
+//			'link'      => get_permalink( $item->ID ),
+			'selected'  => false
+		);
+	}, $entities );
+
+}, 10, 4 );
+
 
 add_action( 'plugins_loaded', '__wl_pods_load' );
 
