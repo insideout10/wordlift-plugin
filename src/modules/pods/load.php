4fd8eac19cb95fc535329b6ed54096c6415e561f
naveen <kmnaveen101@gmail.com>
2022-09-06 09:16:49 +0530

    see #1574: create a module for pods

---
+++ src/modules/pods/load.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Module Name: Pods
+ * Description: Integrates pods framework for displaying schema.org fields
+ * Experimental: Yes
+ *
+ * @since   1.0.0
+ * @package wordlift
+ */
+
+use Wordlift\Modules\Common\Symfony\Component\Config\FileLocator;
+use Wordlift\Modules\Common\Symfony\Component\DependencyInjection\ContainerBuilder;
+use Wordlift\Modules\Common\Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+
+function __wl_pods_load() {
+	// Autoloader for plugin itself.
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require __DIR__ . '/vendor/autoload.php';
+	}
+
+	$container_builder = new ContainerBuilder();
+	$loader            = new YamlFileLoader( $container_builder, new FileLocator( __DIR__ ) );
+	$loader->load( 'services.yml' );
+	$container_builder->compile();
+
+}
+
+add_action( 'plugins_loaded', '__wl_pods_load' );
---
+++ src/modules/pods/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wordlift/acf4so',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8848c482e26f6d4d652792415f66b5cf5e9cdbb8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wordlift/acf4so' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8848c482e26f6d4d652792415f66b5cf5e9cdbb8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
