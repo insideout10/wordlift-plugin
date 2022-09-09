e2a0c818adf881da3a7028a2ed312a6c794f66ca
naveen <kmnaveen101@gmail.com>
2022-09-09 19:31:18 +0530

    see #1574: fix jsonld generation

--- src/includes/properties/class-wordlift-entity-property-service.php
+++ src/includes/properties/class-wordlift-entity-property-service.php
@@ -1,5 +1,8 @@
 <?php
 
+use Wordlift\Jsonld\Term_Reference;
+use Wordlift\Object_Type_Enum;
+
 /**
  * Process references to other entities, local or remote, by returning a
  * {@link Wordlift_Property_Entity_Reference} with the URL of the referenced entity.
@@ -18,6 +21,19 @@ class Wordlift_Entity_Property_Service extends Wordlift_Simple_Property_Service 
 		// Map each returned value to a Wordlift_Property_Entity_Reference.
 		return array_map(
 			function ( $item ) use ( $entity_service ) {
+
+				$regex = '/^(post|term)_(\d+)$/m';
+				if ( preg_match( $regex, $item, $matches ) ) {
+
+					$object_type = Object_Type_Enum::from_string( $matches[1] );
+					$identifier  = (int) $matches[2];
+					if ( $object_type === Object_Type_Enum::POST ) {
+						return is_numeric( $identifier ) && null !== get_post( $identifier )
+							? new Wordlift_Property_Entity_Reference( $entity_service->get_uri( $identifier ), (int) $identifier ) : $item;
+					} elseif ( $object_type === Object_Type_Enum::TERM ) {
+						return new Term_Reference( $identifier );
+					}
+				}
 
 				// If the $item is a number and it's an existing post, return the
 				// URI of the referenced entity. Otherwise return the value.
