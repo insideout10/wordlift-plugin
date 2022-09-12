61e0dec442bd888e0fe524692b19649da044ad19
naveen <kmnaveen101@gmail.com>
2022-09-12 09:25:04 +0530

    see #1574: register based on context

--- src/modules/pods/includes/FieldDefinition/FieldDefinitionFactory.php
+++ src/modules/pods/includes/FieldDefinition/FieldDefinitionFactory.php
@@ -18,19 +18,9 @@ class FieldDefinitionFactory {
 
 
 	public function get_field_definition() {
-
 		$context = $this->schema->get();
+		// For now we are registering all the pods.
 		return new AllPodsDefiniton( $context );
-
-		if ( Context::ADMIN_AJAX === $context->get_object_type() ) {
-			return new AllPodsDefiniton( $context );
-		} elseif ( Context::POST === $context->get_object_type() ) {
-			return new PostTypePodDefinition( $context );
-		} elseif ( Context::TERM === $context->get_object_type() ) {
-			return new TaxonomyPodDefinition( $context );
-		} else {
-			return new NoFieldDefintion( $context );
-		}
 	}
 
 
