0c17796da83c78c3089ccb82f493ee1d276468b1
David Riccitelli <david@wordlift.io>
2022-09-19 10:19:59 +0300

    fix composer configuration

--- src/modules/food-kg/scoper.inc.php
+++ src/modules/food-kg/scoper.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 use Isolated\Symfony\Component\Finder\Finder;
 
@@ -11,93 +11,102 @@ use Isolated\Symfony\Component\Finder\Finder;
 // to auto-load any code here: it can result in a conflict or even corrupt
 // the PHP-Scoper analysis.
 
-return [
-    // The prefix configuration. If a non null value is be used, a random prefix
-    // will be generated instead.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#prefix
-    'prefix' => null,
+$wp_classes   = json_decode( file_get_contents( 'vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-classes.json' ), true );
+$wp_functions = json_decode( file_get_contents( 'vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-functions.json' ), true );
+$wp_constants = json_decode( file_get_contents( 'vendor/sniccowp/php-scoper-wordpress-excludes/generated/exclude-wordpress-constants.json' ), true );
 
-    // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
-    // directory. You can however define which files should be scoped by defining a collection of Finders in the
-    // following configuration key.
-    //
-    // This configuration entry is completely ignored when using Box.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#finders-and-paths
-    'finders' => [
-        Finder::create()
-            ->files()
-            ->ignoreVCS(true)
-            ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
-            ->exclude([
-                'doc',
-                'test',
-                'test_old',
-                'tests',
-                'Tests',
-                'vendor-bin',
-            ])
-            ->in('vendor'),
-        Finder::create()->append([
-            'composer.json',
-        ]),
-    ],
+return array(
+	// The prefix configuration. If a non null value is be used, a random prefix
+	// will be generated instead.
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#prefix
+	'prefix'             => 'Wordlift\Modules\Common',
 
-    // List of excluded files, i.e. files for which the content will be left untouched.
-    // Paths are relative to the configuration file unless if they are already absolute
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
-    'exclude-files' => [
-        'src/a-whitelisted-file.php',
-    ],
+	// By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
+	// directory. You can however define which files should be scoped by defining a collection of Finders in the
+	// following configuration key.
+	//
+	// This configuration entry is completely ignored when using Box.
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#finders-and-paths
+	'finders'            => array(
+		Finder::create()
+			  ->files()
+			  ->ignoreVCS( true )
+			  ->notName( '/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/' )
+			->exclude(
+				array(
+					'doc',
+					'test',
+					'test_old',
+					'tests',
+					'Tests',
+					'vendor-bin',
+				)
+			)
+			->in(
+				array(
+					'vendor/cweagans/composer-patches',
+					'vendor/mcaskill/composer-exclude-files',
+					'vendor/psr/container',
+					'vendor/symfony/config',
+					'vendor/symfony/dependency-injection',
+					'vendor/symfony/filesystem',
+					'vendor/symfony/polyfill-ctype',
+					'vendor/symfony/polyfill-php73',
+					'vendor/symfony/polyfill-php80',
+					'vendor/symfony/yaml',
+				)
+			),
 
-    // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
-    // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
-    // support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
-    // heart contents.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
-    'patchers' => [
-        static function (string $filePath, string $prefix, string $contents): string {
-            // Change the contents here.
+		// Symfony mbstring polyfill.
+		Finder::create()
+			  ->files()
+			  ->ignoreVCS( true )
+			  ->ignoreDotFiles( true )
+			  ->name( '/\.*.php8?/' )
+			  ->in( 'vendor/symfony/polyfill-mbstring/Resources' )
+			->append(
+				array(
+					'vendor/symfony/polyfill-mbstring/Mbstring.php',
+					'vendor/symfony/polyfill-mbstring/composer.json',
+				)
+			),
 
-            return $contents;
-        },
-    ],
+		Finder::create()->append(
+			array(
+				'composer.json',
+			)
+		),
+	),
 
-    // List of symbols to consider internal i.e. to leave untouched.
-    //
-    // For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#excluded-symbols
-    'exclude-namespaces' => [
-        // 'Acme\Foo'                     // The Acme\Foo namespace (and sub-namespaces)
-        // '~^PHPUnit\\\\Framework$~',    // The whole namespace PHPUnit\Framework (but not sub-namespaces)
-        // '~^$~',                        // The root namespace only
-        // '',                            // Any namespace
-    ],
-    'exclude-classes' => [
-        // 'ReflectionClassConstant',
-    ],
-    'exclude-functions' => [
-        // 'mb_str_split',
-    ],
-    'exclude-constants' => [
-        // 'STDIN',
-    ],
+	// List of excluded files, i.e. files for which the content will be left untouched.
+	// Paths are relative to the configuration file unless if they are already absolute
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
+	'exclude-files'      => array(),
 
-    // List of symbols to expose.
-    //
-    // For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#exposed-symbols
-    'expose-global-constants' => true,
-    'expose-global-classes' => true,
-    'expose-global-functions' => true,
-    'expose-namespaces' => [
-        // 'Acme\Foo'                     // The Acme\Foo namespace (and sub-namespaces)
-        // '~^PHPUnit\\\\Framework$~',    // The whole namespace PHPUnit\Framework (but not sub-namespaces)
-        // '~^$~',                        // The root namespace only
-        // '',                            // Any namespace
-    ],
-    'expose-classes' => [],
-    'expose-functions' => [],
-    'expose-constants' => [],
-];
+	// When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
+	// original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
+	// support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
+	// heart contents.
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
+	'patchers'           => array(),
+
+	// List of symbols to consider internal i.e. to leave untouched.
+	//
+	// For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#excluded-symbols
+	'exclude-namespaces' => array(
+		// 'Acme\Foo'                     // The Acme\Foo namespace (and sub-namespaces)
+		// '~^PHPUnit\\\\Framework$~',    // The whole namespace PHPUnit\Framework (but not sub-namespaces)
+		// '~^$~',                        // The root namespace only
+		// '',                            // Any namespace
+	),
+	'exclude-classes'    => $wp_classes,
+
+	'exclude-functions'  => $wp_functions,
+
+	'exclude-constants'  => $wp_constants,
+
+);
