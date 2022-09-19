d809c982d16e901882e5e3116f2901867daf1471
naveen <kmnaveen101@gmail.com>
2022-09-19 17:09:19 +0530

    Bump to version 3.38.3

--- src/includes/class-wordlift.php
+++ src/includes/class-wordlift.php
@@ -630,7 +630,7 @@ class Wordlift {
 		self::$instance = $this;
 
 		$this->plugin_name = 'wordlift';
-		$this->version     = '3.38.2';
+		$this->version     = '3.38.3';
 		$this->load_dependencies();
 		$this->set_locale();
 
