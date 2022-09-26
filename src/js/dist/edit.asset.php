e1408f6104a06f36ee5a22205b096633cf671061
David Riccitelli <david@wordlift.io>
2022-09-26 12:02:41 +0300

    fix merge

--- src/js/dist/edit.asset.php
+++ src/js/dist/edit.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-hooks', 'wp-polyfill'), 'version' => 'a4d352dadfcc4d26881b809cee7ef9ca');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-hooks', 'wp-polyfill'), 'version' => 'af5ab60f7440e61dee7c9d7fccdd8dce');
->>>>>>> 565192e80e2a8462c14291e70e6d4d42180787e7
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-hooks', 'wp-polyfill'), 'version' => '5ec3b133a75a5d47ad115c9f2c65e97b');
--- src/js/dist/ingredients-meta-box.asset.php
+++ src/js/dist/ingredients-meta-box.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '8b885834fc98a81c781fce2f3492d1b7');
-=======
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'a20c0231fce93c1a576d4f3612064a45');
->>>>>>> 565192e80e2a8462c14291e70e6d4d42180787e7
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '96ff459218043af7711d096af5011149');
