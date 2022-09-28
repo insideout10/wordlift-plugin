1844947d30e2eb087dc2200d05d2e4da7adc65de
naveen <kmnaveen101@gmail.com>
2022-09-28 10:35:44 +0530

    Bump version to 3.38.5-dev

--- src/wordlift.php
+++ src/wordlift.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WordLift
  * Plugin URI:        https://wordlift.io
  * Description:       WordLift brings the power of AI to organize content, attract new readers and get their attention. To activate the plugin <a href="https://wordlift.io/">visit our website</a>.
- * Version:           3.38.4
+ * Version:           3.38.5-dev
  * Author:            WordLift, Insideout10
  * Author URI:        https://wordlift.io
  * License:           GPL-2.0+
