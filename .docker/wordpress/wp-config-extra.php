8724ca9182eaad6ed49b004222664c813e594dd1
David Riccitelli <david@wordlift.io>
2022-10-02 12:18:36 +0300

    update extra files

--- .docker/wordpress/wp-config-extra.php
+++ .docker/wordpress/wp-config-extra.php
@@ -1,7 +1,3 @@
-
-define( 'WP_SITEURL', 'https://wordlift.localhost/' );
-define( 'WP_HOME', 'https://wordlift.localhost/' );
-
 // Enable Debug logging to the /wp-content/debug.log file
 define( 'WP_DEBUG_LOG', true );
 
@@ -12,4 +8,4 @@ define( 'WP_DEBUG_DISPLAY', false );
 // Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
 define( 'SCRIPT_DEBUG', true );
 
-define( 'WL_DEBUG', false );
+define( 'WL_DEBUG', WP_DEBUG );
