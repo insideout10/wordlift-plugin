09085ff966464f50c2224f48b5666b9ccf6c7f68
mhimon <mahbubhasanimon@gmail.com>
2022-10-11 09:06:22 +0600

    See #1590 : Added vendor folder

---
+++ src/modules/include-exclude/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wordlift/include-exclude',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'bd972241da84abec50e580eafa4012bf04b411da',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wordlift/include-exclude' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'bd972241da84abec50e580eafa4012bf04b411da',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
