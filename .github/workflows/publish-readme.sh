5158f8cdb303cb14718e3f87378129a070768867
David Riccitelli <david@wordlift.io>
2022-10-24 10:39:00 +0300

    update script

--- .github/workflows/publish-readme.sh
+++ .github/workflows/publish-readme.sh
@@ -9,12 +9,9 @@ if [[ 'src/readme.txt' == $(git diff HEAD~1 --name-only) ]]; then
 
   version="$(get_version)"
   sed -i '' "s|^Stable tag: .*$|Stable tag: $version|g" src/readme.txt
-  git commit -m "update readme version" src/readme.txt
-  git push
 
   # Update readme.txt in trunk and stable version tag.
   svnmucc -u "$SVN_USERNAME" -p "$SVN_PASSWORD" --non-interactive --trust-server-cert \
-    put src/readme.txt "https://plugins.svn.wordpress.org/wordlift/trunk/readme.txt" -m "Update readme.txt"
-  svnmucc -u "$SVN_USERNAME" -p "$SVN_PASSWORD" --non-interactive --trust-server-cert \
+    put src/readme.txt "https://plugins.svn.wordpress.org/wordlift/trunk/readme.txt" -m "Update readme.txt" \
     put src/readme.txt "https://plugins.svn.wordpress.org/wordlift/tags/$version/readme.txt" -m "Update readme.txt"
 fi
