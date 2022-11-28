c7501ef1bb8aa2153ea207df91e90453f446b2d6
mhimon <mahbubhasanimon@gmail.com>
2022-11-28 07:54:35 +0600

    See #1623 : fixes

--- src/js/dist/block-editor.asset.php
+++ src/js/dist/block-editor.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-rich-text'), 'version' => 'eae9e0ed6bca08750c65dad33423de41');
+<?php return array(
+	'dependencies' => array( 'react', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-rich-text' ),
+	'version'      => 'eae9e0ed6bca08750c65dad33423de41',
+);
--- src/js/dist/faq.asset.php
+++ src/js/dist/faq.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-polyfill'), 'version' => '0c57500bd37063fdc65ebde551e8f64d');
+<?php return array(
+	'dependencies' => array( 'react', 'react-dom', 'wp-polyfill' ),
+	'version'      => '0c57500bd37063fdc65ebde551e8f64d',
+);
