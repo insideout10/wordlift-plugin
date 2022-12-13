0f484f1e9d4094bb01370f7946668d18b79ec8d7
mhimon <mahbubhasanimon@gmail.com>
2022-12-13 15:23:40 +0600

    See #1644 : Added Cypress ENV

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  env: {
-    wp_user: 'user',
-    wp_pass: 'bitnami',
-    version: '3.40.4',
-  },
   e2e: {
-    baseUrl: "https://wordlift.localhost",
+    baseUrl: 'https://qa-dev.wordlift.it/5.0',
     experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
