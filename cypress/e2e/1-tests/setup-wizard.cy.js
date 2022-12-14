9f630c9a8eee60a1c82368d5f8bfeea5930a2686
mhimon <mahbubhasanimon@gmail.com>
2022-12-14 11:19:43 +0600

    See #1644 : Finished Setup Wizard - About our privacy policy link test

--- cypress/e2e/1-tests/setup-wizard.cy.js
+++ cypress/e2e/1-tests/setup-wizard.cy.js
@@ -3,7 +3,7 @@ describe('Setup Wizard', () => {
 		cy.visit('/wp-admin/admin.php?page=wl-setup');
 	});
 
-	it( 'Learn more button', () => {
+	it( 'Check if learn more button works', () => {
         cy.get(':nth-child(1) > :nth-child(1) > .btn-wrapper > .button')
 			.invoke('attr', 'target', '_self')
 			.click();
@@ -12,8 +12,13 @@ describe('Setup Wizard', () => {
 		});
     });
 
-	// it( '', () => {} )
-	// it( '', () => {} )
-	// it( '', () => {} )
-	// it( '', () => {} )
+	it( 'Check if about our privacy policy link works', () => {
+        cy.get(':nth-child(1) > :nth-child(1) > .btn-wrapper > .wl-default-action').click();
+        cy.get('.privacy-policy-details > a')
+			.invoke('attr', 'target', '_self')
+			.click();
+		cy.on('url:changed', (newUrl) => {
+			expect(newUrl).to.equal('https://wordlift.io/privacy-policy/')
+		});
+    });
 });
