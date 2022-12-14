fa6823fce9248732d74fd543667385cc9ff464a8
mhimon <mahbubhasanimon@gmail.com>
2022-12-14 11:53:22 +0600

    See #1644 : Finished Setup Wizard - Grab Key link test

--- cypress/e2e/1-tests/setup-wizard.cy.js
+++ cypress/e2e/1-tests/setup-wizard.cy.js
@@ -21,4 +21,13 @@ describe('Setup Wizard', () => {
 			expect(newUrl).to.equal('https://wordlift.io/privacy-policy/')
 		});
     });
+
+	it( 'Check if grab key link works', () => {
+        cy.get(':nth-child(1) > :nth-child(1) > .btn-wrapper > .wl-default-action').click();
+        cy.get(':nth-child(2) > :nth-child(1) > .btn-wrapper > .wl-default-action').click();
+        cy.get('.page-txt > a').click();
+		cy.on('url:changed', (newUrl) => {
+			expect(newUrl).to.contain('https://wordlift.io/pricing')
+		});
+    });
 });
