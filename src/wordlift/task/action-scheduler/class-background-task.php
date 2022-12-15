5952497972a5c3196bd62884ae508843cc5185a8
naveen <kmnaveen101@gmail.com>
2022-12-15 13:42:50 +0530

    see #1648: add background task

---
+++ src/wordlift/task/action-scheduler/class-background-task.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wordlift\Task\Background\Action_Scheduler;
+
+use Wordlift\Common\Background_Process\Action_Scheduler\Action_Scheduler_Background_Process;
+use Wordlift\Task\Background\Background_Route_Task;
+
+class Background_Task extends Action_Scheduler_Background_Process implements Background_Route_Task {
+
+	public function do_task( $args ) {
+		// TODO: Implement do_task() method.
+	}
+
+	public function start() {
+		// TODO: Implement start() method.
+	}
+
+	public function stop() {
+		// TODO: Implement stop() method.
+	}
+
+	public function resume() {
+		// TODO: Implement resume() method.
+	}
+
+	public function get_info() {
+		// TODO: Implement get_info() method.
+	}
+}
---
+++ src/wordlift/task/action-scheduler/class-stopped-state.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wordlift\Task\Background\Action_Scheduler;
+
+use Wordlift\Task\Background\Abstract_Background_Task_State;
+
+class Stopped_State extends Abstract_Background_Task_State {
+
+	public function enter() {
+		// TODO: Implement enter() method.
+	}
+
+	public function leave() {
+		// TODO: Implement leave() method.
+	}
+
+	public function task( $item ) {
+		// TODO: Implement task() method.
+	}
+}
