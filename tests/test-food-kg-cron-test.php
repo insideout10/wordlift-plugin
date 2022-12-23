ffffec68714104bd4676a06b9c1769cb00b21c0d
naveen <kmnaveen101@gmail.com>
2022-12-23 15:00:10 +0530

    see #1658: todo fix api response

---
+++ tests/test-food-kg-cron-test.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @group food-kg
+ */
+class Food_Kg_Cron_Test extends Wordlift_Unit_Test_Case {
+
+	public function test_when_daily_cron_action_fired_should_start_background_process() {
+		do_action( Wordlift\Modules\Food_Kg\Module::RUN_EVENT );
+		$this->assertTrue( as_has_scheduled_action( 'wl_main_ingredient_sync' ), 'Main ingredient background process should run daily' );
+	}
+
+}
