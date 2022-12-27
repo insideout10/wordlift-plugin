f8d704a7a9c7faac60894df3f5111bd8081cf51e
mhimon <mahbubhasanimon@gmail.com>
2022-12-27 16:42:05 +0600

    See #1644 : Finished Entities Cloud test

---
+++ cypress/e2e/2-general-tests/entities-cloud.cy.js
@@ -0,0 +1,36 @@
+describe( 'Entities Cloud', () => {
+    const postTitle = 'WL QA Test Entities Cloud';
+	it( 'Add entities cloud widget in post', () => {
+        cy.createPostOrPage( postTitle, 'Founded by Insideout10 with the help of Maurizio Sarlo and Francesco Scavelli in 2017, WordLift is on a mission to automate content marketing and SEO with the help of artificial intelligence.', 'post', 'publish' );
+        cy.wait(500);
+        cy.get('button[aria-label="WordLift"]').click();
+        cy.wait(1000);
+        cy.get('.fudeeb').find('li').then( ( $li ) => {
+            cy.get( $li ).each( ( li ) => {
+                cy.wrap( li ).click();
+            });
+        });
+        
+        cy.get('.wp-block-paragraph').type('{enter}').type('{enter}');
+        cy.get('.wp-block-paragraph').last().type('/cloud');
+
+        cy.get('#components-autocomplete-item-1-block-wordlift\\/cloud').click();
+
+        cy.get('.editor-post-publish-button__button.is-primary').click()
+            .wait(1000);
+
+        cy.get('.components-button.components-snackbar__action.is-tertiary').then( ( $button ) => {
+            const href = $button.attr('href');
+            cy.visit( href );
+            cy.get('.wl-related-entities-cloud').should('be.visible');
+            cy.get('.wl-related-entities-cloud').find('a').should('have.length.at.least', 2);
+        });
+        
+    });
+
+    after( () => {
+		cy.visit('/wp-admin/edit.php?post_type=post');
+		cy.deletePost( postTitle, 'post' );
+	});
+
+});
