c7ec352da7fac639e674224ae28a89864dbbece7
naveen <kmnaveen101@gmail.com>
2023-03-23 14:00:53 +0530

    see #1668: save progress

--- src/modules/dashboard/includes/Plugin_App.php
+++ src/modules/dashboard/includes/Plugin_App.php
@@ -18,6 +18,7 @@ class Plugin_App {
 					'next_sync' => date_create( '2024-01-31 23:45:23' )->getTimestamp(),
 				),
 				'api_url'         => rest_url( '/wl-dashboard/v1' ),
+				'liftedItems'     => $this->get_lifted_items(),
 			)
 		);
 
@@ -33,6 +34,44 @@ class Plugin_App {
 			<script type=\"text/javascript\">window._wlPluginAppSettings = $params</script>
 			<iframe id='wlx-plugin-app' src='$iframe_src'></iframe>
 		";
+	}
+
+	private function get_lifted_items() {
+
+		return array(
+			array(
+				'description'   => 'Boosted Ingredient are the ones Wordlift matched with KG. Some Explanation how it helps them.',
+				'title'         => 'Lifted Ingredients',
+				'total'         => 120,
+				'color'         => '#0076f6',
+				'show_all_link' => 'https://foo.com/bar',
+				'lifted'        => 70,
+				'updated_at'    => 'Wednesday, Feb 22, 2023',
+			),
+			array(
+				'description'   => 'Boosted Recipes are the ones Wordlift matched with KG. Some Explanation how it helps them.',
+				'title'         => 'Lifted Recipes',
+				'total'         => 70,
+				'color'         => '#00c48c',
+				'show_all_link' => 'https://foo.com/bar',
+				'lifted'        => 25,
+				'updated_at'    => 'Wednesday, Feb 22, 2023',
+			),
+		);
+	}
+
+	private function get_taxonomy_data( $taxonomy ) {
+		global $wpdb;
+		$sql = "SELECT e.content_id as match_id, t.name,  e.id FROM {$wpdb->prefix}wl_entities e
+                  LEFT JOIN {$wpdb->prefix}terms t ON e.content_id = t.term_id
+                  INNER JOIN {$wpdb->prefix}term_taxonomy tt ON t.term_id = tt.term_id
+                  WHERE e.content_type = %d AND tt.taxonomy = %s";
+
+		return array(
+			'total'  => '',
+			'lifted' => '',
+		);
+
 	}
 
 }
