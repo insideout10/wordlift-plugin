b4cc42a19f5c7ed509c15609709a77bf32593565
naveen <kmnaveen101@gmail.com>
2023-03-26 08:39:16 +0530

    see #1668: refactor term matches rest controller

--- src/modules/dashboard/includes/Api/Term_Matches_Rest_Controller.php
+++ src/modules/dashboard/includes/Api/Term_Matches_Rest_Controller.php
@@ -108,47 +108,31 @@ class Term_Matches_Rest_Controller extends \WP_REST_Controller {
 	 * @var $request \WP_REST_Request
 	 */
 	public function get_term_matches( $request ) {
-		global $wpdb;
+
 		$query_params = $request->get_query_params();
-		$taxonomy     = $query_params['taxonomy'];
-		$limit        = $query_params['limit'] ? $query_params['limit'] : 10;
 
 		$cursor_args = array(
-			'limit'     => $limit,
+			'limit'     => $query_params['limit'] ? $query_params['limit'] : 10,
 			'position'  => 0,
 			'direction' => Page::FORWARD,
+			'sort'      => Page::SORT_ASC,
 		);
+
 		if ( isset( $query_params['cursor'] ) && is_string( $query_params['cursor'] ) ) {
 			$cursor_args = wp_parse_args( json_decode( base64_decode( $query_params['cursor'] ), true ), $cursor_args );
 		}
-		$operator       = $cursor_args['direction'] === Page::FORWARD ? '>' : '<';
-		$sort_direction = $cursor_args['direction'] === Page::FORWARD ? 'ASC' : 'DESC';
-		$position       = $cursor_args['position'];
 
-		$query = $wpdb->prepare(
-			"SELECT t.term_id as id, e.about_jsonld as match_jsonld,  t.name,  e.id AS match_id FROM {$wpdb->prefix}terms t
-     INNER JOIN {$wpdb->prefix}term_taxonomy tt ON t.term_id = tt.term_id
-     LEFT JOIN {$wpdb->prefix}wl_entities e ON t.term_id = e.content_id
-     WHERE e.content_type = %d AND tt.taxonomy = %s AND t.term_id {$operator} %d ORDER BY t.term_id {$sort_direction} LIMIT %d",
-			Object_Type_Enum::TERM,
-			$taxonomy,
-			$position,
-			$cursor_args['limit']
+
+		$items = $this->match_service->get_term_matches(
+			$query_params['taxonomy'],
+			$cursor_args['position'],
+			$cursor_args['limit'],
+			$cursor_args['direction'],
+			$cursor_args['sort']
 		);
 
-		error_log( $query );
 
-		$items = array_map(
-			function ( $e ) {
-				return Match_Entry::from( $e )->serialize();
-			},
-			$wpdb->get_results(
-				$query,
-				ARRAY_A
-			)
-		);
-
-		$page = new Page( $items, $limit, $position );
+		$page = new Page( $items, $cursor_args['limit'], $cursor_args['position'] );
 		return $page->serialize();
 
 	}
