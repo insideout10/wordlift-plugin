4e96e8aeb9ad4a63c3ba34e9c2275560d533938b
naveen <kmnaveen101@gmail.com>
2023-04-07 16:23:47 +0530

    see #1668: add jsonld for terms

--- src/modules/gardening-kg/includes/Jsonld.php
+++ src/modules/gardening-kg/includes/Jsonld.php
@@ -19,22 +19,59 @@ class Jsonld {
 	public function register_hooks() {
 		add_action( 'wl_post_jsonld', array( $this, 'post_jsonld' ), 10, 2 );
 		add_action( 'wl_entity_jsonld', array( $this, 'post_jsonld' ), 10, 2 );
+		// Note: priority set to 11 in order to run after the term jsonld is generated.
+		add_filter( 'wl_term_jsonld_array', array( $this, 'term_jsonld' ), 11, 2 );
 	}
 
 	public function post_jsonld( $jsonld, $post_id ) {
-		$about_jsonld = $this->content_service->get_about_jsonld(
-			Wordpress_Content_Id::create_post( $post_id )
-		);
+
 		if ( empty( $jsonld ) ) {
 			return $jsonld;
 		}
 
+		$this->add_about_jsonld( $jsonld, Wordpress_Content_Id::create_post( $post_id ) );
+
+		return $jsonld;
+	}
+
+	public function term_jsonld( $arr, $term_id ) {
+		$jsonld     = $arr['jsonld'];
+		$references = $arr['references'];
+
+		if ( count( $jsonld ) === 0 ) {
+			return $arr;
+		}
+
+		// Remove first item from term jsonld.
+		$term_jsonld = array_shift( $jsonld );
+
+		$this->add_about_jsonld( $term_jsonld, Wordpress_Content_Id::create_term( $term_id ) );
+
+		// Add back the term jsonld.
+		array_unshift( $jsonld, $term_jsonld );
+
+		return array(
+			'jsonld'     => $jsonld,
+			'references' => $references,
+		);
+	}
+
+	/**
+	 * @param $jsonld_item array A single jsonld entry ( not an numeric array )
+	 * @param $content_id \Wordlift\Content\Wordpress\Wordpress_Content_Id
+	 *
+	 * @return void
+	 */
+	private function add_about_jsonld( &$jsonld_item, $content_id ) {
+		$about_jsonld   = $this->content_service->get_about_jsonld(
+			$content_id
+		);
 		$decoded_jsonld = json_decode( $about_jsonld, true );
 		$value          = wp_is_numeric_array( $decoded_jsonld ) ? $decoded_jsonld : array( $decoded_jsonld );
 
-		$jsonld['about'] = isset( $jsonld['about'] ) ? $jsonld['about'] : array();
-		$jsonld['about'] = array_merge( $jsonld['about'], $value );
-		return $jsonld;
+		$jsonld_item['about'] = isset( $jsonld_item['about'] ) ? $jsonld_item['about'] : array();
+		$jsonld_item['about'] = array_merge( $jsonld_item['about'], $value );
+
 	}
 
 }
