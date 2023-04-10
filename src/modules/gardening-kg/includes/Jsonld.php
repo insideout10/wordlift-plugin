0d5ba74e4afcc53c05b8140dba2fccdc89b6d97c
naveen <kmnaveen101@gmail.com>
2023-04-10 09:46:47 +0530

    see #1668: add jsonld to gardening kg

--- src/modules/gardening-kg/includes/Jsonld.php
+++ src/modules/gardening-kg/includes/Jsonld.php
@@ -19,58 +19,60 @@ class Jsonld {
 	public function register_hooks() {
 		add_action( 'wl_post_jsonld', array( $this, 'post_jsonld' ), 10, 2 );
 		add_action( 'wl_entity_jsonld', array( $this, 'post_jsonld' ), 10, 2 );
-		// Note: priority set to 11 in order to run after the term jsonld is generated.
-		add_filter( 'wl_term_jsonld_array', array( $this, 'term_jsonld' ), 11, 2 );
+
 	}
 
 	public function post_jsonld( $jsonld, $post_id ) {
 
-		if ( empty( $jsonld ) ) {
-			return $jsonld;
+		$mentions = array();
+		$term_ids = $this->get_term_ids( $post_id );
+
+		foreach ( $term_ids as $term_id ) {
+			$this->add_about_jsonld( $mentions, Wordpress_Content_Id::create_term( $term_id ) );
 		}
 
-		$this->add_about_jsonld( $jsonld, Wordpress_Content_Id::create_post( $post_id ) );
+		// Add also the post jsonld.
+		$this->add_about_jsonld( $mentions, Wordpress_Content_Id::create_post( $post_id ) );
+
+		$existing_mentions = array_key_exists( 'mentions', $jsonld ) ? $jsonld['mentions'] : array();
+
+		$jsonld['mentions'] = array_merge( $existing_mentions, $mentions );
 
 		return $jsonld;
 	}
 
-	public function term_jsonld( $arr, $term_id ) {
-		$jsonld     = $arr['jsonld'];
-		$references = $arr['references'];
-
-		if ( count( $jsonld ) === 0 ) {
-			return $arr;
-		}
-
-		// Remove first item from term jsonld.
-		$term_jsonld = array_shift( $jsonld );
-
-		$this->add_about_jsonld( $term_jsonld, Wordpress_Content_Id::create_term( $term_id ) );
-
-		// Add back the term jsonld.
-		array_unshift( $jsonld, $term_jsonld );
-
-		return array(
-			'jsonld'     => $jsonld,
-			'references' => $references,
+	private function get_term_ids( $post_id ) {
+		global $wpdb;
+		$result = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT r.term_id from {$wpdb->prefix}term_relationships r 
+    INNER JOIN {$wpdb->prefix}term_taxonomy tt ON r.term_taxonomy_id=tt.term_taxonomy_id WHERE r.object_id= %d",
+				$post_id
+			)
 		);
+
+		return is_array( $result ) ? $result : array();
 	}
 
 	/**
-	 * @param $jsonld_item array A single jsonld entry ( not an numeric array )
+	 * @param $list array Adds the jsonld to the list if valid.
 	 * @param $content_id \Wordlift\Content\Wordpress\Wordpress_Content_Id
 	 *
 	 * @return void
 	 */
-	private function add_about_jsonld( &$jsonld_item, $content_id ) {
+	private function add_about_jsonld( &$list, $content_id ) {
 		$about_jsonld   = $this->content_service->get_about_jsonld(
 			$content_id
 		);
 		$decoded_jsonld = json_decode( $about_jsonld, true );
-		$value          = wp_is_numeric_array( $decoded_jsonld ) ? $decoded_jsonld : array( $decoded_jsonld );
 
-		$jsonld_item['about'] = isset( $jsonld_item['about'] ) ? $jsonld_item['about'] : array();
-		$jsonld_item['about'] = array_merge( $jsonld_item['about'], $value );
+		if ( null === $decoded_jsonld ) {
+			return;
+		}
+
+		$value = wp_is_numeric_array( $decoded_jsonld ) ? $decoded_jsonld : array( $decoded_jsonld );
+
+		$list = array_merge( $list, $value );
 
 	}
 
