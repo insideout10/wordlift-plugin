d45d728c64f14534a15d5fb7b2bf31822bc47afd
naveen <kmnaveen101@gmail.com>
2023-05-16 11:16:51 +0530

    see #1688: fix null values for about_jsonld

--- src/wordlift.php
+++ src/wordlift.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WordLift
  * Plugin URI:        https://wordlift.io
  * Description:       WordLift brings the power of AI to organize content, attract new readers and get their attention. To activate the plugin <a href="https://wordlift.io/">visit our website</a>.
- * Version:           3.44.3
+ * Version:           3.44.4
  * Author:            WordLift, Insideout10
  * Author URI:        https://wordlift.io
  * License:           GPL-2.0+
