b9e7519ab4bcb0621bb7d787870b7c6e9b333137
David Riccitelli <david@wordlift.io>
2023-07-09 15:21:39 +0300

    see #1700: remove  calls

--- src/js/dist/no-editor-analysis.asset.php
+++ src/js/dist/no-editor-analysis.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-polyfill', 'wp-rich-text' ),
-	'version'      => 'dfba913cc5301784719218f1541dee31',
+	'version'      => '09c82bae4ff8b7fddb6c114494bf7b49',
 );
