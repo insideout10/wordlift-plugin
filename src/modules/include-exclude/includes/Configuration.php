1531fdce5e88aaa5647c1b84d92b0710d2ff538e
David Riccitelli <david@wordlift.io>
2023-08-10 15:33:14 +0300

    see #1706: add support to send the default include/exclude configuration

---
+++ src/modules/include-exclude/includes/Configuration.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Wordlift\Modules\Include_Exclude;
+
+class Configuration {
+
+	private static $instance;
+
+	private $type;
+	private $urls;
+
+	protected function __construct() {
+		$include_exclude_data = get_option( 'wl_exclude_include_urls_settings', array() );
+		$this->type           = in_array(
+			$include_exclude_data['include_exclude'],
+			array(
+				'include',
+				'exclude',
+			),
+			true
+		)
+			? $include_exclude_data['include_exclude'] : 'exclude';
+		$this->urls           = $include_exclude_data['urls'];
+	}
+
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function get_type() {
+		return $this->type;
+	}
+
+	/**
+	 * Gets the default action, which is the inverse of the configuration type.
+	 *
+	 * @return string
+	 */
+	public function get_default() {
+		return ( $this->type === 'exclude' ? 'include' : 'exclude' );
+	}
+
+	public function get_urls() {
+		return $this->urls;
+	}
+
+}
