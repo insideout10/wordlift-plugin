edf0c7b003e90fdb70e9fa146ca3c30516b3e20d
Mykyta Dudariev <mykyta.dudariev@gmail.com>
2023-08-15 23:21:28 +0300

    see #1704: created migration for mathc name column

---
+++ src/install/class-wordlift-install-3-49-1.php
@@ -0,0 +1,89 @@
+<?php
+
+use Wordlift\Cache\Ttl_Cache;
+
+/**
+ * @since 3.49.1
+ */
+class Wordlift_Install_3_49_1 extends Wordlift_Install {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static $version = '3.49.1';
+
+    /**
+     * Is column exists
+     *
+     * @param $column_name
+     * @return mixed
+     */
+    public static function is_column_exists( $column_name ) {
+        global $wpdb;
+
+        return $wpdb->get_results(
+            $wpdb->prepare(
+                "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name ='{$wpdb->prefix}wl_relation_instances' AND column_name = %s",
+                $column_name
+            )
+        );
+    }
+
+    /**
+     * Install
+     *
+     * @return void
+     */
+    public function install() {
+        global $wpdb;
+
+        // Check if 'match_name' column exists
+        if ( self::is_column_exists( 'match_name' ) ) {
+            return;
+        }
+
+        // Add new 'match_name' column
+        $wpdb->query(
+            "ALTER TABLE {$wpdb->prefix}wl_relation_instances
+					ADD match_name VARCHAR(255) AFTER about_jsonld;"
+        );
+
+        // Get all rows with 'about_jsonld'
+        $results = $wpdb->get_results(
+            "SELECT id, about_jsonld FROM {$wpdb->prefix}wl_relation_instances WHERE about_jsonld IS NOT NULL",
+            ARRAY_A
+        );
+
+        // Update 'match_name' for each row
+        foreach ( $results as $row ) {
+            $match_name = $this->get_match_name( $row['about_jsonld'] );
+
+            if ( is_null($match_name) ) {
+                continue;
+            }
+
+            $wpdb->update(
+                "{$wpdb->prefix}wl_relation_instances",
+                array( 'match_name' => $match_name ),
+                array( 'id' => $row['id'] )
+            );
+        }
+
+        Ttl_Cache::flush_all();
+    }
+
+    /**
+     * Get match name
+     *
+     * @param $jsonld
+     * @return mixed|null
+     */
+    public function get_match_name( $jsonld ) {
+        $data = json_decode( $jsonld, true );
+        if ( ! $data || ! array_key_exists( 'name', $data ) ) {
+            return null;
+        }
+
+        return $data['name'];
+    }
+}
