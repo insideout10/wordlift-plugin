ea355d6188ffb28e4823cb16dc7cf3022791eaa8
David Riccitelli <david@wordlift.io>
2023-09-19 10:51:03 +0300

    bump to 3.49.2

--- src/wordlift.php
+++ src/wordlift.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WordLift
  * Plugin URI:        https://wordlift.io
  * Description:       WordLift brings the power of AI to organize content, attract new readers and get their attention. To activate the plugin <a href="https://wordlift.io/">visit our website</a>.
- * Version:           3.49.1
+ * Version:           3.49.2
  * Author:            WordLift
  * Author URI:        https://wordlift.io
  * License:           GPL-2.0+
