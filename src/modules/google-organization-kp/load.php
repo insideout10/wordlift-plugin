a06599835bee3a065a97e8e595a071b8c90ba932
David Riccitelli <david@wordlift.io>
2023-12-12 17:42:21 +0200

    see #1717: initial WIP for the Google Organization Knowledge Panel

--- src/modules/google-organization-kp/load.php
+++ src/modules/google-organization-kp/load.php
@@ -1,11 +1,5 @@
 <?php
 
-use Wordlift\Modules\Common\Symfony\Component\Config\FileLocator;
-use Wordlift\Modules\Common\Symfony\Component\DependencyInjection\ContainerBuilder;
-use Wordlift\Modules\Common\Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Wordlift\Modules\Include_Exclude_Push_Config\Include_Exclude_API;
-use Wordlift\Modules\Include_Exclude_Push_Config\Include_Exclude_Default_Config_Installer;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
