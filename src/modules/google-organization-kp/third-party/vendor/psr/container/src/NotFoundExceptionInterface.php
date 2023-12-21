ce0afd6fe7d48a756e8355426b8bdb16d1b57e34
Mauro Anganuzzi <mauro.anganuzzi@gmail.com>
2023-12-21 11:38:15 +0100

    See #1717: Initial work on Google Organization KP API

---
+++ src/modules/google-organization-kp/third-party/vendor/psr/container/src/NotFoundExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wordlift\Modules\Google_Organization_Kp\Psr\Container;
+
+/**
+ * No entry was found in the container.
+ */
+interface NotFoundExceptionInterface extends ContainerExceptionInterface
+{
+}
---
+++ src/modules/google-organization-kp/third-party/vendor/symfony/polyfill-php80/Resources/stubs/PhpToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wordlift\Modules\Google_Organization_Kp;
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+if (\PHP_VERSION_ID < 80000 && \extension_loaded('tokenizer')) {
+    class PhpToken extends \Wordlift\Modules\Google_Organization_Kp\Symfony\Polyfill\Php80\PhpToken
+    {
+    }
+}
