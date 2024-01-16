e5d670b20bcca46ca9746b998fbc5da0b62c016c
Mauro Anganuzzi <mauro.anganuzzi@gmail.com>
2024-01-17 00:27:59 +0100

    Added 'wl_website_jsonld' filter as 'wl_after_get_jsonld' doesn't get called on the homepage.

--- src/modules/google-organization-kp/includes/About_Page_Organization_Filter.php
+++ src/modules/google-organization-kp/includes/About_Page_Organization_Filter.php
@@ -17,26 +17,29 @@ class About_Page_Organization_Filter {
 	 * @since
 	 */
 	public function init() {
-		add_filter( 'wl_post_jsonld', array( $this, '_wl_post_jsonld__expand_publisher' ), 10, 3 );
-		add_filter( 'wl_after_get_jsonld', array( $this, '_wl_after_get_jsonld__add_organization' ), 10, 3 );
+		add_filter( 'wl_website_jsonld', array( $this, '_wl_website_jsonld__add_organization_jsonld' ), 10, 3 );
+		add_filter( 'wl_after_get_jsonld', array( $this, '_wl_after_get_jsonld__add_organization_jsonld' ), 10, 3 );
 	}
 
-	public function _wl_post_jsonld__expand_publisher( $jsonld, $post_id, $references ) {
-
-
-
-		return $jsonld;
-	}
-
-	public function _wl_after_get_jsonld__add_organization( $jsonld, $post_id, $references ) {
-
+	public function add_organization_jsonld( $jsonld, $post_id ) {
+		$is_about_us = false; // @todo: Check if this is the about us page.
 		$is_homepage = is_home() || is_front_page();
 
-		// @todo: Check if "About page" property is set
-		if ( false || $is_homepage ) {
-			// @todo: Add organization JSON-LD
+		if ( $is_about_us || ! $is_about_us && $is_homepage ) {
+			// @todo: Check if publisher exists in JSON-LD.
+			// @todo: Add organization data.
+
+			$in = true;
 		}
 
 		return $jsonld;
+	}
+
+	public function _wl_website_jsonld__add_organization_jsonld( $jsonld, $post_id ) {
+		return $this->add_organization_jsonld( $jsonld, $post_id );
+	}
+
+	public function _wl_after_get_jsonld__add_organization_jsonld( $jsonld, $post_id, $context ) {
+		return $this->add_organization_jsonld( $jsonld, $post_id );
 	}
 }
