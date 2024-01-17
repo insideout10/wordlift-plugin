1a3a86e60dc40aacfd93babb6c111587322aca96
Mauro Anganuzzi <mauro.anganuzzi@gmail.com>
2024-01-17 16:58:25 +0100

    Expand publisher JSON-LD with Organization properties.

--- src/modules/google-organization-kp/includes/About_Page_Organization_Filter.php
+++ src/modules/google-organization-kp/includes/About_Page_Organization_Filter.php
@@ -12,6 +12,10 @@ namespace Wordlift\Modules\Google_Organization_Kp;
 
 use Wordlift\Modules\Google_Organization_Kp\Organization_Extra_Fields_Service;
 use Wordlift\Relation\Relations;
+use Wordlift_Configuration_Service;
+use Wordlift_Entity_Service;
+use Wordlift_Post_To_Jsonld_Converter;
+use Wordlift_Publisher_Service;
 use Wordlift_Storage_Factory;
 use Wordlift_Schema_Service;
 
@@ -34,43 +38,25 @@ class About_Page_Organization_Filter {
 		return $this->add_organization_jsonld( $jsonld, $post_id );
 	}
 
-	public function is_publisher_entity_in_graph() {
-		// @todo
-		return;
+	public function is_publisher_entity_in_graph( $jsonld, $publisher_id ) {
+		$publisher_uri = Wordlift_Entity_Service::get_instance()->get_uri( $publisher_id );
+
+		foreach ( $jsonld as $item ) {
+			if ( $item && array_key_exists( '@id', $item ) && $item['@id'] === $publisher_uri ) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
-	public function add_organization_jsonld( $jsonld, $post_id ) {
-		$is_about_us = false; // @todo: Check if this is the about us page.
-		$is_homepage = is_home() || is_front_page();
-
-		// Return when we are not looking at `About Us` page, or the `Home Page` when `About Us` is not set.
-		if ( ! ( $is_about_us || ! $is_about_us && $is_homepage ) ) {
-			return $jsonld;
-		}
-
-		// Exit if the Publisher is not set or correctly configured.
-		if ( ! \Wordlift_Publisher_Service::get_instance()->is_publisher_set() ) {
-			return $jsonld;
-		}
-
-		// Build the base JSON-LD
-		$publisher_id   = \Wordlift_Configuration_Service::get_instance()->get_publisher_id();
-		$references     = array();
-		$reference_info = array();
-		$relations      = new Relations();
-
-		$organization_jsonld = \Wordlift_Post_To_Jsonld_Converter::get_instance()->convert(
-			$publisher_id,
-			$references,
-			$reference_info,
-			$relations
-		);
-
+	public function expand_publisher_jsonld( &$publisher_jsonld, $publisher_id ) {
 		$schema_service                   = Wordlift_Schema_Service::get_instance();
 		$storage_factory                  = Wordlift_Storage_Factory::get_instance();
 		$organization_extra_field_service = Organization_Extra_Fields_Service::get_instance();
 
 		// Get custom fields.
+
 		$street_address = $storage_factory->post_meta( $schema_service::FIELD_ADDRESS )->get( $publisher_id )[0];
 		$locality       = $storage_factory->post_meta( $schema_service::FIELD_ADDRESS_LOCALITY )->get( $publisher_id )[0];
 		$region         = $storage_factory->post_meta( $schema_service::FIELD_ADDRESS_REGION )->get( $publisher_id )[0];
@@ -83,13 +69,13 @@ class About_Page_Organization_Filter {
 
 		// If all address fields are available, build the `address` property with its sub properties.
 		if (
-			isset( $street_address ) && ! empty( $street_address )
-			&& isset( $locality ) && ! empty( $locality )
-			&& isset( $region ) && ! empty( $region )
-			&& isset( $country ) && ! empty( $country )
-			&& isset( $postal_code ) && ! empty( $postal_code )
+			! empty( $street_address )
+			&& ! empty( $locality )
+			&& ! empty( $region )
+			&& ! empty( $country )
+			&& ! empty( $postal_code )
 		) {
-			$organization_jsonld['address'] = array(
+			$publisher_jsonld['address'] = array(
 				'@type'           => 'PostalAddress',
 				'streetAddress'   => $street_address,
 				'addressLocality' => $locality,
@@ -99,19 +85,74 @@ class About_Page_Organization_Filter {
 			);
 		}
 
-		if ( isset( $telephone ) && ! empty( $telephone ) ) {
-			$organization_jsonld['telephone'] = $telephone;
+		if ( ! empty( $telephone ) ) {
+			$publisher_jsonld['telephone'] = $telephone;
 		}
 
-		if ( isset( $email ) && ! empty( $email ) ) {
-			$organization_jsonld['email'] = $email;
+		if ( ! empty( $email ) ) {
+			$publisher_jsonld['email'] = $email;
 		}
 
 		// Set extra fields.
+
 		$extra_fields = $organization_extra_field_service->get_all_field_data();
+
 		foreach( $extra_fields as $field_slug => $field_value ) {
 			$field_label = $organization_extra_field_service->get_field_label( $field_slug );
-			$organization_jsonld[$field_label] = $field_value;
+			$publisher_jsonld[$field_label] = $field_value;
+		}
+	}
+
+	public function add_organization_jsonld( $jsonld, $post_id ) {
+		// Exit if the Publisher is not set or correctly configured.
+		if ( ! Wordlift_Publisher_Service::get_instance()->is_publisher_set() ) {
+			return $jsonld;
+		}
+
+		$is_about_us = false; // @todo: Check if this is the about us page.
+		$is_homepage = is_home() || is_front_page();
+
+		// Return when we are not looking at `About Us` page, or the `Home Page` when `About Us` is not set.
+		if ( ! ( $is_about_us || ! $is_about_us && $is_homepage ) ) {
+			return $jsonld;
+		}
+
+		$publisher_id = Wordlift_Configuration_Service::get_instance()->get_publisher_id();
+
+		// Add publisher to the JSON-LD if it doesn't exist in the graph.
+		if ( ! $this->is_publisher_entity_in_graph( $jsonld, $publisher_id ) ) {
+			// Get the Publisher data
+			$references     = array();
+			$reference_info = array();
+			$relations      = new Relations();
+
+			$publisher_jsonld = Wordlift_Post_To_Jsonld_Converter::get_instance()->convert(
+				$publisher_id,
+				$references,
+				$reference_info,
+				$relations
+			);
+
+			$jsonld['publisher'] = array(
+				'@id' => $publisher_jsonld['@id']
+			);
+
+			$jsonld[] = $publisher_jsonld;
+		}
+
+		// Check all items in the JSON-LD array expand Publisher when found.
+
+		$publisher_types = array(
+			'Person',
+			'Organization',
+			'localBusiness',
+			'onlineBusiness'
+		);
+
+		foreach( $jsonld as &$jsonld_item ) {
+			if ( $jsonld_item && array_key_exists( '@type', $jsonld_item ) && in_array( $jsonld_item['@type'], $publisher_types ) ) {
+				$this->expand_publisher_jsonld( $jsonld_item, $publisher_id );
+			}
 		}
 
 		return $jsonld;
